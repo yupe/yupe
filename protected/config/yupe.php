76316ef6ab72e8de00443cc543bc51f573b6652f
Yupe <aopeykin@yandex.ru>
2011-10-07 14:36:03 +0400

    почистил от пока ненужного хлама...

--- protected/config/yupe.php
+++
@@ -1,8 +0,0 @@
-<?php
- return array(
-    'class' => 'application.components.Yupe',
-    'siteDescription' => 'Блогик',
-    'siteName' => 'Блогик',
-    'siteKeyWords' => 'Блогик!!!!!!!!',
-);
-?>
