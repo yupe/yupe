8a528afb773d7bcb0051dc5e2a55bc36b02f2cc0
Archaron <tsm@glavset.ru>
2012-03-03 21:08:12 +0400

    Issue #86 Неверные ссылки восстановления пароля

--- themes/default/views/user/account/passwordRecoveryEmail.php
+++ themes/default/views/user/account/passwordRecoveryEmail.php
@@ -1,3 +1,6 @@
+<?php
+	$url = Yii::app()-> request-> hostInfo.$this-> createUrl('account/recoveryPassword', array('code'=> $model-> code));
+?>
 <html>
 <head>
     <title><?php echo Yii::t('user', 'Восстановление пароля для сайта {site} !',array('{site}' => CHtml::encode(Yii::app()->name)));?></title>
@@ -10,12 +13,10 @@
 <?php echo Yii::t('user', 'Если это были не Вы - просто удалите это письмо.');?>
 <br/>
 
-<?php echo Yii::t('user', 'Для восстановления пароля, пожалуйста, перейдите по');?> <a href='http://<?php echo $_SERVER['HTTP_HOST']?>/index.php/user/account/recoveryPassword/code/<?php echo $model->code;?>'><?php echo Yii::t('user', 'ссылке');?></a>
+<?php echo Yii::t('user', 'Для восстановления пароля, пожалуйста, перейдите по');?> <a href='<?php echo $url; ?>'><?php echo Yii::t('user', 'ссылке');?></a>
 
 <br/>
-
-http://<?php echo $_SERVER['HTTP_HOST']?>/index.php/user/account/recoveryPassword/code/<?php echo $model->code;?>
-
+<?php echo $url; ?>
 <br/><br/>
 
 <?php echo Yii::t('user', 'С уважением, администрация сайта {site} !',array('{site}' => CHtml::encode(Yii::app()->name)));?>
