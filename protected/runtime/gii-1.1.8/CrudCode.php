294b21427344284376b2d7e900ab89a5d6c996f1
Yupe <aopeykin@yandex.ru>
2012-03-21 17:46:20 +0400

    Модуль blog + мелкие правки

--- protected/runtime/gii-1.1.8/CrudCode.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'baseControllerClass' => 'YBackController',
-);
--- protected/runtime/gii-1.1.8/ModelCode.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'tablePrefix' => '',
-  'modelPath' => 'application.modules.blog.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
-);
