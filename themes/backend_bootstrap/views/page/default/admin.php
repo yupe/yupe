976fc756c508a094c0d80bc58bc96019af623954
adminnu <mail@lyzhenkov.ru>
2012-04-30 00:46:44 +0400

    Исправлена ошибка в админке страниц + Рефакторинг

--- themes/backend_bootstrap/views/page/default/admin.php
+++ themes/backend_bootstrap/views/page/default/admin.php
@@ -1,86 +1,87 @@
-<?php $this->pageTitle = Yii::t('user', 'Управление страницами'); ?>
-
 <?php
-$this->breadcrumbs = array(
-    $this->getModule('page')->getCategory() => array(''),
-    Yii::t('page', 'Страницы') => array('admin'),
-    Yii::t('page', 'Управление'),
-);
-
-$this->menu = array(
-    array('label' => Yii::t('page', 'Добавить страницу'), 'url' => array('create')),
-);
-
-
-Yii::app()->clientScript->registerScript('search', "
-$('#Page_parent_Id').val('');
-$('#Page_status').val('');
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$.fn.yiiGridView.update('page-grid', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
+    $this->pageTitle = Yii::t('user', 'Управление страницами');
+    $this->breadcrumbs = array(
+        $this->getModule('page')->getCategory() => array(''),
+        Yii::t('page', 'Страницы') => array('admin'),
+        Yii::t('page', 'Управление'),
+    );
+    
+    $this->menu = array(
+        array('label' => Yii::t('page', 'Добавить страницу'), 'url' => array('create')),
+    );
+    
+    Yii::app()->clientScript->registerScript('search', "
+        $('#Page_parent_Id').val('');
+        $('#Page_status').val('');
+        $('.search-button').click(function() {
+        	$('.search-form').toggle();
+        	return false;
+        });
+        $('.search-form form').submit(function() {
+        	$.fn.yiiGridView.update('page-grid', {
+        		data: $(this).serialize()
+        	});
+        	return false;
+        });
+    ");
 ?>
 
-<h1><?php echo $this->module->getName();?></h1>
+<h1><?=$this->module->getName()?></h1>
 
 <?php $this->widget('YModuleInfo'); ?>
 
-<?php echo CHtml::link(Yii::t('page', 'Поиск страниц'), '#', array('class' => 'search-button')); ?>
+<?=CHtml::link(Yii::t('page', 'Поиск страниц'), '#', array('class' => 'search-button'))?>
+
 <div class="search-form" style="display:none">
-    <?php $this->renderPartial('_search', array(
-                                               'model' => $model,
-                                               'pages' => $pages
-                                          ));
+    <?php
+        $this->renderPartial('_search', array(
+           'model' => $model,
+           'pages' => $pages
+        ));
     ?>
 </div><!-- search-form -->
 
 <?php
-
-$this->widget('bootstrap.widgets.BootGridView', array(
-                                                 'type'=>'striped bordered condensed',
-                                                 'id' => 'page-grid',
-                                                 'dataProvider' => $model->search(),
-                                                 'columns' => array(
-                                                     'id',
-                                                     array(
-                                                         'name' => 'name',
-                                                         'type' => 'raw',
-                                                         'value' => 'CHtml::link($data->name,array("/page/default/update","id" => $data->id))'
-                                                     ),
-                                                     array(
-                                                         'name' => 'parent_Id',
-                                                         'value' => '$data->parent_Id ? page::model()->findByPk($data->parent_Id)->name : Yii::t("page","нет")'
-                                                     ),
-                                                     'title',
-                                                     array(
-                                                         'name' => 'status',
-                                                         'value' => '$data->getStatus()',
-                                                     ),
-                                                     array(
-                                                         'name'=>'creation_date',
-                                                         'type'=>'datetime',
-                                                         'value'=> 'Yii::app()->dateFormatter->formatDateTime($data->creation_date,"short")',
-                                                     ),
-                                                     'change_date',
-                                                     array(
-                                                         'name' => 'user_id',
-                                                         'value' => '$data->author->getFullName()'
-                                                     ),
-                                                     array(
-                                                         'name' => 'change_user_id',
-                                                         'value' => '$data->changeAuthor->getFullName()'
-                                                     ),
-                                                     array(
-                                                         'class' => 'bootstrap.widgets.BootButtonColumn',
-                                                         'htmlOptions'=>array('style'=>'width: 50px'),
-                                                     ),
-
-                                                 ),
-                                            )); ?>
+    $this->widget('bootstrap.widgets.BootGridView', array(
+        'type'=>'striped bordered condensed',
+        'id'=>'page-grid',
+        'dataProvider'=>$model->search(),
+        'columns'=>array(
+            'id',
+             array(
+                'name'=>'name',
+                'type'=>'raw',
+                'value'=>'CHtml::link($data->name,array("/page/default/update","id" => $data->id))'
+             ),
+             array(
+                'name'=>'parent_Id',
+                'value'=>'$data->parent_Id ? page::model()->findByPk($data->parent_Id)->name : Yii::t("page","нет")'
+             ),
+             'title',
+             array(
+                'name'=>'status',
+                'value'=>'$data->getStatus()',
+             ),
+             array(
+                'name'=>'creation_date',
+                'value'=>'Yii::app()->dateFormatter->formatDateTime($data->creation_date, "short")',
+             ),
+             array(
+                'name'=>'change_date',
+                'value'=>'Yii::app()->dateFormatter->formatDateTime($data->creation_date, "short")',
+             ),
+             array(
+                'name'=>'user_id',
+                'value'=>'$data->author->getFullName()'
+             ),
+             array(
+                'name'=>'change_user_id',
+                'value'=>'$data->changeAuthor->getFullName()'
+             ),
+             array(
+                'class'=>'bootstrap.widgets.BootButtonColumn',
+                'htmlOptions'=>array('style'=>'width: 50px'),
+            ),
+        ),
+    ));
+?>
