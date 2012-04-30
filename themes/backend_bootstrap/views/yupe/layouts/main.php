050f8e20ef27ff616fc2d1c5c6acee9d3c570212
adminnu <mail@lyzhenkov.ru>
2012-04-30 05:04:53 +0400

    Фикс для работы yii-bootstrap после его обновления.

--- themes/backend_bootstrap/views/yupe/layouts/main.php
+++ themes/backend_bootstrap/views/yupe/layouts/main.php
@@ -4,9 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="language" content="en"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php
-        Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl. '/css/styles.css');
-?>
+    
     <link rel="stylesheet" type="text/css"  href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css"/>
     <title><?php echo CHtml::encode(Yii::app()->name);?> <?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
