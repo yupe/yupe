223f205ff48d647dc9ab8faaf9e2eba9aafc76f5
adminnu <mail@lyzhenkov.ru>
2012-05-02 13:38:13 +0400

    Рефакторинг модуля blog

--- protected/modules/blog/views/blogAdmin/create.php
+++ protected/modules/blog/views/blogAdmin/create.php
@@ -1,15 +1,15 @@
 <?php
-$this->breadcrumbs=array(
-	Yii::t('blog','Блоги') => array('admin'),
-	Yii::t('blog','Добавление'),
-);
+    $this->breadcrumbs = array(
+        Yii::t('blog', 'Блоги')=>array('admin'),
+        Yii::t('blog', 'Добавление'),
+    );
 
-$this->menu=array(
-	array('label' => Yii::t('blog','Список блогов'), 'url'=>array('index')),
-	array('label' => Yii::t('blog','Управление блогами'), 'url'=>array('admin')),
-);
+    $this->menu = array(
+        array('label'=>Yii::t('blog', 'Список блогов'), 'url'=>array('index')),
+        array('label'=>Yii::t('blog', 'Управление блогами'), 'url'=>array('admin')),
+    );
 ?>
 
-<h1><?php echo Yii::t('blog','Добавление блога');?></h1>
+<h1><?php echo Yii::t('blog','Добавление блога'); ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- protected/modules/blog/views/postAdmin/index.php
+++ protected/modules/blog/views/postAdmin/index.php
@@ -1,20 +1,22 @@
-<?php $this->pageTitle = Yii::t('blog', 'Записи'); ?>
-
 <?php
-$this->breadcrumbs = array(
-    $this->getModule('blog')->getCategory() => array(''),
-    Yii::t('page', 'Записи'),
-);
+    $this->pageTitle = Yii::t('blog', 'Записи');
 
-$this->menu = array(
-    array('label' => Yii::t('blog', 'Добавить запись'), 'url' => array('create')),
-    array('label' => Yii::t('bog', 'Управление записями'), 'url' => array('admin')),
-);
+    $this->breadcrumbs = array(
+        $this->getModule('blog')->getCategory()=>array(''),
+        Yii::t('page', 'Записи'),
+    );
+
+    $this->menu = array(
+        array('label'=>Yii::t('blog', 'Добавить запись'), 'url'=>array('create')),
+        array('label'=>Yii::t('bog', 'Управление записями'), 'url'=>array('admin')),
+    );
 ?>
 
-<h1><?php echo Yii::t('page', 'Записи');?></h1>
+<h1><?php echo Yii::t('page', 'Записи'); ?></h1>
 
-<?php $this->widget('zii.widgets.CListView', array(
-    'dataProvider' => $dataProvider,
-    'itemView' => '_view',
-)); ?>
+<?php
+    $this->widget('zii.widgets.CListView', array(
+        'dataProvider'=>$dataProvider,
+        'itemView'=>'_view',
+    ));
+?>
