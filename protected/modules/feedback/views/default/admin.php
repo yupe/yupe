4c15f12b72fb2fa9bd35bdb0329bfb41413b2008
adminnu <mail@lyzhenkov.ru>
2012-05-04 11:18:58 +0400

    Исправлен #145 + рефакторинг

--- protected/modules/feedback/views/default/admin.php
+++ protected/modules/feedback/views/default/admin.php
@@ -1,6 +1,7 @@
 <?php $this->pageTitle = Yii::t('feedback', 'Сообщения с сайта'); ?>
 
 <?php
+//@formatter:off
 $this->breadcrumbs = array(
     Yii::t('feedback', 'Сообщения с сайта') => array('admin'),
     Yii::t('feedback', 'Управление'),
@@ -10,57 +11,59 @@ $this->menu = array(
     array('label' => Yii::t('feedback', 'Добавить сообщение'), 'url' => array('create')),
     array('label' => Yii::t('feedback', 'Список сообщений'), 'url' => array('index')),
 );
-
+//@formatter:on
 Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$.fn.yiiGridView.update('feed-back-grid', {
-		data: $(this).serialize()
-	});
-	return false;
-});
+    $('.search-button').click(function() {
+        $('.search-form').toggle();
+        return false;
+    });
+    $('.search-form form').submit(function() {
+        $.fn.yiiGridView.update('feed-back-grid', {
+            data: $(this).serialize()
+        });
+        return false;
+    });
 ");
 ?>
 
-<h1><?php echo $this->module->getName();?></h1>
+<h1><?php echo $this->module->getName(); ?></h1>
 
 <?php $this->widget('YModuleInfo'); ?>
 
 <?php echo CHtml::link(Yii::t('feedback', 'Поиск сообщений'), '#', array('class' => 'search-button')); ?>
 <div class="search-form" style="display:none">
-    <?php $this->renderPartial('_search', array(
-                                               'model' => $model,
-                                          )); ?>
+    <?php $this->renderPartial('_search', array('model' => $model)); ?>
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-                                                       'id' => 'feed-back-grid',
-                                                       'dataProvider' => $model->search(),
-                                                       'columns' => array(
-                                                           'id',
-                                                           array(
-                                                               'name' => 'theme',
-                                                               'type' => 'raw',
-                                                               'value' => 'CHtml::link($data->theme,array("/feedback/default/update/","id" => $data->id))'
-                                                           ),
-                                                           array(
-                                                               'name' => 'type',
-                                                               'value' => '$data->getType()'
-                                                           ),
-                                                           'name',
-                                                           'email',
-                                                           array(
-                                                               'name' => 'status',
-                                                               'value' => '$data->getStatus()',
-                                                               'filter' => CHtml::activeDropDownList($model, 'status', $model->getTypeList())
-                                                           ),
-                                                           'creation_date',
-                                                           'change_date',
-                                                           array(
-                                                               'class' => 'CButtonColumn',
-                                                           ),
-                                                       ),
-                                                  )); ?>
+<?php $this->widget('YCustomGridView', array(
+        'id' => 'feed-back-grid',
+        'dataProvider' => $model->search(),
+        'columns' => array(
+            'id',
+            array(
+                'name' => 'theme',
+                'type' => 'raw',
+                'value' => 'CHtml::link($data->theme,array("/feedback/default/update/", "id" => $data->id))',
+            ),
+            array(
+                'name' => 'type',
+                'value' => '$data->getType()',
+            ),
+            'name',
+            'email',
+            array(
+                'name' => 'status',
+                'value' => '$data->getStatus()',
+                'filter' => CHtml::activeDropDownList($model, 'status', $model->getTypeList()),
+            ),
+            array(
+                'name' => 'is_faq',
+                'type' => 'raw',
+                'value' => '$data->getIsFaq()',
+            ),
+            'creation_date',
+            'change_date',
+            array('class' => 'CButtonColumn'),
+        ),
+    ));
+ ?>
