ebbbe8ffcaea65cfb334708a485160bd0da36d97
Yupe <aopeykin@yandex.ru>
2012-05-05 16:31:35 +0400

    начал доделывать авторизацию через соц. сети

--- protected/modules/social/SocialModule.php
+++ protected/modules/social/SocialModule.php
@@ -1,6 +1,44 @@
 <?php
 class SocialModule extends YWebModule
-{	
+{
+    public $twitterKey;
+
+    public $twitterSecret;
+
+    public $facebookClientId;
+
+    public $facebookClientSecret;
+
+    public $vkontakteClientId;
+
+    public $vkontakteClientSecret;
+
+    public $mailruClientId;
+
+    public $mailruClientSecret;
+
+    public function getEditableParams()
+    {
+        return array(
+            'twitterKey',
+            'twitterSecret',
+            'facebookClientId',
+            'facebookClientSecret',
+            'vkontakteClientId',
+            'vkontakteClientSecret',
+            'mailruClientId',
+            'mailruClientSecret'
+        );
+    }
+
+    public function getParamsLabels()
+    {
+        return array(
+            'vkontakteClientId' => Yii::t('social','Вконтакте. ID приложения'),
+            'vkontakteClientSecret' => Yii::t('social','Вконтакте. Защищенный ключ')
+        );
+    }
+
 	public function getCategory()
     {
         return Yii::t('social', 'Сервисы');
