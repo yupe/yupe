74fa3fd6efb2a9d80326c3c4634f8f8860f49b47
adminnu <mail@lyzhenkov.ru>
2012-05-16 13:31:13 +0400

    Отдельный рефакторинг.

--- themes/backend_bootstrap/views/yupe/layouts/main.php
+++ themes/backend_bootstrap/views/yupe/layouts/main.php
@@ -1,4 +1,4 @@
-<?php $module = Yii::app()->getModule('yupe');?>
+<?php $module = Yii::app()->getModule('yupe'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,7 @@
     <meta name="language" content="en"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php echo CHtml::encode(Yii::app()->name);?> <?php echo CHtml::encode($this->pageTitle); ?></title>
+    <title><?php echo CHtml::encode(Yii::app()->name); ?> <?php echo CHtml::encode($this->pageTitle); ?></title>
     <link rel="stylesheet" type="text/css"  href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css"/>
 </head>
 
@@ -15,47 +15,46 @@
 <div id="overall-wrap">
 <!-- mainmenu -->
     <?php
+    $items = $module->getModules(true);
 
-        $items=$module->getModules(true);
-
-        // Для верхнего меню делаем иконки белыми ибо он с черным фоном
-        foreach($items as &$itm)
-            if (isset($itm['icon']) && $itm['icon'])
-                $itm['icon'].=" white";
+    // Для верхнего меню делаем иконки белыми ибо он с черным фоном
+    foreach ($items as &$itm)
+        if (isset($itm['icon']) && $itm['icon'])
+            $itm['icon'] .= " white";
 
     $rightMenu = array(
         array(
             'icon' => 'home white',
             'label' => Yii::t('yupe', 'На сайт'),
-            'url' => array('/')
+            'url' => array('/'),
         ),
         array(
-        'icon' => 'off white',
-        'label' => Yii::t('yupe', 'Выйти'),
-        'url' => array('/user/account/logout'),
-        'visible' => Yii::app()->user->isAuthenticated()
+            'icon' => 'off white',
+            'label' => Yii::t('yupe', 'Выйти'),
+            'url' => array('/user/account/logout'),
+            'visible' => Yii::app()->user->isAuthenticated(),
         ),
     );
 
     $this->widget('bootstrap.widgets.BootNavbar', array(
-            'fixed'=> 'top',
-            'fluid'=>true,
-            'brand'=>"<img src='".Yii::app()->theme->baseUrl."/images/logo.png' width=\"38\" height=\"38\" title=\"".Yii::t('yupe','Перейти на главную панели управления')."\"/>",//Yii::t('yupe',"Юпи!"),
-            'brandUrl' => CHtml::normalizeUrl(array("/yupe/backend")),
-            'collapse'=>false, // requires bootstrap-responsive.css
-            'items'=>array(
-                array(
-                    'class'=>'bootstrap.widgets.BootMenu',
-                    'items'=>$items,
-                ),
-                array(
-                    'class'=>'bootstrap.widgets.BootMenu',
-                    'htmlOptions'=>array('class'=>'pull-right'),
-                    'items'=>$rightMenu,
-                ),
-                "<div style='float:right;line-height:15px;'><small style='font-size: 80%;'>".Yii::t('yupe','Администатор')."</small><br /><span class='label'>".Yii::app()->user->nick_name."</span></div>",
+        'fixed' => 'top',
+        'fluid' => true,
+        'brand' => "<img src='" . Yii::app()->theme->baseUrl . "/images/logo.png' width=\"38\" height=\"38\" title=\"" . Yii::t('yupe', 'Перейти на главную панели управления') . "\"/>", //Yii::t('yupe',"Юпи!"),
+        'brandUrl' => CHtml::normalizeUrl(array("/yupe/backend")),
+        'collapse' => false, // requires bootstrap-responsive.css
+        'items' => array(
+            array(
+                'class' => 'bootstrap.widgets.BootMenu',
+                'items' => $items,
             ),
-        ));
+            array(
+                'class' => 'bootstrap.widgets.BootMenu',
+                'htmlOptions' => array('class' => 'pull-right'),
+                'items' => $rightMenu,
+            ),
+            "<div style='float:right;line-height:15px;'><small style='font-size: 80%;'>" . Yii::t('yupe', 'Администатор') . "</small><br /><span class='label'>" . Yii::app()->user->nick_name . "</span></div>",
+        ),
+    ));
     ?>
     <div class="container-fluid" id="page">
         <?php echo $content; ?>
@@ -66,9 +65,9 @@
 
     <footer>
         Copyright &copy; 2009-<?php echo date('Y'); ?> <a
-        href='<?php echo $module->brandUrl?>'><?php echo CHtml::encode(Yii::app()->name);?></a> <a href="mailto:team@yupe.ru">yupe team</a><br/>
+        href='<?php echo $module->brandUrl?>'><?php echo CHtml::encode(Yii::app()->name); ?></a> <a href="mailto:team@yupe.ru">yupe team</a><br/>
         <?php echo Yii::powered(); ?>
-        <?php $this->widget('YPerformanceStatistic');?>
+        <?php $this->widget('YPerformanceStatistic'); ?>
     </footer>
 
 </body>
