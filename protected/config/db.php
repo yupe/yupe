3f9c94df3e0a13deffa91a1ec813982e1d32905b
Yupe <aopeykin@yandex.ru>
2012-05-28 16:21:43 +0400

    При просмотре новость выводилась 3 раза

--- protected/config/db.php
+++ protected/config/db.php
@@ -3,7 +3,7 @@
   'class' => 'CDbConnection',
   'connectionString' => 'mysql:host=localhost;dbname=yupe',
   'username' => 'root',
-  'password' => '',
+  'password' => 'root',
   'emulatePrepare' => true,
   'charset' => 'utf8',
   'enableParamLogging' => 1,
