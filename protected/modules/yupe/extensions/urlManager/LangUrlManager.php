893a40fe348fcd92f6690a536358882da5cbd0fc
Alexander Tischenko <tsm@glavset.ru>
2012-06-18 18:40:42 +0400

    Поддержка мультиязычности: Правки выбора языка - теперь переходит на ту же страницу

--- protected/modules/yupe/extensions/urlManager/LangUrlManager.php
+++ protected/modules/yupe/extensions/urlManager/LangUrlManager.php
@@ -1,71 +1,64 @@
 <?php
-/**
- * Description of LangUrlManager
- *
- * @author Ekstazi
- * @ver 1.2
+/*
+ * LangUrlManager - альтернативный менеджер урлов с поддержкой языков
+ * при инициализации добавляет к существующим правилам маршрутизации
+ * правила для выбора языков в началае пути.
+ * Т.о. /page/<slug> к примеру становится /<language>/page/<slug>.
+ * Добавленный параметр используется в LanguageBehavior
  */
 class LangUrlManager extends CUrlManager{
     public $languages;
-    public $langParam='lang';
+    public $langParam='language';
 
     public function init()
     {
-	// Получаем из настроек доступные языки
-	$yupe = Yii::app()->getModule('yupe');
+        // Получаем из настроек доступные языки
+        $yupe = Yii::app()->getModule('yupe');
         $this-> languages = explode(",",$yupe->availableLanguages);
-	$this-> languages || ($this-> languages=array('ru'));
 
-	// Добавляем правила для обработки языков
-	$r= array();
-	foreach( $this-> rules as $rule=>$p )
-		$r[(($rule[0]=='/')?'/<lang:\w{2}>':'<lang:\w{2}>/').$rule]=$p;
-	$this-> rules = array_merge($r, $this->rules);
+        // Если указаны - добавляем правила для обработки, иначе ничего не трогаем вообще
+        if (is_array($this->languages))
+        {
+            // Добавляем правила для обработки языков
+            $r= array();
 
-	$p = parent::init();
- 	$this->processRules();
-	return $p;
+            foreach( $this-> rules as $rule=>$p )
+                $r[(($rule[0]=='/')?('/<'.$this->langParam.':\w{2}>'):('<'.$this->langParam.':\w{2}>/')).$rule]=$p;
 
-    }
+            $this-> rules = array_merge($r, $this->rules);
 
-    public function parsePathInfo($pathInfo)
-    {
-        parent::parsePathInfo($pathInfo);
-        $userLang=Yii::app()->getRequest()->getPreferredLanguage();
+            $p = parent::init();
+            $this->processRules();
+            return $p;
+        } else
+            return parent::init();
 
-        // Если язык передали через запрос
-        if(isset($_GET[$this->langParam])&&in_array($_GET[$this->langParam],$this->languages))
-            Yii::app()->language=$_GET[$this->langParam];
-        // иначе попробуем угадать какой нужен
-        elseif(in_array($userLang,$this->languages)) {
-            Yii::app()->language=$userLang;
-        // иначе первый в списке
-        }else Yii::app()->language=$this->languages[0];
+
     }
 
     public function createUrl($route, $params=array(), $ampersand='&')
     {
-	
-        if(!isset($params[$this->langParam]))
-		$params[$this->langParam]=Yii::app()->language;
-	if ( Yii::app()->sourceLanguage == $params[$this->langParam] ) 
-		unset($params[$this->langParam]);
+        // Если указаны языки, дописываем указанны язык
+	    if (is_array($this->languages))
+        {
+            // Если язык не указан - берем текущий
+            if(!isset($params[$this->langParam]))
+                $params[$this->langParam]=Yii::app()->language;
+
+            // Если указан "нативный" язык и к тому же он текущий  - делаем URL без него, т.к. он соответсвует пустому пути
+            if ( (Yii::app()->sourceLanguage == $params[$this->langParam]) && ($params[$this->langParam]==Yii::app()->language) )
+                unset($params[$this->langParam]);
+        }
         return parent::createUrl($route,$params,$ampersand);
     }
 
-    public function parseUrl($request)
+    public function getCleanUrl($url)
     {
-	if($this->getUrlFormat()===self::PATH_FORMAT)
-	{
-		$pi=$request->getPathInfo();
-		$l = substr($pi,0,2);
-		if ( (strlen($l)==2) && in_array($l,$this->languages) && ( $l==Yii::app()-> sourceLanguage ))
-		{
-			$newLocation = substr($pi,2);
-			Yii::app()-> request-> redirect($newLocation?$newLocation:"/");
-		}
-				                                        
-	}
-    	return parent::parseUrl($request);
+        if ( in_array($url,$this->languages)) return "/";
+        $r = join("|",$this->languages);
+        $url=preg_replace("/^($r)\//","",$url);
+        if ( !isset($url[0]) || ($url[0]!='/') ) $url= '/'.$url;
+        return $url;
     }
+
 }
