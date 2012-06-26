906027778e9c0b179fe69ec6e130df761963d365
Azat Galiev <AzatXaker@gmail.com>
2012-06-27 04:18:09 +0600

    Closed #202: добавил аттрибут breadcrumbs в базовый класс контроллера

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -6,4 +6,10 @@
 class Controller extends CController
 {
 
+    /**
+     * Contains data for "CBreadcrumbs" widget (navigation element on a site, 
+     * a look "Main >> Category 1 >> Subcategory 1")
+     */
+    public $breadcrumbs = array();
+
 }
