2f425a5d48554ef2a121573c172e4c3a2843a5df
adminnu <mail@lyzhenkov.ru>
2012-08-07 14:50:27 +0400

    Забытое изменение

--- protected/config/db.php
+++ protected/config/db.php
@@ -2,8 +2,8 @@
  return array (
   'class' => 'CDbConnection',
   'connectionString' => 'mysql:host=localhost;dbname=yupe',
-  'username' => 'yupe',
-  'password' => '000000',
+  'username' => 'root',
+  'password' => 'root',
   'emulatePrepare' => true,
   'charset' => 'utf8',
   'enableParamLogging' => 1,
