0b4bec76902ef64e4a8e5484ab64018c6135606d
adminnu <mail@lyzhenkov.ru>
2012-08-08 11:22:40 +0400

    Небольшая документация к виджету меню

--- protected/modules/menu/widgets/MenuWidget.php
+++ protected/modules/menu/widgets/MenuWidget.php
@@ -1,11 +1,49 @@
 <?php
+
+/**
+ * Класс MenuWidget - виджет вывода меню на страницы сайта
+ *
+ * @package menu.widgets
+ * @author yupe team
+ * @link http://yupe.ru
+ */
+
+/**
+ * Данный плагин реализует вывод меню модуля Menu.
+ * 
+ * Подключение виджета:
+ * <?php
+ * $this->widget('application.modules.menu.widgets.MenuWidget', array(
+ *     'name' => 'top-menu',
+ *     'id' => 'myslidemenu',
+ *     'params' => array('hideEmptyItems' => true),
+ *     'htmlOptions' => array('class' => 'jqueryslidemenu'),
+ * ));
+ * ?>
+ */
+ 
 class MenuWidget extends YWidget
 {
+    /**
+     * @var string данный парамметр указывает уникальный код выводимого меню.
+     */
     public $name;
+    /**
+     * @var string данный парамметр указывает начиная с id какого родителя начинать вывод меню, по умолчанию 0, корень меню.
+     */
     public $parent_id = 0;
 
-    public $id;
+    /**
+     * @var array парамметры виджета zii.widgets.CMenu.
+     */
     public $params = array();
+    /**
+     * @var integer данная переменная задает id блока div оборачивающего меню.
+     */
+    public $id;
+    /**
+     * @var array htmlOptions тега div.
+     */
     public $htmlOptions = array();
 
     public function init()
