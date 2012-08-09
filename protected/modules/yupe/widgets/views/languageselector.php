fa37fe9e58f62e475074de1d1f2edb400e9aafcd
adminnu <mail@lyzhenkov.ru>
2012-08-09 13:07:29 +0400

    closed #231

--- protected/modules/yupe/widgets/views/languageselector.php
+++ protected/modules/yupe/widgets/views/languageselector.php
@@ -1,20 +1,15 @@
 <?php $currentLanguage = Yii::app()->language; ?>
-<?php $cp = Yii::app()->urlManager->getCleanUrl(Yii::app()->request->getPathInfo());?>
-<?php $i = 1;?>
-<?php $langs = explode(',',Yii::app()->getModule('yupe')->availableLanguages);?>
-
-    <li class="langs">
-			<?php foreach($langs as $lang):?>
-			    <?php $i++;?>
-			    <?php if($currentLanguage == $lang):?>
-			    <span>
-				   <?php echo strtoupper($lang);?> 
-			    </span>
-				<?php else:?>
-				    <?php echo CHtml::link(strtoupper($lang),Yii::app()->baseUrl.'/index.php/'.$lang.$cp);?>
-				<?php endif?>
-				<?php if($i == count($langs)):?>|<?php endif;?>
-			<?php endforeach;?>			
-	</li>
-
-
+<?php $cp = Yii::app()->urlManager->getCleanUrl(Yii::app()->request->getPathInfo()); ?>
+<?php $i = 1; ?>
+<?php $langs = explode(',', Yii::app()->getModule('yupe')->availableLanguages); ?>
+<div style="font-size: 11px">
+    <?php foreach($langs as $lang): ?>
+        <?php $i++; ?>
+        <?php if($currentLanguage == $lang): ?>
+            <span><?php echo strtoupper($lang); ?></span>
+        <?php else:?>
+            <?php echo CHtml::link(strtoupper($lang), Yii::app()->baseUrl . '/index.php/' . $lang . $cp);?>
+        <?php endif?>
+            <?php if($i == count($langs)): ?>|<?php endif; ?>
+    <?php endforeach;?>
+</div>
