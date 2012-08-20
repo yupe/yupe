f0fe5ff5fe4a80152a19e7304304f24140ccdee6
Anton Cherepanov <davetoxa@gmail.com>
2012-08-21 02:08:53 +0500

    Добавление linkOptions - target => _blank

--- protected/modules/yupe/widgets/views/yupeDownload.php
+++ protected/modules/yupe/widgets/views/yupeDownload.php
@@ -7,8 +7,8 @@
         $this->widget('zii.widget.CMenu', array(
             'items' => array(
                 array('label' => 'СКАЧАТЬ ЮПИ', 'url' => 'https://github.com/yupe/yupe/tarball/master', 'active' => true),
-                array('label' => 'Отослать ошибку', 'url' => 'https://github.com/yupe/yupe/issues/new'),
-                array('label' => 'Юпи в твиттере', 'url' => 'https://twitter.com/YupeCms'),
+                array('label' => 'Отослать ошибку', 'url' => 'https://github.com/yupe/yupe/issues/new', 'linkOptions'=>array('target'=>'_blank')),
+                array('label' => 'Юпи в твиттере', 'url' => 'https://twitter.com/YupeCms',),
             ),
         ));
         ?>
