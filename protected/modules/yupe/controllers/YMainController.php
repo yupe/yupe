df9d32e54209c70907cd162f129336fdb2178454
Anton Cherepanov <davetoxa@gmail.com>
2012-08-21 20:58:43 +0500

    small phpdoc fix

--- protected/modules/yupe/controllers/YMainController.php
+++ protected/modules/yupe/controllers/YMainController.php
@@ -1,11 +1,6 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
- * Description of YMainController
+ * Содержит общие функции для админки и фронтенда
  *
  * @author aopeykin
  */
