205c2c18ef1712420ec010be2ecb6d996875d5c3
Alexander Tischenko <tsm@glavset.ru>
2012-08-30 09:35:23 +0400

    Переделка стилей

--- protected/modules/blog/views/blogAdmin/_form.php
+++ protected/modules/blog/views/blogAdmin/_form.php
@@ -5,6 +5,7 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     'enableClientValidation' => true,
     'type'                   => 'vertical',
     'htmlOptions'            => array('class' => 'well form-vertical'),
+    'inlineErrors'           => true,
 ));
 
 Yii::app()->clientScript->registerScript('fieldset', "
@@ -13,48 +14,49 @@ Yii::app()->clientScript->registerScript('fieldset', "
     });
 ");
 ?>
-
     <div class="alert alert-info">
         <?php echo Yii::t('blog', 'Поля, отмеченные'); ?>
         <span class="required">*</span>
         <?php echo Yii::t('blog', 'обязательны.'); ?>
     </div>
-
     <?php echo $form->errorSummary($model); ?>
-
+    <div class="wide row-fluid control-group <?php echo ($model->hasErrors('type') || $model->hasErrors('status') )? 'error' : ''; ?>">
+        <div class="span3"><?php echo $form->dropDownListRow($model, 'type', $model->getTypeList(), array('class' => 'popover-help', 'data-original-title' => $model->getAttributeLabel('type'), 'data-content' => $model->getAttributeDescription('type'))); ?></div>
+        <div class="span4"><?php echo $form->dropDownListRow($model, 'status', $model->getStatusList(), array('class' => 'popover-help', 'data-original-title' => $model->getAttributeLabel('status'), 'data-content' => $model->getAttributeDescription('status'))); ?></div>
+    </div>
     <div class="row-fluid control-group <?php echo $model->hasErrors('name') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'name', array('class' => 'span3 popover-help', 'maxlength' => 300, 'size' => 60, 'data-original-title' => $model->getAttributeLabel('name'), 'data-content' => $model->getAttributeDescription('name'))); ?>
+            <?php echo $form->textFieldRow($model, 'name',
+                array('class' => 'popover-help span7',
+                      'maxlength' => 300,
+                      'size' => 60,
+                      'data-original-title' => $model->getAttributeLabel('name'),
+                      'data-content' => $model->getAttributeDescription('name'),
+                )
+            ); ?>
     </div>
     <div class="row-fluid control-group <?php echo $model->hasErrors('slug') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'slug', array('class' => 'span3 popover-help', 'maxlength' => 150, 'size' => 60, 'data-original-title' => $model->getAttributeLabel('slug'), 'data-content' => $model->getAttributeDescription('slug'))); ?>
+            <?php echo $form->textFieldRow($model, 'slug', array('class' => 'popover-help span7', 'maxlength' => 150, 'size' => 60, 'data-original-title' => $model->getAttributeLabel('slug'), 'data-content' => $model->getAttributeDescription('slug'))); ?>
     </div>
     <div class="row-fluid control-group <?php echo $model->hasErrors('icon') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'icon', array('class' => 'span3 popover-help', 'maxlength' => 300, 'size' => 60, 'data-original-title' => $model->getAttributeLabel('icon'), 'data-content' => $model->getAttributeDescription('icon'))); ?>
+            <?php echo $form->textFieldRow($model, 'icon', array('class' => 'popover-help span7', 'maxlength' => 300, 'size' => 60, 'data-original-title' => $model->getAttributeLabel('icon'), 'data-content' => $model->getAttributeDescription('icon'))); ?>
     </div>
     <div class="row-fluid control-group <?php echo $model->hasErrors('description') ? 'error' : ''; ?>">
-        <?php echo $form->labelEx($model, 'publish_date'); ?>
-        <?php $this->widget($this->module->editor, array(
-            'model' => $model,
-            'attribute' => 'description',
-            'options' => array(
-                'toolbar' => 'main',
-                'imageUpload' => Yii::app()->baseUrl . '/index.php/yupe/backend/AjaxFileUpload/',
-            ),
-            'htmlOptions' => array('rows' => 20, 'cols' => 6),
-        )); ?>
-        <br />
-        <?php $this->widget('bootstrap.widgets.TbLabel', array('type' => 'info', 'label' => $model->getAttributeDescription('description'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('type') ? 'error' : ''; ?>">
-        <?php echo $form->dropDownListRow($model, 'type', $model->getTypeList(), array('class' => 'span3 popover-help', 'data-original-title' => $model->getAttributeLabel('type'), 'data-content' => $model->getAttributeDescription('type'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('status') ? 'error' : ''; ?>">
-        <?php echo $form->dropDownListRow($model, 'status', $model->getStatusList(), array('class' => 'span3 popover-help', 'data-original-title' => $model->getAttributeLabel('status'), 'data-content' => $model->getAttributeDescription('status'))); ?>
+        <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('description') ?>' data-content='<?php echo $model->getAttributeDescription('description')?>'>
+            <?php echo $form->labelEx($model, 'description'); ?>
+            <?php $this->widget($this->module->editor, array(
+                'model' => $model,
+                'attribute' => 'description',
+                'options' => array(
+                    'toolbar' => 'main',
+                    'imageUpload' => Yii::app()->baseUrl . '/index.php/yupe/backend/AjaxFileUpload/',
+                ),
+                'htmlOptions' => array('rows' => 20, 'cols' => 6),
+            )); ?>
+        </div>
     </div>
     <?php $this->widget('bootstrap.widgets.TbButton', array(
         'buttonType' => 'submit',
         'type'       => 'primary',
         'label'      => $model->isNewRecord ? Yii::t('blog', 'Добавить блог') : Yii::t('blog', 'Сохранить блог'),
     )); ?>
-
 <?php $this->endWidget(); ?>
