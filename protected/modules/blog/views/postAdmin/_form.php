7b0e3b3725654d966e2c1f80acb38fb2ae70e0cd
adminnu <mail@lyzhenkov.ru>
2012-09-05 15:01:39 +0400

    small fix

--- protected/modules/blog/views/postAdmin/_form.php
+++ protected/modules/blog/views/postAdmin/_form.php
@@ -38,8 +38,7 @@ Yii::app()->clientScript->registerScript('fieldset', "
         <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('publish_date'); ?>' data-content='<?php echo $model->getAttributeDescription('publish_date'); ?>'>
             <?php echo $form->labelEx($model, 'publish_date'); ?>
             <?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-                'attribute' => 'publish_date',
-                'name' => class_name($model).'[publish_date]',
+                'name' => get_class($model).'[publish_date]',
                 'value' => date('d-m-Y', $model->publish_date),
                 'language' => Yii::app()->language,
                 'htmlOptions' => array("class" => "span7"),
