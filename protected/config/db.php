b592727a6d8b569d8279daac3e5eb04b3fd8674a
Yupe <aopeykin@yandex.ru>
2012-09-07 11:39:04 +0400

    установка

---
+++ protected/config/db.php
@@ -0,0 +1,14 @@
+<?php
+ return array (
+  'class' => 'CDbConnection',
+  'connectionString' => 'mysql:host=localhost;dbname=yupe',
+  'username' => 'root',
+  'password' => 'root',
+  'emulatePrepare' => true,
+  'charset' => 'utf8',
+  'enableParamLogging' => 1,
+  'enableProfiling' => 1,
+  'schemaCachingDuration' => 108e000,
+  'tablePrefix' => '',
+) ;
+?>
