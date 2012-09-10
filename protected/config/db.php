526eb6b22023b90f26ff93b60c35d77f83d6ce1b
Yupe <aopeykin@yandex.ru>
2012-09-10 14:56:06 +0400

    мелкие правки + автоматическое создание каталога assets при установке

--- protected/config/db.php
+++ protected/config/db.php
@@ -1,14 +1,14 @@
 <?php
  return array (
   'class' => 'CDbConnection',
-  'connectionString' => 'mysql:host=localhost;dbname=yupe',
+  'connectionString' => 'mysql:host=localhost;port=3306;dbname=yupe',
   'username' => 'root',
   'password' => 'root',
   'emulatePrepare' => true,
   'charset' => 'utf8',
   'enableParamLogging' => 1,
   'enableProfiling' => 1,
-  'schemaCachingDuration' => 108e000,
+  'schemaCachingDuration' => 108000,
   'tablePrefix' => '',
 ) ;
 ?>
