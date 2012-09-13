e7d3f690d2e5e7f134df7d45fe25c2910dbb52e9
adminnu <mail@lyzhenkov.ru>
2012-09-13 17:17:14 +0400

    Исправления и рефакторинг по последнему пулу.

--- protected/modules/yupe/extensions/yupe/gii/yupe/YupeCode.php
+++ protected/modules/yupe/extensions/yupe/gii/yupe/YupeCode.php
@@ -39,19 +39,19 @@ class YupeCode extendS CrudCode
             return "\$form->checkBoxRow(\$model, '{$column->name}', array('class' => 'popover-help', 'data-original-title' => \$model->getAttributeLabel('{$column->name}'), 'data-content' => \$model->getAttributeDescription('{$column->name}')))";
         else if (stripos($column->dbType, 'text') !== false)
             return "\$form->textAreaRow(\$model, '{$column->name}', array('class' => 'span5 popover-help', 'rows' => 6, 'cols' => 50, 'data-original-title' => \$model->getAttributeLabel('{$column->name}'), 'data-content' => \$model->getAttributeDescription('{$column->name}')))";
-        else if ($column->isForeignKey) {
+        else if ($column->isForeignKey)
+        {
             $relations = CActiveRecord::model($modelClass)->relations();
-            foreach($relations as $key => $relation)
-            {
-                if($relation[0] == 'CBelongsToRelation' && $relation[2] == $column->name
-                    )
-                    {
-                        $relationModel = CActiveRecord::model($relation[1]);
-                        $suggestedName = $this->suggestName($relationModel->tableSchema->columns)->name;
-                        return "\$form->dropDownList(\$model, $relation[2], CHtml::listData($relation[1]::model()->findAll(), 'id', $suggestedName))";
+            foreach ($relations as $key => $relation)
+                if ($relation[0] == 'CBelongsToRelation' && $relation[2] == $column->name)
+                {
+                    $relationModel = CActiveRecord::model($relation[1]);
+                    $suggestedName = $this->suggestName($relationModel->tableSchema->columns)->name;
+                    return "\$form->dropDownListRow(\$model, '{$relation[2]}', CHtml::listData($relation[1]::model()->findAll(), 'id', $suggestedName))";
                 }
-            }
-        } else {
+        } 
+        else
+        {
             if (preg_match('/^(password|pass|passwd|passcode)$/i', $column->name))
                 $inputField = 'passwordFieldRow';
             else
@@ -60,7 +60,7 @@ class YupeCode extendS CrudCode
             if ($column->type !== 'string' || $column->size === null)
                 return "\$form->{$inputField}(\$model, '{$column->name}', array('class' => 'span3 popover-help', 'size' => 60, 'maxlength' => 60, 'data-original-title' => \$model->getAttributeLabel('{$column->name}'), 'data-content' => \$model->getAttributeDescription('{$column->name}')))";
             else
-                return "\$form->{$inputField}(\$model, '{$column->name}', array('class' => 'span3 popover-help', 'size' => 60, 'maxlength' => $column->size, 'data-original-title' => \$model->getAttributeLabel('{$column->name}'), 'data-content' => \$model->getAttributeDescription('{$column->name}')))";
+                return "\$form->{$inputField}(\$model, '{$column->name}', array('class' => 'span3 popover-help', 'size' => 60, 'maxlength' => {$column->size}, 'data-original-title' => \$model->getAttributeLabel('{$column->name}'), 'data-content' => \$model->getAttributeDescription('{$column->name}')))";
         }
     }
 
@@ -69,31 +69,36 @@ class YupeCode extendS CrudCode
         return mb_strtoupper(mb_substr($word, 0, 1, 'UTF-8'), 'UTF-8') . mb_substr(mb_convert_case($word, MB_CASE_LOWER, 'UTF-8'), 1, mb_strlen($word), 'UTF-8');
     }
 
-    //определяем первый осмысленный текстовый столбец в таблице. Как правило это что-то вроде blahblahName, 
-    //который можно в будущем использовать в качестве текстового значения для списков в связанных таблицах
-    public function suggestName($columns) {
+    // Определяем первый осмысленный текстовый столбец в таблице. Как правило это что-то вроде blahblahName, 
+    // который можно в будущем использовать в качестве текстового значения для списков в связанных таблицах.
+    public function suggestName($columns)
+    {
         $j = 0;
-        foreach ($columns as $column) {
-                if (!$column->isForeignKey
-                                && !$column->isPrimaryKey
-                                && $column->type != 'INT'
-                                && $column->type != 'INTEGER'
-                                && $column->type != 'BOOLEAN'
-                                && substr_count(strtolower($column->name), 'name') > 0) {
-                        $num = $j;
-                        break;
-                }
-                $j++;
+        foreach ($columns as $column)
+        {
+            if (!$column->isForeignKey &&
+                !$column->isPrimaryKey &&
+                $column->type != 'INT' &&
+                $column->type != 'INTEGER' &&
+                $column->type != 'BOOLEAN' &&
+                substr_count(strtolower($column->name), 'name') > 0
+            )
+            {
+                $num = $j;
+                break;
+            }
+            $j++;
         }
 
         for ($i = 0; $i < $j; $i++)
-                next($columns);
+            next($columns);
 
         if (is_object(current($columns)))
-                return current($columns);
-        else {
-                $column = reset($columns);
-                return $column;
+            return current($columns);
+        else
+        {
+            $column = reset($columns);
+            return $column;
         }
     }
 }
