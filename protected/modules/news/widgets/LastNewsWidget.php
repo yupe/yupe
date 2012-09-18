5af8325f255152901c7cd1496d0e78aedbacad14
Alexander Tischenko <tsm@glavset.ru>
2012-09-18 12:59:19 +0400

    Новости - выбор по категориям, последние N

--- protected/modules/news/widgets/LastNewsWidget.php
+++ protected/modules/news/widgets/LastNewsWidget.php
@@ -3,20 +3,25 @@ class LastNewsWidget extends YWidget
 {
     public $count = 5;
 
+    /** @var $categories mixed Список категорий, из которых выбирать новости. NULL - все */
+    public $categories = null;
+
     public function run()
     {
+        $criteria = new CDbCriteria();
+        $criteria->limit = $this->count;
+        $criteria->order = 'date DESC';
+
+        if($this->categories)
+            if(is_array($this->categories))
+                $criteria->addInCondition('category_id',$this->categories);
+            else
+                $criteria->compare('category_id',$this->categories);
 
         if ( $this->controller->isMultilang() )
-            $news = News::model()->published()->language(Yii::app()->language)->cache($this->cacheTime)->findAll(array(
-                'limit' => $this->count,
-                'order' => 'date DESC',
-            ));
+            $news = News::model()->published()->language(Yii::app()->language)->cache($this->cacheTime)->findAll($criteria);
         else
-            $news = News::model()->published()->cache($this->cacheTime)->findAll(array(
-                'limit' => $this->count,
-                'order' => 'date DESC',
-            ));
-
+            $news = News::model()->published()->cache($this->cacheTime)->findAll($criteria);
 
         $this->render('news', array('models' => $news));
     }
