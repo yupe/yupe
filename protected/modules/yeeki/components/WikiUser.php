e46533b96e6936997bc827ae92f708ab262044da
adminnu <mail@lyzhenkov.ru>
2012-09-26 16:38:28 +0400

    Рефакторинг yeeki

--- protected/modules/yeeki/components/WikiUser.php
+++ protected/modules/yeeki/components/WikiUser.php
@@ -4,25 +4,25 @@
  */
 class WikiUser implements IWikiUser
 {
-	/**
-	 * @param mixed $id ID of the user.
-	 *
-	 * @return string name of the user.
-	 */
-	public function getNameById($id)
-	{
-		$user = User::model()->findByPk($id);
-		return $user->name;
-	}
+    /**
+     * @param mixed $id ID of the user.
+     *
+     * @return string name of the user.
+     */
+    public function getNameById($id)
+    {
+        $user = User::model()->findByPk($id);
+        return $user->name;
+    }
 
-	/**
-	 * @param int $id ID of the user.
-	 *
-	 * @return string email of the user.
-	 */
-	public function getEmailById($id)
-	{
-		$user = User::model()->findByPk($id);
-		return $user->email;
-	}
+    /**
+     * @param int $id ID of the user.
+     *
+     * @return string email of the user.
+     */
+    public function getEmailById($id)
+    {
+        $user = User::model()->findByPk($id);
+        return $user->email;
+    }
 }
