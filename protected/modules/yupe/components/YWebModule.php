66fef0cc0b36eb593d72e879956ebf40c4e748c4
Yupe <aopeykin@yandex.ru>
2012-09-27 23:09:45 +0400

    Revert "попытался пофиксить http://yupe.ru/talk/viewtopic.php?id=45"
    This reverts commit 98bb3fd10dcd0e7dac78f2b7a3183ef5ed534ef9.

--- protected/modules/yupe/components/YWebModule.php
+++ protected/modules/yupe/components/YWebModule.php
@@ -215,7 +215,7 @@ abstract class YWebModule extends CWebModule
         try
         {
             // инициализация модуля
-            $settings = Settings::model()->cache($this->coreCacheTime)->findAll('module_id = :module_id', array('module_id' => $this->getId()));
+            $settings = Settings::model()->cache($this->coreCacheTime)->findAll('module_id = :module_id', array('module_id' => $this->id));
         }
         catch (CDbException $e)
         {
