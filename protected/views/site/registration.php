8bf5698cd85ed3639fa253c2c09069a909f4f67b
Yupe <aopeykin@yandex.ru>
2012-10-05 23:41:27 +0400

    чистка + маркетинг =)))

--- protected/views/site/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $this->breadcrumbs = array(
-        'Регистрация',
-    );
-?>
-
-<h1>Регистрация нового пользователя</h1>
-
-<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
---
+++ protected/views/site/welcome.php
@@ -0,0 +1,20 @@
+<h1>Поздравляем!</h1>
+<p> Ваш очередной сайт на <a href='http://yupe.ru/?from=mainpage'>Юпи!</a> готов к работе!</p>
+<p> Для управления содержимым сайта, пожалуйста, перейдите в <?php echo CHtml::link('панель управления',array('/yupe/backend/'));?>.</p>
+<p> При возникновении вопросов или проблем - обращайтесь на наш <a href='http://yupe.ru/talk/?from=mainpage'>форум</a>.</p>
+<p>Читайте нас в <a href='http://twitter.com/yupecms'>twitter</a></p>
+<p><a href='http://yupe.ru/site/page/view/help/?from=mainpage'>Желаете помочь проекту ?</a></p>
+
+
+<p><b>По вопросам коммерческой поддержки - <a href='http://yupe.ru/feedback/contact/?from=mainpage'>напишите нам</a> !</b></p>
+
+<div style="float:left">
+    <div style="float:left;padding-right:5px">
+        <?php
+            $this->widget('application.modules.social.widgets.ysc.yandex.YandexShareApi', array(
+              'type' => 'button',
+              'services' => 'all',
+            ));
+        ?>
+    </div>
+</div>
