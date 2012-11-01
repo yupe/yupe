9ca96096418d2964438a27f4dd0b61f12714d876
adminnu <mail@lyzhenkov.ru>
2012-11-01 14:55:35 +0400

    Doc bootstrap theme

--- themes/backend_bootstrap/BootstrapTheme.php
+++ themes/backend_bootstrap/BootstrapTheme.php
@@ -1,8 +1,18 @@
 <?php
-    Yii::app()->setComponent('bootstrap', Yii::createComponent(array(
-        'class' => $themeBase.'.extensions.bootstrap.components.Bootstrap',
-    )));
+/**
+ *  BootstrapTheme
+ *
+ *  Тема bootstrap, используется приложение Yii-Bootstrap http://www.yiiframework.com/extension/bootstrap/
+ *
+ * @author Yupe Team
+ * @link   http://yupe.ru
+ * @version 1.1.0
+ *
+ */
+ 
+Yii::app()->setComponent('bootstrap', Yii::createComponent(array(
+    'class' => $themeBase . '.extensions.bootstrap.components.Bootstrap',
+)));
 
-    Yii::setPathOfAlias('bootstrap', Yii::app()->theme->basePath.'/extensions/bootstrap');
-    Yii::app()->preload[] = 'bootstrap';
-?>
+Yii::setPathOfAlias('bootstrap', Yii::app()->theme->basePath . '/extensions/bootstrap');
+Yii::app()->preload[] = 'bootstrap';
