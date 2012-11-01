3cc1361cfc04bdb55c9144383ab283be4bd7bcf2
adminnu <mail@lyzhenkov.ru>
2012-11-01 14:45:22 +0400

    Все представления bootstrap-темы перенесены в модули.

--- themes/backend_bootstrap/views/contest/defaults/addImage.php
+++
@@ -1,5 +0,0 @@
-<h1><?php echo Yii::t('contest', 'Добавление изображения в конкурс');?> <br/>
-    "<?php echo $contest->name;?>"</h1>
-
-<?php $this->renderPartial('_add_foto_form', array('model' => $model)); ?>
-
--- themes/backend_bootstrap/views/gallery_/default/addImage.php
+++
@@ -1,5 +0,0 @@
-<h1><?php echo Yii::t('gallery', 'Добавление изображения в галерею');?> <br/>
-    "<?php echo $gallery->name;?>"</h1>
-
-<?php $this->renderPartial('_add_foto_form', array('model' => $model)); ?>
-
