64aa12fdd4a301417058e3ba6b4dafd7fc5a442d
xoma <aopeykin@yande.ru>
2012-11-12 19:42:21 +0400

    module "comment" migrated to bootstrap

--- protected/modules/comment/views/default/create.php
+++ protected/modules/comment/views/default/create.php
@@ -1,16 +1,16 @@
 <?php
 $this->breadcrumbs = array(
-    $this->getModule('category')->getCategory() => array(''),
+    $this->getModule('comment')->getCategory() => array(''),
     Yii::t('comment', 'Комментарии') => array('admin'),
     Yii::t('comment', 'Добавление'),
 );
 
-$this->menu = array(
-    array('label' => Yii::t('comment', 'Список комментариев'), 'url' => array('index')),
-    array('label' => Yii::t('comment', 'Управление комментариями'), 'url' => array('admin')),
+$this->menu=array(
+    array('icon'=> 'list-alt', 'label' => Yii::t('comment','Список комментариев'),'url'=>array('/comment/default/admin')),
+    array('icon'=> 'plus-sign', 'label' => Yii::t('comment','Добавить комментарий'),'url'=>array('/comment/default/create')),
 );
 ?>
 
-<h1><?php echo Yii::t('comment', 'Добавление комментария'); ?></h1>
+<h1><?php echo Yii::t('comment', 'Комментарии'); ?> <small><?php echo Yii::t('comment','добавление');?></small></h1>
 
 <?php echo $this->renderPartial('_form', array('model' => $model)); ?>
--- protected/modules/comment/views/default/view.php
+++ protected/modules/comment/views/default/view.php
@@ -1,23 +1,23 @@
 <?php
 $this->breadcrumbs = array(
-    $this->getModule('category')->getCategory() => array(''),
+    $this->getModule('comment')->getCategory() => array(''),
     Yii::t('comment', 'Комментарии') => array('admin'),
     $model->id,
 );
 
 $this->menu = array(
-    array('label' => Yii::t('comment', 'Список комментариев'), 'url' => array('index')),
-    array('label' => Yii::t('comment', 'Добавить комментарий'), 'url' => array('create')),
-    array('label' => Yii::t('comment', 'Редактировать комментарий'), 'url' => array('update', 'id' => $model->id)),
-    array('label' => Yii::t('comment', 'Удалить комментарий'), 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Подтверждаете удаление ?')),
-    array('label' => Yii::t('comment', 'Управление комментариями'), 'url' => array('admin')),
+    array('icon'=> 'list-alt','label' => Yii::t('comment', 'Список комментариев'), 'url' => array('admin')),
+    array('icon'=> 'plus-sign','label' => Yii::t('comment', 'Добавить комментарий'), 'url' => array('create')),
+    array('icon'=> 'pencil','label' => Yii::t('comment', 'Редактировать комментарий'), 'url' => array('update', 'id' => $model->id)),
+    array('icon'=> 'remove','label' => Yii::t('comment', 'Удалить комментарий'), 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Подтверждаете удаление ?')),
 );
 ?>
 
-<h1><?php echo Yii::t('comment', 'Просмотр комментария');?>
-    #<?php echo $model->id; ?></h1>
+<div class="page-header">
+    <h1><?php echo Yii::t('comment','Просмотр комментария');?><br /><small style='margin-left:-10px;'>&laquo;<?php echo  $model->id; ?>&raquo;</small></h1>
+</div>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
+<?php $this->widget('bootstrap.widgets.TbDetailView', array(
     'data' => $model,
     'attributes' => array(
         'id',
