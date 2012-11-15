da550b0235b9d9e6d3390bd5aeb3a8fe37e4a923
adminnu <mail@lyzhenkov.ru>
2012-11-15 13:07:03 +0400

    Обновлен yii-bootstrap, добавлен yiibooster #357

--- themes/backend_bootstrap/BootstrapTheme.php
+++ themes/backend_bootstrap/BootstrapTheme.php
@@ -2,11 +2,12 @@
 /**
  *  BootstrapTheme
  *
- *  Тема bootstrap, используется приложение Yii-Bootstrap http://www.yiiframework.com/extension/bootstrap/
+ *  Тема bootstrap, используется приложение Yii-Bootstrap http://www.yiiframework.com/extension/bootstrap/ (1.2.0)
+ *  Тема booster, используется приложение YiiBooster http://yii-booster.clevertech.biz/index.html (1.0.3)
  *
  * @author Yupe Team
  * @link   http://yupe.ru
- * @version 1.1.0
+ * @version 1.2.0
  *
  */
  
