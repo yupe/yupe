4f6420e095517717b4ecf3123aabef57389c722f
adminnu <mail@lyzhenkov.ru>
2012-11-28 16:45:33 +0400

    comment small fix bootstrap

--- protected/modules/comment/views/default/_search.php
+++ protected/modules/comment/views/default/_search.php
@@ -1,9 +1,17 @@
-<?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+<?php
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     'action'      => Yii::app()->createUrl($this->route),
     'method'      => 'get',
-    'htmlOptions' => array('class' => 'well search-form'),
-)); ?>
+    'type'        => 'vertical',
+    'htmlOptions' => array('class' => 'well'),
+));
 
+Yii::app()->clientScript->registerScript('fieldset', "
+    $('document').ready(function () {
+        $('.popover-help').popover({ trigger : 'hover', delay : 500 });
+    });
+");
+?>
     <fieldset class="inline">
         <?php echo $form->textFieldRow($model, 'id', array('size' => 10, 'maxlength' => 10)); ?> 
         <?php echo $form->textFieldRow($model, 'model', array('size' => 60, 'maxlength' => 150)); ?>
@@ -15,14 +23,13 @@
         <?php echo $form->textAreaRow($model, 'text', array('rows' => 6, 'cols' => 50)); ?>
         <?php echo $form->dropDownListRow($model, 'status', $model->statusList); ?>
         <?php echo $form->textFieldRow($model, 'ip', array('size' => 20, 'maxlength' => 20)); ?>
-        <div class="form-actions">
-            <?php $this->widget('bootstrap.widgets.TbButton', array(
-                'buttonType'  => 'submit',
-                'type'        => 'primary',
-                'encodeLabel' => false,
-                'label'       => '<i class="icon-search icon-white"></i> ' . Yii::t('mail', 'Искать'),
-            )); ?>
-        </div>
     </fieldset>
+
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'type'        => 'primary',
+        'encodeLabel' => false,
+        'buttonType'  => 'submit',
+        'label'       => '<i class="icon-search icon-white">&nbsp;</i> ' . Yii::t('comment', 'Искать комментарии'),
+    )); ?>
 
 <?php $this->endWidget(); ?>
