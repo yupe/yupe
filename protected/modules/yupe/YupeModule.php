acf936eb6fec476d25973caaa54b9e039df92882
adminnu <mail@lyzhenkov.ru>
2012-12-10 15:52:39 +0400

    revert 0.4 reliase

--- protected/modules/yupe/YupeModule.php
+++ protected/modules/yupe/YupeModule.php
@@ -47,7 +47,7 @@ class YupeModule extends YWebModule
 
     public function getVersion()
     {
-        return Yii::t('yupe', '0.5 (dev)');
+        return Yii::t('yupe', '0.4');
     }
 
     public function checkSelf()
