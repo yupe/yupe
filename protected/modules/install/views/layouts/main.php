c04e38a04449164506976c8a68648f92b64fa91f
andrey <andrey@andrey-home.(none)>
2012-12-11 23:28:31 +0400

    исправление запуска установки

--- protected/modules/install/views/layouts/main.php
+++ protected/modules/install/views/layouts/main.php
@@ -3,13 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php
-        $module = Yii::app()->getModule('yupe');
-        $webPath = Yii::app()->assetManager->publish($module->basePath . '/web/');
-        Yii::app()->clientScript->registerScriptFile($webPath . '/yupeAdmin.js');
-    ?>
-
     <title><?php echo CHtml::encode(Yii::app()->name); ?> <?php echo CHtml::encode($this->pageTitle); ?></title>
     <link rel="stylesheet" type="text/css"  href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css"/>
 </head>
