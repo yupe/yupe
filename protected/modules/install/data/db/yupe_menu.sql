ba0d169df5fb614c27746bade11b73367c72b987
adminnu <mail@lyzhenkov.ru>
2012-12-12 01:44:35 +0400

    Fixed #393: исправлены значение адреса в дампе для страниц

--- protected/modules/install/data/db/yupe_menu.sql
+++ protected/modules/install/data/db/yupe_menu.sql
@@ -7,10 +7,10 @@ INSERT INTO `yupe_menu` (`id`, `name`, `code`, `description`, `status`) VALUES
 INSERT INTO `yupe_menu_item` (`id`, `parent_id`, `menu_id`, `title`, `href`, `class`, `title_attr`, `before_link`, `after_link`, `target`, `rel`, `condition_name`, `condition_denial`, `sort`, `status`) VALUES
 (1, 0, 1, 'Главная', '/site/index', '', 'Главная страница сайта', '', '', '', '', '', 0, 1, 1),
 (2, 0, 1, 'Блоги', '/blog/blog/index/', '', 'Блоги', '', '', '', '', '', 0, 2, 1),
-(3, 0, 1, 'О проекте', '/site/page/view/about/', '', 'О проекте', '', '', '', '', '', 0, 3, 1),
+(3, 0, 1, 'О проекте', '/site/page?view=about', '', 'О проекте', '', '', '', '', '', 0, 3, 1),
 (4, 0, 1, 'Пользователи', '/user/people/index/', '', 'Пользователи', '', '', '', '', '', 0, 4, 1),
 (5, 0, 1, 'Социальные виджеты', '/site/social/', '', 'Социальные виджеты', '', '', '', '', '', 0, 5, 0),
-(6, 0, 1, 'Помощь проекту', '/site/page/view/help/', '', 'Помощь проекту', '', '', '', '', '', 0, 6, 1),
+(6, 0, 1, 'Помощь проекту', '/site/page?view=help', '', 'Помощь проекту', '', '', '', '', '', 0, 6, 1),
 (7, 0, 1, 'Контакты', '/feedback/contact/', '', 'Контакты', '', '', '', '', '', 0, 7, 1),
 (8, 0, 1, 'Wiki', '/wiki/default/index/', '', 'Wiki', '', '', '', '', '', 0, 8, 0),
 (9, 0, 1, 'Войти', '/user/account/login', '', 'Войти на сайт', '', '', '', '', 'isAuthenticated', 1, 9, 1),
