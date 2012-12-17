026a19d69a586eede6e6164287f107e5ab7d9a31
yupe <aopeykin@yandex.ru>
2012-12-17 22:44:22 +0400

    db.php

--- protected/config/db.php
+++ protected/config/db.php
@@ -1,9 +1,9 @@
 <?php
  return array (
   'class' => 'CDbConnection',
-  'connectionString' => 'mysql:host=localhost;port=3306;dbname=yupe',
-  'username' => 'root',
-  'password' => 'root',
+  'connectionString' => 'mysql:host=localhost;port=3306;dbname=<db.name>',
+  'username' => '<db.user>',
+  'password' => '<db.pass>',
   'emulatePrepare' => true,
   'charset' => 'utf8',
   'enableParamLogging' => 0,
