34a2a8b742b0f7270c3dba7f1d01f53308daf9c4
adminnu <mail@lyzhenkov.ru>
2012-12-19 15:26:27 +0400

    remove bug file

--- catalog.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'module'   => array(
-        'class' => 'application.modules.catalog.CatalogModule',
-    ),
-    'import'    => array(),
-    'component' => array(),
-    'rules'     => array(),
-);
