4490f36fe6237e6e13c4f487c9e839666a24b708
adminnu <mail@lyzhenkov.ru>
2012-12-19 15:29:47 +0400

    small fix

--- protected/config/modules/dictionary.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'module'   => array(
-        'class' => 'application.modules.dictionary.DictionaryModule',
-    ),
-    'import'    => array(),
-    'component' => array(),
-    'rules'     => array(),
-);
--- protected/config/modules/gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'module'   => array(
-        'class' => 'application.modules.gallery.GalleryModule',
-    ),
-    'import'    => array(),
-    'component' => array(),
-    'rules'     => array(),
-);
