f98130eaffc7cb5fd85e216fd067dd6b62099d75
Alexander Tischenko <tsm@glavset.ru>
2013-01-02 16:48:18 +0400

    Поле "category_id" в блоге

---
+++ protected/modules/blog/install/migrations/m121231_184100_blog_category.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Add category relations to blog
+ */
+class m121231_184100_blog_category extends CDbMigration
+{
+    public function safeUp()
+    {
+        $db = $this->getDbConnection();
+
+        // blog
+        $tableName = $db->tablePrefix.'blog';
+        $this->createIndex("blog_category_idx",$tableName,"category_id", false);
+        $this->addForeignKey("blog_category_fk",$tableName,'category_id',$db->tablePrefix.'category','id','SET NULL','CASCADE');
+    }
+
+    public function safeDown()
+    {
+        $db = $this->getDbConnection();
+        $tableName = $db->tablePrefix.'blog';
+        $this->dropForeignKey("blog_category_fk", $tableName);
+        $this->dropIndex("blog_category_idx", $tableName);
+
+    }
+}
