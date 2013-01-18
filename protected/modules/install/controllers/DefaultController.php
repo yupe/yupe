6a4148040b426a3522145c1268b0f278c6d3800f
AKulikov <tuxuls@gmail.com>
2013-01-19 00:52:47 +0200

    Revert "UPD: возможность выбора другого типа СУБД при инстале"
    This reverts commit dc515d6f350abd7c79445992a80e2ab48f1129a4.

--- protected/modules/install/controllers/DefaultController.php
+++ protected/modules/install/controllers/DefaultController.php
@@ -356,12 +356,10 @@ class DefaultController extends YBackController
             {
                 try
                 {
-                    $socket  = ($form->socket == '') ? '' : 'unix_socket=' . $form->socket . ';';
-                    $port    = ($form->port == '') ? '' : 'port=' . $form->port . ';';
-                    $dbTypes = $form->getDbTypes();
-                    $dbType  = (isset($dbTypes[$form->dbType]) ? $dbTypes[$form->dbType] : $dbType[DbSettingsForm::DB_MYSQL]);
+                    $socket = ($form->socket == '') ? '' : 'unix_socket=' . $form->socket . ';';
+                    $port   = ($form->port == '') ? '' : 'port=' . $form->port . ';';
 
-                    $connectionString = "{$dbType}:host={$form->host};{$port}{$socket}dbname={$form->dbName}";
+                    $connectionString = "mysql:host={$form->host};{$port}{$socket}dbname={$form->dbName}";
 
                     $connection = new CDbConnection($connectionString, $form->user, $form->password);
                     $connection->connectionString = $connectionString;
