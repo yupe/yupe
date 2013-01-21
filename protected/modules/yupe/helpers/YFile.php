9f3545fd2389fbcf4828e2083a3457bf1d779a83
Shiryaev Andrey <grabzila@gmail.com>
2013-01-22 01:46:14 +0400

    + в YFile добавлен метод rmDir, который рекрусивно удаляет директории + добавлена удаление assets при выполнение полной очистки кеша в админке

--- protected/modules/yupe/helpers/YFile.php
+++ protected/modules/yupe/helpers/YFile.php
@@ -29,4 +29,58 @@ class YFile extends CFileHelper
             return false;
         return true; // папка существует и доступна для записи
     }
+
+
+	/**
+	 * Рекрусивное удаление директорий.
+	 *
+	 * @param $path Если $path оканчивается на *, то удаляется только содержимое директории.
+	 * @return bool
+	 */
+	public static function rmDir($path)
+	{
+		static $doNotRemoveBaseDirectory = false, $baseDirectory;
+
+		$path = trim($path);
+		if(substr($path, -1) == '*') {
+			$doNotRemoveBaseDirectory = true;
+			$path = substr($path, 0, -1);
+		}
+		if (substr($path, -1) == '/') {
+			$path = substr($path, 0, -1);
+		}
+		if ($doNotRemoveBaseDirectory) {
+			$baseDirectory = $path;
+		}
+
+		if (is_dir($path)) {
+			$dirHandle = opendir($path);
+			while (false !== ($file = readdir($dirHandle))) {
+				if ($file != '.' && $file != '..') {
+					$tmpPath = $path.'/'.$file;
+
+					#chmod($tmpPath, 0777);
+					if (is_dir($tmpPath)) {
+						self::rmDir($tmpPath);
+					}else {
+						if (file_exists($tmpPath)) {
+							#exec("rm $tmpPath");
+							unlink($tmpPath);
+						}
+					}
+				}
+			}
+			closedir($dirHandle);
+
+			// удаляем текущую папку
+			if ($doNotRemoveBaseDirectory === true && $baseDirectory == $path) {
+				return true;
+			}
+			return rmdir($path);
+		}elseif (is_file($path) || is_link($path)) {
+			return unlink($path);
+		}else {
+			return false;
+		}
+	}
 }
