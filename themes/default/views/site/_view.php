aeacfeb8d696bf091307ad61186d6c3b93ed352c
apexwire <timashov@pay2pay.com>
2013-01-24 14:02:32 +0400

    ЧПУ - представления SiteController

--- themes/default/views/site/_view.php
+++ themes/default/views/site/_view.php
@@ -4,10 +4,10 @@
     </div>
     <div class="author">
         <?php echo Yii::t('blog', 'Опубликовал'); ?>: 
-        <b><?php echo CHtml::link($data->createUser->nick_name, array('/user/people/userInfo', 'username' => $data->createUser->nick_name)); ?></b>
+        <b><?php echo CHtml::link($data->createUser->nick_name, array('/user/default/userInfo', 'username' => $data->createUser->nick_name)); ?></b>
 
         <?php echo Yii::t('blog', 'в блоге'); ?>: 
-        "<?php echo CHtml::link($data->blog->name, array('/blog/blog/show/', 'slug' => $data->blog->slug)); ?>"
+        "<?php echo CHtml::link($data->blog->name, array('/blog/default/show/', 'slug' => $data->blog->slug)); ?>"
 
         <?php echo Yii::t('blog', 'дата'); ?>: 
         <?php echo Yii::app()->getDateFormatter()->formatDateTime($data->publish_date, "short", "short"); ?>
