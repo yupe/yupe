9bb0b51b7582e621f1b289e66ad042b0118b4b6a
AKulikov <tuxuls@gmail.com>
2013-01-28 01:55:39 +0200

    UPD: fix #541 (Битый линк Сервисы - Почтовые сообщения) + поправил код модуля под стандарты стиля PSR

--- protected/modules/mail/controllers/EventAdminController.php
+++ protected/modules/mail/controllers/EventAdminController.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * Event admin controller
+ * Класс контроллера Event:
+ *
+ * @category YupeController
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
 
+/**
+ * Event admin controller
+ * Класс контроллера Event:
+ *
+ * @category YupeController
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
 class EventAdminController extends YBackController
 {
     /**
      * Отображает почтовое событие по указанному идентификатору
+     *
      * @param integer $id Идинтификатор почтовое событие для отображения
+     *
+     * @return nothing
      */
     public function actionView($id)
     {
@@ -14,6 +37,8 @@ class EventAdminController extends YBackController
     /**
      * Создает новую модель почтового события.
      * Если создание прошло успешно - перенаправляет на просмотр.
+     *
+     * @return nothing
      */
     public function actionCreate()
     {
@@ -22,12 +47,10 @@ class EventAdminController extends YBackController
         // Uncomment the following line if AJAX validation is needed
         // $this->performAjaxValidation($model);
 
-        if (isset($_POST['MailEvent']))
-        {
+        if (isset($_POST['MailEvent'])) {
             $model->attributes = $_POST['MailEvent'];
 
-            if ($model->save())
-            {
+            if ($model->save()) {
                 Yii::app()->user->setFlash(
                     YFlashMessages::NOTICE_MESSAGE,
                     Yii::t('MailModule.mail', 'Запись добавлена!')
@@ -44,7 +67,10 @@ class EventAdminController extends YBackController
 
     /**
      * Редактирование почтового события.
+     *
      * @param integer $id the ID of the model to be updated
+     *
+     * @return nothing
      */
     public function actionUpdate($id)
     {
@@ -53,12 +79,10 @@ class EventAdminController extends YBackController
         // Uncomment the following line if AJAX validation is needed
         // $this->performAjaxValidation($model);
 
-        if (isset($_POST['MailEvent']))
-        {
+        if (isset($_POST['MailEvent'])) {
             $model->attributes = $_POST['MailEvent'];
 
-           if ($model->save())
-            {
+            if ($model->save()) {
                 Yii::app()->user->setFlash(
                     YFlashMessages::NOTICE_MESSAGE,
                     Yii::t('MailModule.mail', 'Запись добавлена!')
@@ -76,12 +100,14 @@ class EventAdminController extends YBackController
     /**
      * Удаяет модель почтового события из базы.
      * Если удаление прошло успешно - возвращется в index
+     *
      * @param integer $id идентификатор почтового события, который нужно удалить
+     *
+     * @return nothing
      */
     public function actionDelete($id)
     {
-        if (Yii::app()->request->isPostRequest)
-        {
+        if (Yii::app()->request->isPostRequest) {
             // поддерживаем удаление только из POST-запроса
             $this->loadModel($id)->delete();
 
@@ -100,6 +126,8 @@ class EventAdminController extends YBackController
 
     /**
      * Управление почтовыми событиями.
+     *
+     * @return nothing
      */
     public function actionIndex()
     {
@@ -113,7 +141,10 @@ class EventAdminController extends YBackController
     /**
      * Возвращает модель по указанному идентификатору
      * Если модель не будет найдена - возникнет HTTP-исключение.
-     * @param integer идентификатор нужной модели
+     *
+     * @param integer $id - integer идентификатор нужной модели
+     *
+     * @return nothing
      */
     public function loadModel($id)
     {
@@ -125,12 +156,14 @@ class EventAdminController extends YBackController
 
     /**
      * Производит AJAX-валидацию
-     * @param CModel модель, которую необходимо валидировать
+     *
+     * @param class $model - CModel модель, которую необходимо валидировать
+     *
+     * @return nothing
      */
     protected function performAjaxValidation($model)
     {
-        if (isset($_POST['ajax']) && $_POST['ajax'] === 'mail-event-form')
-        {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'mail-event-form') {
             echo CActiveForm::validate($model);
             Yii::app()->end();
         }
