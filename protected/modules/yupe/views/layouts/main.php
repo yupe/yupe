c62f2635b2430c1cd4f62e8cb5ccab3959d8c064
Anton Cherepanov <davetoxa@gmail.com>
2013-01-28 23:07:15 +0600

    better view

--- protected/modules/yupe/views/layouts/main.php
+++ protected/modules/yupe/views/layouts/main.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo CHtml::encode(Yii::app()->name); ?> <?php echo CHtml::encode($this->pageTitle); ?></title>
     <?php
