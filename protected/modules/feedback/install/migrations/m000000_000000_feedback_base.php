c30f02a7a0702d75123e28436cef87571c0231c7
AKulikov <tuxuls@gmail.com>
2013-01-31 00:19:12 +0200

    UPD: рефакторинг по #528 feedback_base

--- protected/modules/feedback/install/migrations/m000000_000000_feedback_base.php
+++ protected/modules/feedback/install/migrations/m000000_000000_feedback_base.php
@@ -1,41 +1,91 @@
 <?php
+/**
+ * Dictionary install migration
+ * Класс миграций для модуля Dictionary:
+ *
+ * @category YupeMigration
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
+
+/**
+ * Dictionary install migration
+ * Класс миграций для модуля Dictionary:
+ *
+ * @category YupeMigration
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ */
 class m000000_000000_feedback_base extends CDbMigration
 {
+    /**
+     * Накатываем миграцию
+     *
+     * @return nothing
+     **/
     public function safeUp()
     {
         $db = $this->getDbConnection();
-        $tableName = $db->tablePrefix.'feedback';
-        $this->createTable($tableName, array(
-            'id' => 'pk',
-            'answer_user' => 'integer DEFAULT NULL',
-            'creation_date' => 'datetime NOT NULL',
-            'change_date' => 'datetime NOT NULL',
-            'name' => 'string NOT NULL',
-            'email' => 'string NOT NULL',
-            'phone' => 'string DEFAULT NULL',
-            'theme' => 'string NOT NULL',
-            'text' => 'text NOT NULL',
-            'type' => "tinyint(4) NOT NULL DEFAULT '0'",
-            'answer' => 'text NOT NULL',
-            'answer_date' => 'datetime DEFAULT NULL',
-            'is_faq' => "tinyint(1) NOT NULL DEFAULT '0'",
-            'status' => "tinyint(4) NOT NULL DEFAULT '0'",
-            'ip' => 'string NOT NULL',
-        ),"ENGINE=InnoDB DEFAULT CHARSET=utf8");
 
-        $this->createIndex("feedback_type",$tableName,"type", false);
-        $this->createIndex("feedback_status",$tableName,"status", false);
-        $this->createIndex("feedback_isfaq",$tableName,"is_faq", false);
-        $this->createIndex("feedback_answer_user",$tableName,"answer_user", false);
-        $this->createIndex("feedback_answer_date",$tableName,"answer_date", false);
+        $this->createTable(
+            $db->tablePrefix . 'feedback', array(
+                'id' => 'pk',
+                'answer_user' => 'integer DEFAULT NULL',
+                'creation_date' => 'datetime NOT NULL',
+                'change_date' => 'datetime NOT NULL',
+                'name' => 'string NOT NULL',
+                'email' => 'string NOT NULL',
+                'phone' => 'string DEFAULT NULL',
+                'theme' => 'string NOT NULL',
+                'text' => 'text NOT NULL',
+                'type' => "tinyint(4) NOT NULL DEFAULT '0'",
+                'answer' => 'text NOT NULL',
+                'answer_date' => 'datetime DEFAULT NULL',
+                'is_faq' => "tinyint(1) NOT NULL DEFAULT '0'",
+                'status' => "tinyint(4) NOT NULL DEFAULT '0'",
+                'ip' => 'string NOT NULL',
+            ), "ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
 
-        $this->addForeignKey("feedback_answer_user_fk",$tableName,'answer_user',$db->tablePrefix.'user','id','SET NULL','CASCADE');
+        $this->createIndex($db->tablePrefix . "feedback_type", $db->tablePrefix . 'feedback', "type", false);
+        $this->createIndex($db->tablePrefix . "feedback_status", $db->tablePrefix . 'feedback', "status", false);
+        $this->createIndex($db->tablePrefix . "feedback_isfaq", $db->tablePrefix . 'feedback', "is_faq", false);
+        $this->createIndex($db->tablePrefix . "feedback_answer_user", $db->tablePrefix . 'feedback', "answer_user", false);
+        $this->createIndex($db->tablePrefix . "feedback_answer_date", $db->tablePrefix . 'feedback', "answer_date", false);
+
+        $this->addForeignKey($db->tablePrefix . "feedback_answer_user_fk", $db->tablePrefix . 'feedback', 'answer_user', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
 
     }
  
+    /**
+     * Откатываем миграцию
+     *
+     * @return nothing
+     **/
     public function safeDown()
     {
         $db = $this->getDbConnection();
-        $this->dropTable($db->tablePrefix.'feedback');
+        /**
+         * Убиваем внешние ключи, индексы и таблицу - feedback
+         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
+         **/
+        if ($db->schema->getTable($db->tablePrefix . 'feedback') !== null) {
+            /*
+            $this->dropIndex($db->tablePrefix . "feedback_type", $db->tablePrefix . 'feedback');
+            $this->dropIndex($db->tablePrefix . "feedback_status", $db->tablePrefix . 'feedback');
+            $this->dropIndex($db->tablePrefix . "feedback_isfaq", $db->tablePrefix . 'feedback');
+            $this->dropIndex($db->tablePrefix . "feedback_answer_user", $db->tablePrefix . 'feedback');
+            $this->dropIndex($db->tablePrefix . "feedback_answer_date", $db->tablePrefix . 'feedback');
+            */
+            
+            if (in_array($db->tablePrefix . "feedback_answer_user_fk", $db->schema->getTable($db->tablePrefix . 'feedback')->foreignKeys))
+                $this->dropForeignKey($db->tablePrefix . "feedback_answer_user_fk", $db->tablePrefix . 'feedback');
+            
+            $this->dropTable($db->tablePrefix . 'feedback');
+        }
     }
 }
