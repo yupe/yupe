42c2844833100b0114e27b097773c1cee2026173
Shiryaev Andrey <grabzila@gmail.com>
2013-02-03 18:09:42 +0400

    Fix #473

--- protected/modules/page/install/page.php
+++ protected/modules/page/install/page.php
@@ -2,7 +2,7 @@
 return array(
     'module'   => array(
         'class'  => 'application.modules.page.PageModule',
-	    'layout' => '//layouts/column2',
+        'layout' => '//layouts/column2',
     ),
     'import'    => array(
         'application.modules.page.models.*',
