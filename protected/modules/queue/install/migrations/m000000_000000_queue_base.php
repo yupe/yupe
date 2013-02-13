d2cd613aebbb0d9fe62800ab05eef2b41033930e
AKulikov <tuxuls@gmail.com>
2013-02-13 15:38:49 +0200

    UPD: fixing m000000_000000_queue_base

--- protected/modules/queue/install/migrations/m000000_000000_queue_base.php
+++ protected/modules/queue/install/migrations/m000000_000000_queue_base.php
@@ -38,13 +38,13 @@ class m000000_000000_queue_base extends CDbMigration
         $this->createTable(
             $db->tablePrefix . 'queue', array(
                 'id' => 'pk',
-                'worker' => 'tinyint(3) unsigned NOT NULL',
+                'worker' => 'integer unsigned NOT NULL',
                 'create_time' => 'datetime NOT NULL',
                 'task' => 'text NOT NULL',
                 'start_time' => 'datetime DEFAULT NULL',
                 'complete_time' => 'datetime DEFAULT NULL',
-                'priority' => "tinyint(3) unsigned NOT NULL DEFAULT '1'",
-                'status' => "tinyint(3) unsigned NOT NULL DEFAULT '0'",
+                'priority' => "integer unsigned NOT NULL DEFAULT '1'",
+                'status' => "integer unsigned NOT NULL DEFAULT '0'",
                 'notice' => 'varchar(300) DEFAULT NULL',
             ), $options
         );
