af4363a571764b9a46afbe5808ffbc87031e87c0
Andrey Opeykin <aopeykin@yandex.ru>
2013-02-20 16:22:33 +0400

    closed #648

--- themes/default/views/menu/widgets/MenuWidget/main.php
+++
@@ -1,4 +0,0 @@
-<?php echo CHtml::openTag('div', $layoutParams['htmlOptions']); ?>
-    <?php echo $this->widget('zii.widgets.CMenu', $params, true); ?>
-    <br style="clear: left"/>
-<?php echo CHtml::closeTag('div'); ?>
