640828bc6d43262610e5c3d256c50f9de28b1bf8
Andrey Opeykin <aopeykin@yandex.ru>
2013-03-07 15:52:23 +0400

    fix migrations for "dictionary" module

--- protected/modules/dictionary/install/migrations/m000000_000000_dictionary_base.php
+++ protected/modules/dictionary/install/migrations/m000000_000000_dictionary_base.php
@@ -20,7 +20,7 @@
  * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
  * @link     http://yupe.ru
  */
-class m000000_000000_dictionary_base extends CDbMigration
+class m000000_000000_dictionary_base extends YDbMigration
 {
     /**
      * Накатываем миграцию:
@@ -29,57 +29,56 @@ class m000000_000000_dictionary_base extends CDbMigration
      **/
     public function safeUp()
     {
-        $db = $this->getDbConnection();
-        $options = Yii::app()->db->schema instanceof CMysqlSchema ? 'ENGINE=InnoDB DEFAULT CHARSET=utf8' : '';
-        $this->createTable(
-            $db->tablePrefix . 'dictionary_group', array(
+        $this->createTable('{{dictionary_dictionary_group}}', array(
                 'id' => 'pk',
-                'code' => 'string NOT NULL',
-                'name' => "string NOT NULL",
-                'description' => "varchar(300) NOT NULL DEFAULT ''",
+                'code' => 'varchar(100) NOT NULL',
+                'name' => "varchar(250) NOT NULL",
+                'description' => "varchar(250) NOT NULL DEFAULT ''",
                 'creation_date' => 'datetime NOT NULL',
                 'update_date' => 'datetime NOT NULL',
                 'create_user_id' => 'integer DEFAULT NULL',
                 'update_user_id' => 'integer DEFAULT NULL',
-            ), $options
+            ), $this->getOptions()
         );
 
-        $this->createIndex($db->tablePrefix . "dictionary_group_code_unique", $db->tablePrefix . 'dictionary_group', "code", true);
-        $this->createIndex($db->tablePrefix . "dictionary_group_create_user_id", $db->tablePrefix . 'dictionary_group', "create_user_id", false);
-        $this->createIndex($db->tablePrefix . "dictionary_group_update_user_id", $db->tablePrefix . 'dictionary_group', "update_user_id", false);
+        //ix
+        $this->createIndex("ux_{{dictionary_dictionary_group}}_code", '{{dictionary_dictionary_group}}', "code", true);
+        $this->createIndex("ix_{{dictionary_dictionary_group}}_create_user_id", '{{dictionary_dictionary_group}}', "create_user_id", false);
+        $this->createIndex("ix_{{dictionary_dictionary_group}}_update_user_id", '{{dictionary_dictionary_group}}', "update_user_id", false);
 
-        $this->addForeignKey($db->tablePrefix . "dictionary_group_createuser_id_fk", $db->tablePrefix . 'dictionary_group', 'create_user_id', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
-        $this->addForeignKey($db->tablePrefix . "dictionary_group_updateuser_id_fk", $db->tablePrefix . 'dictionary_group', 'update_user_id', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
+        //fk
+        $this->addForeignKey("fk_{{dictionary_dictionary_group}}_create_user_id",'{{dictionary_dictionary_group}}', 'create_user_id','{{user}}', 'id', 'SET NULL', 'CASCADE');
+        $this->addForeignKey("fk_{{dictionary_dictionary_group}}_updateuser_id",'{{dictionary_dictionary_group}}', 'update_user_id', '{{user}}', 'id', 'SET NULL', 'CASCADE');
 
         /**
          * Dictionary_data
          **/
-        $this->createTable(
-            $db->tablePrefix . 'dictionary_data', array(
+        $this->createTable('{{dictionary_dictionary_data}}', array(
                 'id' => 'pk',
                 'group_id' => 'integer NOT NULL',
-                'code' => 'string NOT NULL',
-                'name' => 'string NOT NULL',
-                'value' => 'string NOT NULL',
-                'description' => "varchar(300) NOT NULL DEFAULT ''",
+                'code' => 'varchar(100) NOT NULL',
+                'name' => 'varchar(250) NOT NULL',
+                'value' => 'varchar(250) NOT NULL',
+                'description' => "varchar(250) NOT NULL DEFAULT ''",
                 'creation_date' => 'datetime NOT NULL',
                 'update_date' => 'datetime NOT NULL',
                 'create_user_id' => 'integer DEFAULT NULL',
                 'update_user_id' => 'integer DEFAULT NULL',
                 'status' => "integer NOT NULL DEFAULT '1'",
-            ), $options
+            ), $this->getOptions()
         );
 
-        $this->createIndex($db->tablePrefix . "dictionary_data_code_unique", $db->tablePrefix . 'dictionary_data', "code", true);
-        $this->createIndex($db->tablePrefix . "dictionary_data_group_id", $db->tablePrefix . 'dictionary_data', "group_id", false);
-        $this->createIndex($db->tablePrefix . "dictionary_data_create_user_id", $db->tablePrefix . 'dictionary_data', "create_user_id", false);
-        $this->createIndex($db->tablePrefix . "dictionary_data_update_user_id", $db->tablePrefix . 'dictionary_data', "update_user_id", false);
-        $this->createIndex($db->tablePrefix . "dictionary_data_status", $db->tablePrefix . 'dictionary_data', "status", false);
+        //ix
+        $this->createIndex("ux_{{dictionary_dictionary_data}}_code_unique",'{{dictionary_dictionary_data}}', "code", true);
+        $this->createIndex("ix_{{dictionary_dictionary_data}}_group_id",'{{dictionary_dictionary_data}}', "group_id", false);
+        $this->createIndex("ix_{{dictionary_dictionary_data}}_create_user_id",'{{dictionary_dictionary_data}}', "create_user_id", false);
+        $this->createIndex("ix_{{dictionary_dictionary_data}}_update_user_id",'{{dictionary_dictionary_data}}', "update_user_id", false);
+        $this->createIndex("ix_{{dictionary_dictionary_data}}_status",'{{dictionary_dictionary_data}}', "status", false);
 
-        $this->addForeignKey($db->tablePrefix . "dictionary_data_createuser_id_fk", $db->tablePrefix . 'dictionary_data', 'create_user_id', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
-        $this->addForeignKey($db->tablePrefix . "dictionary_data_updateuser_id_fk", $db->tablePrefix . 'dictionary_data', 'update_user_id', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
-        $this->addForeignKey($db->tablePrefix . "dictionary_data_group_id_fk", $db->tablePrefix . 'dictionary_data', 'group_id', $db->tablePrefix . 'dictionary_group', 'id', 'CASCADE', 'CASCADE');
-
+        //fk
+        $this->addForeignKey("fk_{{dictionary_dictionary_data}}_create_user_id",'{{dictionary_dictionary_data}}', 'create_user_id', '{{user}}', 'id', 'SET NULL', 'CASCADE');
+        $this->addForeignKey("fk_{{dictionary_dictionary_data}}_update_user_id",'{{dictionary_dictionary_data}}', 'update_user_id', '{{user}}', 'id', 'SET NULL', 'CASCADE');
+        $this->addForeignKey("fk_{{dictionary_dictionary_data}}_data_group_id",'{{dictionary_dictionary_data}}', 'group_id','{{dictionary_dictionary_group}}', 'id', 'CASCADE', 'CASCADE');
     }
  
     /**
@@ -89,51 +88,7 @@ class m000000_000000_dictionary_base extends CDbMigration
      **/
     public function safeDown()
     {
-        $db = $this->getDbConnection();
-        /**
-         * Убиваем внешние ключи, индексы и таблицу - dictionary_group
-         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
-         **/
-        if ($db->schema->getTable($db->tablePrefix . 'dictionary_group') !== null) {
-            /*
-            $this->dropIndex($db->tablePrefix . "dictionary_group_code_unique", $db->tablePrefix . 'dictionary_group');
-            $this->dropIndex($db->tablePrefix . "dictionary_group_create_user_id", $db->tablePrefix . 'dictionary_group');
-            $this->dropIndex($db->tablePrefix . "dictionary_group_update_user_id", $db->tablePrefix . 'dictionary_group');
-            */
-            if (in_array($db->tablePrefix . "dictionary_group_createuser_id", $db->schema->getTable($db->tablePrefix . 'dictionary_group')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "dictionary_group_createuser_id", $db->tablePrefix . 'dictionary_group');
-            if (in_array($db->tablePrefix . "dictionary_group_updateuser_id", $db->schema->getTable($db->tablePrefix . 'dictionary_group')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "dictionary_group_updateuser_id", $db->tablePrefix . 'dictionary_group');
-            $this->dropTable($db->tablePrefix . 'dictionary_group');
-        }
-
-        /**
-         * Убиваем внешние ключи, индексы и таблицу - dictionary_data
-         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
-         **/
-        if ($db->schema->getTable($db->tablePrefix . 'dictionary_data') !== null) {
-            if (in_array($db->tablePrefix . "dictionary_data_createuser_id", $db->schema->getTable($db->tablePrefix . 'dictionary_group')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "dictionary_data_createuser_id", $db->tablePrefix . 'dictionary_data');
-            if (in_array($db->tablePrefix . "dictionary_data_updateuser_id", $db->schema->getTable($db->tablePrefix . 'dictionary_group')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "dictionary_data_updateuser_id", $db->tablePrefix . 'dictionary_data');
-            if (in_array($db->tablePrefix . "dictionary_data_group_id", $db->schema->getTable($db->tablePrefix . 'dictionary_group')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "dictionary_data_group_id", $db->tablePrefix . 'dictionary_data');
-            /*
-            $this->dropIndex($db->tablePrefix . "dictionary_data_code_unique", $db->tablePrefix . 'dictionary_data');
-            $this->dropIndex($db->tablePrefix . "dictionary_data_group_id", $db->tablePrefix . 'dictionary_data');
-            $this->dropIndex($db->tablePrefix . "dictionary_data_create_user_id", $db->tablePrefix . 'dictionary_data');
-            $this->dropIndex($db->tablePrefix . "dictionary_data_update_user_id", $db->tablePrefix . 'dictionary_data');
-            $this->dropIndex($db->tablePrefix . "dictionary_data_status", $db->tablePrefix . 'dictionary_data');
-            */
-            $this->dropTable($db->tablePrefix . 'dictionary_data');
-        }
-
-        /**
-         * Убиваем внешние ключи, индексы и таблицу - content_block
-         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
-         **/
-        if ($db->schema->getTable($db->tablePrefix . 'dictionary') !== null) {
-            $this->dropTable($db->tablePrefix . 'dictionary');
-        }
+        $this->dropTable('{{dictionary_dictionary_data}}');
+        $this->dropTable('{{dictionary_dictionary_group}}');
     }
 }
