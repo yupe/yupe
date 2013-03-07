43e61b939a3a0427c3fccd2b2d036f626147ef77
Andrey Opeykin <aopeykin@yandex.ru>
2013-03-07 15:19:47 +0400

    fix migrations for "contentblock" module

--- protected/modules/contentblock/install/migrations/m000000_000000_contentblock_base.php
+++ protected/modules/contentblock/install/migrations/m000000_000000_contentblock_base.php
@@ -20,7 +20,7 @@
  * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
  * @link     http://yupe.ru
  */
-class m000000_000000_contentblock_base extends CDbMigration
+class m000000_000000_contentblock_base extends YDbMigration
 {
     /**
      * Накатываем миграцию:
@@ -29,21 +29,18 @@ class m000000_000000_contentblock_base extends CDbMigration
      **/
     public function safeUp()
     {
-        $db = $this->getDbConnection();
-        $options = Yii::app()->db->schema instanceof CMysqlSchema ? 'ENGINE=InnoDB DEFAULT CHARSET=utf8' : '';
-        $this->createTable(
-            $db->tablePrefix . 'content_block', array(
+        $this->createTable('{{contentblock_content_block}}', array(
                 'id' => 'pk',
-                'name' => 'string NOT NULL',
-                'code' => 'string NOT NULL',
+                'name' => 'varchar(250) NOT NULL',
+                'code' => 'varchar(100) NOT NULL',
                 'type' => "integer NOT NULL DEFAULT '1'",
                 'content' => "text NOT NULL",
-                'description' => "varchar(300) DEFAULT NULL"
-            ), $options
+                'description' => "varchar(255) DEFAULT NULL"
+            ), $this->getOptions()
         );
 
-        $this->createIndex($db->tablePrefix . "contentblock_code_unique", $db->tablePrefix . 'content_block', "code", true);
-        $this->createIndex($db->tablePrefix . "contentblock_type", $db->tablePrefix . 'content_block', "type", false);
+        $this->createIndex("ux_{{contentblock_content_block}}_code", '{{contentblock_content_block}}', "code", true);
+        $this->createIndex("ix_{{contentblock_content_block}}_type", '{{contentblock_content_block}}', "type", false);
     }
  
     /**
@@ -53,17 +50,6 @@ class m000000_000000_contentblock_base extends CDbMigration
      **/
     public function safeDown()
     {
-        $db = $this->getDbConnection();
-        /**
-         * Убиваем внешние ключи, индексы и таблицу - content_block
-         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
-         **/
-        if ($db->schema->getTable($db->tablePrefix . 'content_block') !== null) {
-            /*
-            $this->dropIndex($db->tablePrefix . "contentblock_code_unique", $db->tablePrefix . 'content_block');
-            $this->dropIndex($db->tablePrefix . "contentblock_type", $db->tablePrefix . 'content_block');
-            */
-            $this->dropTable($db->tablePrefix . 'content_block');
-        }
+        $this->dropTable('{{contentblock_content_block}}');
     }
 }
