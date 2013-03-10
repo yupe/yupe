8b68bb71d5534d93076de48a36b78a10efdaca1b
Nickolay Sedov <yamikspark@gmail.com>
2013-03-10 20:24:16 +0500

    upd: fix migrations for "News" module

--- protected/modules/news/install/migrations/m000000_000000_news_base.php
+++ protected/modules/news/install/migrations/m000000_000000_news_base.php
@@ -36,7 +36,7 @@ class m000000_000000_news_base extends CDbMigration
          * news:
          **/
         $this->createTable(
-            $db->tablePrefix . 'news_news', array(
+            '{news_news}', array(
                 'id' => 'pk',
                 'category_id' => 'integer DEFAULT NULL',
                 'lang' => 'char(2) DEFAULT NULL',
@@ -57,15 +57,14 @@ class m000000_000000_news_base extends CDbMigration
             ), $options
         );
 
-        $this->createIndex($db->tablePrefix . "news_news_alias_unique", $db->tablePrefix . 'news_news', "alias,lang", true);
-        $this->createIndex($db->tablePrefix . "news_news_status", $db->tablePrefix . 'news_news', "status", false);
-        $this->createIndex($db->tablePrefix . "news_news_user", $db->tablePrefix . 'news_news', "user_id", false);
-        $this->createIndex($db->tablePrefix . "news_news_category", $db->tablePrefix . 'news_news', "category_id", false);
-        $this->createIndex($db->tablePrefix . "news_news_date", $db->tablePrefix . 'news_news', "date", false);
+        $this->createIndex("{news_news_alias_unique}", "{news_news}", "alias,lang", true);
+        $this->createIndex("{news_news_status", "{news_news}", "status", false);
+        $this->createIndex("{news_news_user}", "{news_news}", "user_id", false);
+        $this->createIndex("{news_news_category}", "{news_news}", "category_id", false);
+        $this->createIndex("{news_news_date}", "{news_news}", "date", false);
 
-        $this->addForeignKey($db->tablePrefix . "news_news_user_fk", $db->tablePrefix . 'news_news', 'user_id', $db->tablePrefix . 'user', 'id', 'SET NULL', 'CASCADE');
-        $this->addForeignKey($db->tablePrefix . "news_news_category_fk", $db->tablePrefix . 'news_news', 'category_id', $db->tablePrefix . 'category_category', 'id', 'SET NULL', 'CASCADE');
-
+        $this->addForeignKey("{news_news_user_fk}", "{news_news}", 'user_id', "{user}", 'id', 'SET NULL', 'CASCADE');
+        $this->addForeignKey("{news_news_category_fk}", "{news_news}", 'category_id', "{category_category}", 'id', 'SET NULL', 'CASCADE');
     }
  
     /**
@@ -75,29 +74,6 @@ class m000000_000000_news_base extends CDbMigration
      **/
     public function safeDown()
     {
-        $db = $this->getDbConnection();
-
-        /**
-         * Убиваем внешние ключи, индексы и таблицу - news
-         * @todo найти как проверять существование индексов, что бы их подчищать (на абстрактном уровне без привязки к типу БД):
-         **/
-        if ($db->schema->getTable($db->tablePrefix . 'news_news') !== null) {
-
-            /*
-            $this->dropIndex($db->tablePrefix . "news_alias_unique", $db->tablePrefix . 'news');
-            $this->dropIndex($db->tablePrefix . "news_status", $db->tablePrefix . 'news');
-            $this->dropIndex($db->tablePrefix . "news_user", $db->tablePrefix . 'news');
-            $this->dropIndex($db->tablePrefix . "news_category", $db->tablePrefix . 'news');
-            $this->dropIndex($db->tablePrefix . "news_date", $db->tablePrefix . 'news');
-            */
-
-            if (in_array($db->tablePrefix . "news_news_user_fk", $db->schema->getTable($db->tablePrefix . 'news_news')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "news_news_user_fk", $db->tablePrefix . 'news_news');
-
-            if (in_array($db->tablePrefix . "news_news_category_fk", $db->schema->getTable($db->tablePrefix . 'news_news')->foreignKeys))
-                $this->dropForeignKey($db->tablePrefix . "news_news_user_fk", $db->tablePrefix . 'news_news');
-
-            $this->dropTable($db->tablePrefix . 'news_news');
-        }
+        $this->dropTable("{news_news}");    
     }
 }
