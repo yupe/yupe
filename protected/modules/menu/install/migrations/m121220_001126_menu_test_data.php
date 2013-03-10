b8cf229e67e454b72a1fc48cec74ee8f0ef4ccf1
Nickolay Sedov <yamikspark@gmail.com>
2013-03-10 20:17:47 +0500

    upd: fix migrations for "Menu" module

--- protected/modules/menu/install/migrations/m121220_001126_menu_test_data.php
+++ protected/modules/menu/install/migrations/m121220_001126_menu_test_data.php
@@ -35,7 +35,7 @@ class m121220_001126_menu_test_data extends CDbMigration
         $db = $this->getDbConnection();
 
         $this->insert(
-            $db->tablePrefix . 'menu_menu', array(
+            '{menu_menu}', array(
                 'id'          => 1,
                 'name'        => 'Верхнее меню',
                 'code'        => 'top-menu',
@@ -70,7 +70,7 @@ class m121220_001126_menu_test_data extends CDbMigration
 
             foreach ($columns as $c)
                 $item[$c] = $i[$n++];
-            $this->insert($db->tablePrefix . 'menu_menu_item', $item);
+            $this->insert('{menu_menu_item}', $item);
         }
     }
 }
