98e4c1e8807f9a894075d80603023ba79a05ff58
Nickolay Sedov <yamikspark@gmail.com>
2013-03-10 23:02:26 +0500

    copyright comments

--- protected/modules/menu/install/migrations/m130310_135201_menu_regular_links.php
+++ protected/modules/menu/install/migrations/m130310_135201_menu_regular_links.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Menu install migration
+ * Класс миграций для модуля Menu:
+ *
+ * @category YupeMigration
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
 
 class m130310_135201_menu_regular_links extends YDbMigration
 {
