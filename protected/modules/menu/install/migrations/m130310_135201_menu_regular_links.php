1f9727106433e38bdd134638633fbcb04b269225
Nickolay Sedov <yamikspark@gmail.com>
2013-03-10 19:29:38 +0500

    fix migrations for "Menu" module

---
+++ protected/modules/menu/install/migrations/m130310_135201_menu_regular_links.php
@@ -0,0 +1,23 @@
+<?php
+
+class m130310_135201_menu_regular_links extends CDbMigration
+{
+	public function safeUp()
+    {
+        $db = $this->getDbConnection();
+        
+        $this->addColumn($db->tablePrefix . 'menu_item', 'regular_link', 'boolean');
+    }
+ 
+    /**
+     * Откатываем миграцию:
+     *
+     * @return nothing
+     **/
+    public function safeDown()
+    {
+        $db = $this->getDbConnection();
+
+        $this->dropColumn($db->tablePrefix . 'menu_item', 'regular_link');
+    }
+}
