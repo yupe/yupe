45ee86967996e73ddb2e74a6d79ee40da31b93a5
AKulikov <tuxuls@gmail.com>
2013-04-05 00:19:46 +0300

    UPD: начальные правки по #488  #779

--- protected/modules/yupe/models/YModel.php
+++ protected/modules/yupe/models/YModel.php
@@ -87,7 +87,7 @@ abstract class YModel extends Model
      **/
     public function cache($duration, $dependency=null, $queryCount=1)
     {
-        $this->cacheKey='yii:dbschema'.$this->getDbConnection()->connectionString.':'.$this->getDbConnection()->username;
+        $this->cacheKey = 'yii:dbschema' . $this->getDbConnection()->connectionString . ':' . $this->getDbConnection()->username;
         return parent::cache($duration, $dependency, $queryCount);
     }
 
