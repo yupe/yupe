ede4a6bb253aea659e8e9f068fa456cd9022835d
AKulikov <tuxuls@gmail.com>
2013-04-05 03:51:22 +0300

    UPD: подогнал BackendController под стандарты PSR

--- protected/modules/yupe/controllers/BackendController.php
+++ protected/modules/yupe/controllers/BackendController.php
@@ -1,5 +1,29 @@
 <?php
+/**
+ * Главный контроллер админ-панели,
+ * который содержит методы для управления модулями,
+ * а также их настройками.
+ *
+ * @category YupeController
+ * @package  Yupe
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.5 (dev)
+ * @link     http://yupe.ru
+ *
+ **/
 
+/**
+ * Главный контроллер админ-панели:
+ *
+ * @category YupeController
+ * @package  Yupe
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.5 (dev)
+ * @link     http://yupe.ru
+ *
+ **/
 class BackendController extends YBackController
 {
 
