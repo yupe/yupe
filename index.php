77fc587b1cd2bb7fef59927e1c85b5f6173baf5a
adminnu <mail@lyzhenkov.ru>
2013-04-09 15:55:25 +0400

    Fixed #632: Работа в prod и debug режиме

--- index.php
+++ index.php
@@ -2,14 +2,25 @@
 // подробнее про index.php http://www.yiiframework.ru/doc/guide/ru/basics.entry
 date_default_timezone_set('Europe/Moscow');
 
-// путь к фреймворку Yii, при необходиомсти можно изменить
-$yii = dirname(__FILE__) . '/framework/yiilite.php';
-// путь к основному конфигурационному файлу Yii, при необходиомсти можно изменить
-$config = dirname(__FILE__) . '/protected/config/main.php';
+// Выбираем конфигурацию development-main.php, если переменная окружения php_env установлена в development
+// или совпадение домена на котором запущен проект
+if (
+	getenv('php_env') == 'development' ||
+	strpos($_SERVER['SERVER_NAME'], 'yupe.local') !== false
+) {
+	// путь к фреймворку Yii
+	$yii = dirname(__FILE__) . '/framework/yiilite.php';
+	// путь к основному конфигурационному файлу Yii
+	$config = dirname(__FILE__) . '/protected/config/main-development.php';
 
-// при работе сайта в "боевом" режиме следующие две строки рекомендуется закомментировать
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+} else { //production считается во всех случаях, когда не выполнены условия
+	// путь к фреймворку Yii
+	$yii = dirname(__FILE__) . '/framework/yiilite.php';
+	// путь к основному конфигурационному файлу Yii
+	$config = dirname(__FILE__) . '/protected/config/main.php';
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
