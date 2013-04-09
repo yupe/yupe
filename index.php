116247b1a5e80344ee0c85093524bdb14cdeb6ef
AKulikov <tuxuls@gmail.com>
2013-04-09 16:52:31 +0300

    UPD: правки входного скрипта

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
 <?php
+/**
+ * Входной скрипт index:
+ * 
+ *   @category YupeScript
+ *   @package  YupeCMS
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ **/
 // подробнее про index.php http://www.yiiframework.ru/doc/guide/ru/basics.entry
 date_default_timezone_set('Europe/Moscow');
 
+// Комментируем перед выпуском в продакшен:
+define('YII_DEBUG', true);
+
 // Выбираем конфигурацию development-main.php, если переменная окружения php_env установлена в development
 // или совпадение домена на котором запущен проект
-if (
-	getenv('php_env') == 'development' ||
-	strpos($_SERVER['SERVER_NAME'], 'localhost') !== false ||
-	$_SERVER['SERVER_ADDR'] == '127.0.0.1'
-) {
-	// путь к фреймворку Yii
-	$yii = dirname(__FILE__) . '/framework/yiilite.php';
-	// путь к основному конфигурационному файлу Yii
-	$config = dirname(__FILE__) . '/protected/config/main-development.php';
+if (defined('YII_DEBUG') || getenv('php_env') == 'development' || strpos($_SERVER['SERVER_NAME'], 'localhost') !== false || $_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+    // путь к фреймворку Yii
+    $yii = dirname(__FILE__) . '/framework/yiilite.php';
+    // путь к основному конфигурационному файлу Yii
+    $config = dirname(__FILE__) . '/protected/config/main-development.php';
 
-	defined('YII_DEBUG') or define('YII_DEBUG', true);
-	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 } else { //production считается во всех случаях, когда не выполнены условия
-	// путь к фреймворку Yii
-	$yii = dirname(__FILE__) . '/framework/yiilite.php';
-	// путь к основному конфигурационному файлу Yii
-	$config = dirname(__FILE__) . '/protected/config/main.php';
+    // путь к фреймворку Yii
+    $yii = dirname(__FILE__) . '/framework/yiilite.php';
+    // путь к основному конфигурационному файлу Yii
+    $config = dirname(__FILE__) . '/protected/config/main.php';
 }
 
-require_once($yii);
+require_once $yii;
 Yii::createWebApplication($config)->run();
