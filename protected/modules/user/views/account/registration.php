83c66cfe0ca2b4e1be7417daf97cdff84d4a2b06
Vitaly Martynenko <contact@vitalka.me>
2013-04-15 23:46:35 +0300

    Revert excessive changes

--- protected/modules/user/views/account/registration.php
+++ protected/modules/user/views/account/registration.php
@@ -1,11 +1,8 @@
-<?php
-$this->pageTitle = Yii::t('user', 'Регистрация нового пользователя');
-$this->breadcrumbs = array('Регистрация нового пользователя');
-?>
+<?php $this->pageTitle = Yii::t('UserModule.user', 'Регистрация новго пользователя'); ?>
 
-<h1>Регистрация нового пользователя</h1>
+<h1><?php echo Yii::t('UserModule.user', 'Регистрация нового пользователя'); ?></h1>
 
-<div class='hint'>Пожалуйста, имя пользователя и пароль заполняйте только латинскими буквами и цифрами.</div>
+<div class='hint'><?php echo Yii::t('UserModule.user', 'Пожалуйста, имя пользователя и пароль заполняйте только латинскими буквами и цифрами.'); ?></div>
 
 <br/><br/>
 
@@ -13,52 +10,48 @@ $this->breadcrumbs = array('Регистрация нового пользова
 
 <div class="form">
 
-    <?php $form = $this->beginWidget('CActiveForm', array('id' => 'registration-form'));?>
+    <?php $form = $this->beginWidget('CActiveForm', array('id' => 'registration-form')); ?>
 
     <?php echo $form->errorSummary($model); ?>
 
     <div class="row">
         <?php echo $form->labelEx($model, 'nick_name'); ?>
-        <?php echo $form->textField($model, 'nick_name') ?>
+        <?php echo $form->textField($model, 'nick_name'); ?>
         <?php echo $form->error($model, 'nick_name'); ?>
     </div>
 
     <div class="row">
         <?php echo $form->labelEx($model, 'email'); ?>
-        <?php echo $form->textField($model, 'email') ?>
+        <?php echo $form->textField($model, 'email'); ?>
         <?php echo $form->error($model, 'email'); ?>
     </div>
 
     <div class="row">
         <?php echo $form->labelEx($model, 'password'); ?>
-        <?php echo $form->passwordField($model, 'password');?>
+        <?php echo $form->passwordField($model, 'password'); ?>
         <?php echo $form->error($model, 'password'); ?>
     </div>
 
     <div class="row">
         <?php echo $form->labelEx($model, 'cPassword'); ?>
-        <?php echo $form->passwordField($model, 'cPassword');?>
+        <?php echo $form->passwordField($model, 'cPassword'); ?>
         <?php echo $form->error($model, 'cPassword'); ?>
     </div>
 
-
-    <?php if ($module->showCaptcha && CCaptcha::checkRequirements()): ?>
-        <div class="row">
-            <?php echo $form->labelEx($model, 'verifyCode'); ?>
-            <div>
-                <?php $this->widget('CCaptcha', array(
-                    'showRefreshButton' => true,
-                    'clickableImage' => true,
-                    'buttonLabel' => 'обновить',
-                    'buttonOptions' => array('class' => 'captcha-refresh-link'),
-                )); ?>
-                <?php echo $form->textField($model, 'verifyCode'); ?>
-                <?php echo $form->error($model, 'verifyCode'); ?>
+    <?php if (Yii::app()->getModule('user')->showCaptcha): ?>
+        <?php if (CCaptcha::checkRequirements('gd')): ?>
+            <div class="row">
+                <?php echo $form->labelEx($model, 'verifyCode'); ?>
+                <div>
+                    <?php $this->widget('CCaptcha', array('showRefreshButton' => false)); ?>
+                    <?php echo $form->textField($model, 'verifyCode'); ?>
+                    <?php echo $form->error($model, 'verifyCode'); ?>
+                </div>
+                <div class="hint">
+                    <?php echo Yii::t('UserModule.user', 'Введите текст указанный на картинке'); ?>
+                </div>
             </div>
-            <div class="hint">
-                Введите текст указанный на картинке
-            </div>
-        </div>
+        <?php endif; ?>
     <?php endif; ?>
 
     <div class="row submit">
@@ -67,3 +60,14 @@ $this->breadcrumbs = array('Регистрация нового пользова
 
     <?php $this->endWidget(); ?>
 </div><!-- form -->
+
+<?php  $this->widget('application.modules.social.extensions.eauth.EAuthWidget', array('action' => '/social/social/login')); ?>
+
+<div style='float:left;'>
+    <div style='float:left;padding-right:5px'>
+        <?php $this->widget('application.modules.social.widgets.ysc.yandex.YandexShareApi', array(
+            'type'     => 'button',
+            'services' => 'all',
+        )); ?>
+    </div>
+</div>
