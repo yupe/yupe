7f1203eef9020d75661766dfbaf8810ae607a692
AKulikov <tuxuls@gmail.com>
2013-04-18 17:08:44 +0300

    UPD: protected/modules/queue/models/Queue.php меняем CDbExpression на YDbMigration::expression

--- protected/modules/queue/models/Queue.php
+++ protected/modules/queue/models/Queue.php
@@ -28,7 +28,7 @@ class Queue extends YModel
     public function beforeSave()
     {
         if ($this->isNewRecord)
-            $this->create_time = new CDbExpression(Yii::app()->db->schema instanceof CSqliteSchema ? 'DATETIME("now")' :'NOW()');
+            $this->create_time = YDbMigration::expression('NOW()');
         return parent::beforeSave();
     }
 
