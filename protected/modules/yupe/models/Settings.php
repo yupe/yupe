d989cd5b76a7c04d479e548a65aaee255d6494fc
AKulikov <tuxuls@gmail.com>
2013-04-18 17:14:09 +0300

    UPD: protected/modules/yupe/models/Settings.php меняем CDbExpression на YDbMigration::expression

--- protected/modules/yupe/models/Settings.php
+++ protected/modules/yupe/models/Settings.php
@@ -58,7 +58,7 @@ class Settings extends YModel
 
     public function beforeSave()
     {
-        $this->change_date = new CDbExpression(Yii::app()->db->schema instanceof CSqliteSchema ? 'DATETIME("now")' :'NOW()');
+        $this->change_date = YDbMigration::expression('NOW()');
 
         if ($this->isNewRecord)
             $this->creation_date = $this->change_date;
