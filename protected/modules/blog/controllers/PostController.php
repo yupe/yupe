f32c7da4610ba7d9996a537ffad02cf678d72f1f
AKulikov <tuxuls@gmail.com>
2013-04-21 14:31:45 +0300

    fix #865: Добавлено ajax получения комментариев к посту

--- protected/modules/blog/controllers/PostController.php
+++ protected/modules/blog/controllers/PostController.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * Класс контроллера Post, для работы с постами блогов:
+ *
+ * @category YupeController
+ * @package  YupeCMS
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.6 (dev)
+ * @link     http://yupe.ru
+ *
+ **/
 class PostController extends YFrontController
 {
+    /**
+     * Показываем пост по урлу
+     * 
+     * @param string $slug - урл поста
+     * 
+     * @return void
+     */
     public function actionShow($slug)
     {
         $post = Post::model()->with(
@@ -17,6 +35,13 @@ class PostController extends YFrontController
         $this->render('show', array('post' => $post));
     }
 
+    /**
+     * Показываем посты по тегу
+     * 
+     * @param string $tag - Tag поста
+     * 
+     * @return void
+     */
     public function actionList($tag)
     {
         $tag = CHtml::encode($tag);
@@ -30,6 +55,43 @@ class PostController extends YFrontController
             'list', array(
                 'posts' => $posts,
                 'tag'   => $tag,
+            )
+        );
+    }
+
+    /**
+     * Обновляем список постов:
+     * 
+     * @return void
+     */
+    public function actionUpdatecomments()
+    {
+        if (!Yii::app()->request->isPostRequest || !Yii::app()->request->isAjaxRequest)
+            throw new CHttpException(404, Yii::t('BlogModule.blog', 'Страница не найдена!'));
+        
+        if (($postID = Yii::app()->request->getPost('postID')) === null || ($post = Post::model()->loadModel($postID)) === null)
+            Yii::app()->ajax->failure(
+                Yii::t(
+                    'BlogModule.blog', 'Запись #{postID} не найдена!', array(
+                        '{postID}' => $postID
+                    )
+                )
+            );
+
+        Yii::app()->ajax->success(
+            array(
+                'message' => Yii::t(
+                    'BlogModule.blog', 'Комментарии записи #{postID} успешно обновлены!', array(
+                        '{postID}' => $postID
+                    )
+                ),
+                'content' => $this->widget(
+                    'application.modules.comment.widgets.CommentsListWidget', array(
+                            'model' => $post,
+                            'modelId'  => $post->id,
+                            'comments' => $post->comments
+                    ), true
+                )
             )
         );
     }
--- themes/default/views/blog/post/show.php
+++ themes/default/views/blog/post/show.php
@@ -1,13 +1,28 @@
 <?php
+/**
+ * Отображение для post/show:
+ * 
+ *   @category YupeView
+ *   @package  YupeCMS
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ **/
 $this->pageTitle = $post->title;
 $this->description = $post->description;
 $this->keywords = $post->keywords;
+
+Yii::app()->clientScript->registerScript(
+    "ajaxBlogToken", "var ajaxToken = " . json_encode(
+        Yii::app()->request->csrfTokenName . '=' . Yii::app()->request->csrfToken
+    ) . ";", CClientScript::POS_BEGIN
+);
+
 $this->breadcrumbs = array(
     Yii::t('blog', 'Блоги') => array('/blog/blog/index/'),
     CHtml::encode($post->blog->name) => array('/blog/blog/show/', 'slug' => $post->blog->slug),
     CHtml::encode($post->title),
-);
-?>
+); ?>
 
 <div class="post">
     <div class="title">
@@ -38,29 +53,71 @@ $this->breadcrumbs = array(
     </div>
 </div>
 
-<script type="text/javascript">(function() {
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        $(document).on('click', '.post-updatecomments', function(){
+            var link = $(this);
+            var postID = link.attr('rel');
+            link.addClass('ajax-loading');
+            $.ajax({
+                url: '/blog/post/updatecomments',
+                data: ajaxToken + '&postID=' + postID,
+                dataType: 'json',
+                type: 'post',
+                success: function(data){
+                    if (data.result && data.data.content) {
+                        $('#comments').replaceWith(
+                            data.data.content
+                        ).before(
+                            "<div class='flash'><div class='flash-error'><b>" + data.data.message + "</b></div></div>"
+                        );
+                    } else {
+                        $('.comments').before("<div class='flash'><div class='flash-error'><b>" + data.data.message + "</b></div></div>");
+                    }
+                    link.removeClass('ajax-loading');
+                }
+            });
+            return false;
+        });
+    });
+    (function() {
         if(window.pluso) if(typeof window.pluso.start == "function") return;
         var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
         s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
         s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
         var h=d[g]('head')[0] || d[g]('body')[0];
         h.appendChild(s);
-    })();</script>
+    })();
+</script>
 <div class="pluso" data-options="small,round,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print" data-background="transparent"></div>
 
 <br/><br />
 
-<?php $this->widget('application.modules.comment.widgets.CommentsListWidget', array(
+<?php
+echo CHtml::link(
+    Yii::t('BlogModule.blog', 'Обновить комментарии'), 'javascript:void(0);', array(
+        'class' => 'post-updatecomments',
+        'rel'   => $post->id
+    )
+); ?>
+
+<?php
+$this->widget(
+    'application.modules.comment.widgets.CommentsListWidget', array(
         'model' => $post,
         'modelId'  => $post->id,
         'comments' => $post->comments
-)); ?>
+    )
+); ?>
 
 <br/><br />
 
-<h3><?php echo Yii::t('blog', 'Оставить комментарий'); ?></h3>
-<?php $this->widget('application.modules.comment.widgets.CommentFormWidget', array(
-    'redirectTo' => $this->createUrl('/blog/post/show/', array('slug' => $post->slug)),
-    'model' => $post,
-    'modelId' => $post->id,
-)); ?>
+<h3><?php echo Yii::t('BlogModule.blog', 'Оставить комментарий'); ?></h3>
+<?php
+$this->widget(
+    'application.modules.comment.widgets.CommentFormWidget', array(
+        'redirectTo' => $this->createUrl('/blog/post/show/', array('slug' => $post->slug)),
+        'model' => $post,
+        'modelId' => $post->id,
+    )
+); ?>
