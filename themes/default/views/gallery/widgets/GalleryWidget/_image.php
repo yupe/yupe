cb37f203d57b7acca2c0018da3a5709637577be1
AKulikov <tuxuls@gmail.com>
2013-04-23 20:15:26 +0300

    fix #693: генерация превьюшек для виджета Галереи

--- themes/default/views/gallery/widgets/GalleryWidget/_image.php
+++ themes/default/views/gallery/widgets/GalleryWidget/_image.php
@@ -1,6 +1,6 @@
 <div class="span-5 image">
 <?php
-    echo CHtml::link(CHtml::image($data->image->getUrl(), $data->image->alt),
+    echo CHtml::link(CHtml::image($data->image->getUrl(190), $data->image->alt),
     	$data->image->getUrl(), array('class' => 'fancybox', 'title' => $data->image->description, 'rel' => $data->gallery->id)
 	);
 ?>
