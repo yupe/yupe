9792f149137e67aaaf6944eabbe6d55d121b1436
AKulikov <tuxuls@gmail.com>
2013-04-24 01:29:04 +0300

    fix #691: Модуль gallery - редактирование фотографии на фронт-сайде

---
+++ themes/default/views/gallery/gallery/editimage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Отображение для gallery/editimage:
+ * 
+ *   @category YupeView
+ *   @package  YupeCMS
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ **/
+$this->pageTitle   = 'Галерея';
+$this->breadcrumbs = array(
+    'Галереи'                => array('/gallery/gallery/list'),
+    $model->gallery[0]->name => array('/gallery/gallery/show', 'id' => $model->gallery[0]->id),
+    Yii::t(
+        'GalleryModule.gallery', 'Редактирование изображения #{id}', array(
+            '{id}' => $model->id
+        )
+    )
+); ?>
+<h1><?php echo Yii::t('GalleryModule.gallery', 'Редактирование изображения #{id}', array('{id}' => $model->id)); ?></h1>
+
+<?php $this->renderPartial('_add_foto_form', array('model' => $model)); ?>
