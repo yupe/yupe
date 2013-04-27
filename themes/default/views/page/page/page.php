5eefe31f51c5ce94bf1bf9ee9054e48c414d3593
AKulikov <tuxuls@gmail.com>
2013-04-27 11:09:16 +0300

    fix #859: Ключевые слова и метаописания, которые ввели для каждой страницы контента, не отображались после опубликации

--- themes/default/views/page/page/page.php
+++ themes/default/views/page/page/page.php
@@ -1,6 +1,17 @@
 <?php
-    $this->pageTitle   = $page->title;
-    $this->breadcrumbs = $this->breadcrumbs;
+/**
+ * Отображение для page/page:
+ * 
+ *   @category YupeView
+ *   @package  YupeCMS
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ **/
+$this->pageTitle   = $page->title;
+$this->breadcrumbs = $this->breadcrumbs;
+$this->description = !empty($page->description) ? $page->description : $this->description;
+$this->keywords    = !empty($page->keywords)    ? $page->keywords    : $this->keywords
 ?>
 
 <h1><?php echo $page->title; ?></h1>
