3d7e008d8acf266a08fc1345065ff729cae74964
AKulikov <tuxuls@gmail.com>
2013-05-06 11:54:51 +0300

    UPD: behavior class fix alias

--- protected/modules/yupe/install/yupe.php
+++ protected/modules/yupe/install/yupe.php
@@ -4,14 +4,14 @@ return array(
         'class' => 'CFileCache',
         'behaviors' => array(
             'clear' => array(
-                'class' => 'yupe.extensions.tagcache.TaggingCacheBehavior',
+                'class' => 'application.modules.yupe.extensions.tagcache.TaggingCacheBehavior',
             ),
         ),
     ),
     'preload'   => array('log'),
     'component' => array(
         // параметры подключения к базе данных, подробнее http://www.yiiframework.ru/doc/guide/ru/database.overview
-        'db' => require(dirname(__FILE__) . '/../db.php'),
+        'db' => include_once dirname(__FILE__) . '/../db.php',
     ),
     'rules' => array(
         '/yupe/backend/modulesettings/<module:\w+>' => 'yupe/backend/modulesettings',
