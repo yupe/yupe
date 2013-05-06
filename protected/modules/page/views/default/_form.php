1352b0ad01f8c4010b12bae563153da20c27e70a
AKulikov <tuxuls@gmail.com>
2013-05-06 21:57:35 +0300

    fix #928: Исправлена ошибка запроса в постгрес при редактировании страницы

--- protected/modules/page/views/default/_form.php
+++ protected/modules/page/views/default/_form.php
@@ -1,12 +1,23 @@
 <?php
-$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-    'id'                     => 'page-form',
-    'enableAjaxValidation'   => false,
-    'enableClientValidation' => true,
-    'type'                   => 'vertical',
-    'htmlOptions'            => array('class' => 'well'),
-    'inlineErrors'           => true,
-)); ?>
+/**
+ * Отображение для default/_form:
+ * 
+ *   @category YupeView
+ *   @package  YupeCMS
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ **/
+$form = $this->beginWidget(
+    'bootstrap.widgets.TbActiveForm', array(
+        'id'                     => 'page-form',
+        'enableAjaxValidation'   => false,
+        'enableClientValidation' => true,
+        'type'                   => 'vertical',
+        'htmlOptions'            => array('class' => 'well'),
+        'inlineErrors'           => true,
+    )
+); ?>
     <div class="alert alert-info">
         <?php echo Yii::t('PageModule.page', 'Поля, отмеченные'); ?>
         <span class="required">*</span>
@@ -15,20 +26,20 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 
     <?php echo $form->errorSummary($model); ?>
 
-    <?php if(count($languages) > 1):?>
+    <?php if (count($languages) > 1) : ?>
         <?php echo $form->dropDownListRow($model, 'lang', $languages, array('class' => 'popover-help','empty' => Yii::t('NewsModule.news', '--выберите--'))); ?>
-        <?php if(!$model->isNewRecord):?>
-            <?php foreach($languages as $k => $v):?>
-                <?php if($k !== $model->lang):?>
-                    <?php if(empty($langModels[$k])):?>
-                        <a href="<?php echo $this->createUrl('/page/default/create',array('id' => $model->id,'lang'  => $k));?>"><i class="iconflags iconflags-<?php echo $k;?>" title="<?php echo Yii::t('NewsModule.news','Добавить перевод на {lang} язык',array('{lang}' => $v))?>"></i></a>
-                    <?php else:?>
-                        <a href="<?php echo $this->createUrl('/page/default/update',array('id' => $langModels[$k]));?>"><i class="iconflags iconflags-<?php echo $k;?>" title="<?php echo Yii::t('NewsModule.news','Редактировать перевод на {lang} язык',array('{lang}' => $v))?>"></i></a>
+        <?php if (!$model->isNewRecord) : ?>
+            <?php foreach ($languages as $k => $v) : ?>
+                <?php if ($k !== $model->lang) : ?>
+                    <?php if (empty($langModels[$k])) : ?>
+                        <a href="<?php echo $this->createUrl('/page/default/create', array('id' => $model->id, 'lang'  => $k)); ?>"><i class="iconflags iconflags-<?php echo $k; ?>" title="<?php echo Yii::t('NewsModule.news', 'Добавить перевод на {lang} язык', array('{lang}' => $v)); ?>"></i></a>
+                    <?php else : ?>
+                        <a href="<?php echo $this->createUrl('/page/default/update', array('id' => $langModels[$k])); ?>"><i class="iconflags iconflags-<?php echo $k; ?>" title="<?php echo Yii::t('NewsModule.news', 'Редактировать перевод на {lang} язык', array('{lang}' => $v)); ?>"></i></a>
                     <?php endif;?>
                 <?php endif;?>
             <?php endforeach;?>
         <?php endif;?>
-    <?php else:?>
+    <?php else : ?>
         <?php echo $form->hiddenField($model,'lang');?>
     <?php endif;?>
 
@@ -63,11 +74,14 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     <div class="row-fluid control-group <?php echo $model->hasErrors('body') ? 'error' : ''; ?>">
         <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('body'); ?>' data-content='<?php echo $model->getAttributeDescription('body'); ?>'>
             <?php echo $form->labelEx($model, 'body'); ?>
-            <?php $this->widget($this->module->editor, array(
-                'model'       => $model,
-                'attribute'   => 'body',
-                'options'     => $this->module->editorOptions,
-            )); ?>
+            <?php
+            $this->widget(
+                $this->module->editor, array(
+                    'model'       => $model,
+                    'attribute'   => 'body',
+                    'options'     => $this->module->editorOptions,
+                )
+            ); ?>
         </div>
     </div>
 
@@ -75,7 +89,7 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     <div class="accordion-group">
         <div class="accordion-heading">
             <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-                <?php echo Yii::t('PageModule.page','Данные для поисковой оптимизации');?>
+                <?php echo Yii::t('PageModule.page', 'Данные для поисковой оптимизации');?>
             </a>
         </div>
         <div id="collapseOne" class="accordion-body collapse">
@@ -93,15 +107,21 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 
     <br/><br/>
 
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-        'buttonType' => 'submit',
-        'type'       => 'primary',
-        'label'      => $model->isNewRecord ? Yii::t('blog', 'Добавить страницу и продолжить') : Yii::t('blog', 'Сохранить страницу и продолжить'),
-    )); ?>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-        'buttonType' => 'submit',
-        'htmlOptions'=> array('name' => 'submit-type', 'value' => 'index'),
-        'label'      => $model->isNewRecord ? Yii::t('blog', 'Добавить страницу и закрыть') : Yii::t('blog', 'Сохранить страницу и закрыть'),
-    )); ?>
+    <?php
+    $this->widget(
+        'bootstrap.widgets.TbButton', array(
+            'buttonType' => 'submit',
+            'type'       => 'primary',
+            'label'      => $model->isNewRecord ? Yii::t('blog', 'Добавить страницу и продолжить') : Yii::t('blog', 'Сохранить страницу и продолжить'),
+        )
+    ); ?>
+    <?php
+    $this->widget(
+        'bootstrap.widgets.TbButton', array(
+            'buttonType' => 'submit',
+            'htmlOptions'=> array('name' => 'submit-type', 'value' => 'index'),
+            'label'      => $model->isNewRecord ? Yii::t('blog', 'Добавить страницу и закрыть') : Yii::t('blog', 'Сохранить страницу и закрыть'),
+        )
+    ); ?>
 
 <?php $this->endWidget(); ?>
