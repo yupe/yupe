5fe469b7b920dc9f9654e036dd8c724783f12531
AKulikov <tuxuls@gmail.com>
2013-05-07 15:50:22 +0300

    UPD: правки по #534 и fix #955

--- protected/modules/yupe/components/YDbMigration.php
+++ protected/modules/yupe/components/YDbMigration.php
@@ -198,14 +198,87 @@ class YDbMigration extends CDbMigration
      */
     public function dropTableWithForeignKeys($table)
     {
-        /**$schema = $this->getDbConnection()->schema;
+        $schema = $this->getDbConnection()->schema;
+        
         if ($schema->getTable($table) !== null) {
-            // drop foreign keys
-            foreach ( $schema->getTable($table)->foreignKeys as $fk_name) {
-                $this->dropForeignKey($fk_name, $table);
+            $foreignKeys = (array) $this->getTableForeignKeys($table);
+
+            // Получаем форейнкеи от которых зависит таблица:
+            foreach ($schema->getTable($table)->foreignKeys as $foreignKey) {
+                $foreignKeys = array_merge(
+                    (array) $foreignKeys, (array) $this->getTableForeignKeys($foreignKey[0])
+                );
             }
+
+            // drop foreign keys
+            foreach ($foreignKeys as $tableName => $fkeys) {
+                foreach ($fkeys as $foreignKey) {
+                    $this->dropForeignKey($foreignKey, $tableName);
+                }
+            }
+            
+            // Удаляем таблицу:
             $this->dropTable($table);
-        }*/
-        $this->dropTable($table);
+        }
+    }
+
+    /**
+     * get Foreign key for table
+     *
+     * @param string $tableName - table name for getting foreign keys
+     *
+     * @return array of foreign keys
+     **/
+    public function getTableForeignKeys($tableName)
+    {
+        $schema      = $this->dbConnection->schema;
+        $command     = Yii::app()->db->createCommand();
+        $foreignKeys = array();
+        
+        if (!($schema instanceof CPgsqlSchema) && !($schema instanceof CMysqlSchema))
+            return array();
+
+        // Получаем форейн-кеи для заданной таблицы,
+        // для этого выполняем запрос вида:
+        // 
+        // SELECT conname AS fk_name
+        // FROM pg_constraint t
+        // LEFT JOIN pg_class ON t.conrelid  = pg_class.oid
+        // WHERE contype = 'f' AND relname = '$tableName'
+        // 
+        if ($schema instanceof CPgsqlSchema) {
+            $command->select('conname AS name');
+            $command->from('pg_constraint');
+            $command->setJoin('LEFT JOIN  pg_class ON pg_constraint.conrelid = pg_class.oid');
+            $command->where("contype = 'f' AND relname = '{$tableName}'");
+        }
+        
+        // Получаем форейн-кеи для заданной таблицы,
+        // для этого выполняем запрос вида:
+        // 
+        // SELECT CONSTRAINT_NAME AS name
+        // FROM information_schema.TABLE_CONSTRAINTS
+        // WHERE CONSTRAINT_TYPE =  'FOREIGN KEY'
+        // AND TABLE_SCHEMA =  'yupe'
+        // AND TABLE_NAME =  'yupe_blog_blog'
+        // 
+        if ($schema instanceof CMysqlSchema) {
+            list(, $dbName) = explode('dbname=', Yii::app()->db->connectionString);
+            $command->select('CONSTRAINT_NAME AS name');
+            $command->from('information_schema.TABLE_CONSTRAINTS');
+            $command->where(
+                "CONSTRAINT_TYPE = :consType AND TABLE_SCHEMA = :dbName AND TABLE_NAME = :tableName", array(
+                    ':consType'  => 'FOREIGN KEY',
+                    ':dbName'    => $dbName,
+                    ':tableName' => $this->normTable($tableName),
+
+                )
+            );
+        }
+        
+        foreach ((array) $command->queryAll() as $fkey)
+            $foreignKeys[$this->normTable($tableName)][] = $fkey['name'];
+
+        return $foreignKeys;
     }
 }
