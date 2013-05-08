9025657b2e92b311333f4eca5c9ce493871053d0
AKulikov <tuxuls@gmail.com>
2013-05-09 00:57:25 +0300

    UPD: add method isConfigNeedUpdate() to YWebModule

--- protected/modules/yupe/components/YWebModule.php
+++ protected/modules/yupe/components/YWebModule.php
@@ -5,14 +5,13 @@
  * Все модули, разработанные для Юпи! должны наследовать этот класс.
  * Панель управления Юпи!, собираемая из модулей, использует определенные методы этого базового класса.
  *
- * @package yupe.core.components
- * @abstract
- * @author yupe team
- * @version 0.0.3
- * @link http://yupe.ru - основной сайт
+ * @category YupeModule
+ * @package  YupeCms
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @link     http://yupe.ru
  *
  */
-
 abstract class YWebModule extends CWebModule
 {
     const CHECK_ERROR = 'error';
@@ -664,5 +663,25 @@ abstract class YWebModule extends CWebModule
     {
         return !(Yii::app()->migrator->checkForUpdates((array) $this->getId()))
                 && count(Yii::app()->migrator->getMigrationHistory($this->getId(), -1)) < 1;
+    }
+
+    /**
+     * Проверяем настройки модуля, на необходимость обновления:
+     *
+     * @return is module config need update
+     **/
+    public function isConfigNeedUpdate()
+    {
+        // Сверяем хеш суммы двух файлов
+        // и возвращаем обратный результат
+        // от полученного, то есть - требуется
+        // ли обновление:
+        return !(
+            md5_file(
+                Yii::getPathOfAlias($this->getId() . '.install.' . $this->getId())
+            ) === md5_file(
+                Yii::getPathOfAlias('application.config.modules.' . $this->getId())
+            )
+        );
     }
 }
