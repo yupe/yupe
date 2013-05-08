a358ad65eb97b827dc208cfee42a59ec08a7d108
AKulikov <tuxuls@gmail.com>
2013-05-09 00:56:53 +0300

    UPD: small fixes

--- protected/modules/yupe/controllers/BackendController.php
+++ protected/modules/yupe/controllers/BackendController.php
@@ -5,10 +5,9 @@
  * а также их настройками.
  *
  * @category YupeController
- * @package  Yupe
+ * @package  YupeCms
  * @author   YupeTeam <team@yupe.ru>
  * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
- * @version  0.5 (dev)
  * @link     http://yupe.ru
  *
  **/
@@ -17,10 +16,9 @@
  * Главный контроллер админ-панели:
  *
  * @category YupeController
- * @package  Yupe
+ * @package  YupeCms
  * @author   YupeTeam <team@yupe.ru>
  * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
- * @version  0.5 (dev)
  * @link     http://yupe.ru
  *
  **/
