e3774f858bf97bae8626e6c3f1737a27beb720da
Nickolay Sedov <yamikspark@gmail.com>
2013-06-29 16:09:59 +0500

    fancybox removed #923

--- themes/default/views/gallery/widgets/GalleryWidget/gallerywidget.php
+++ themes/default/views/gallery/widgets/GalleryWidget/gallerywidget.php
@@ -1,48 +1,50 @@
 <?php
 /**
  * Отображение для GalleryWidget/gallerywidget:
- * 
- *   @category YupeView
- *   @package  YupeCMS
- *   @author   Yupe Team <team@yupe.ru>
- *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
- *   @link     http://yupe.ru
+ *
+ * @category YupeView
+ * @package  YupeCMS
+ * @author   Yupe Team <team@yupe.ru>
+ * @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ * @link     http://yupe.ru
  **/
 $this->widget(
-    'gallery.extensions.fancybox.FancyBox', array(
-        'target' => '.fancybox',
-        'config' => array(
-            'helpers' => array('title' => array('type' => 'inside'))
+    'gallery.extensions.colorbox.ColorBox',
+    array(
+        'target' => '.gallery-image',
+        'lange' => 'ru',
+        'config' => array( // тут конфиги плагина, подробнее http://www.jacklmoore.com/colorbox
         ),
     )
 ); ?>
 
-
 <?php
-if ($gallery->status == Gallery::STATUS_PRIVATE && $gallery->owner != Yii::app()->user->id)
+if ($gallery->status == Gallery::STATUS_PRIVATE && $gallery->owner != Yii::app()->user->id) {
     echo CHtml::tag(
-        'h3', array(), Yii::t('GalleryModule.gallery', 'Это приватная галерея!')
+        'h3',
+        array(),
+        Yii::t('GalleryModule.gallery', 'Это приватная галерея!')
     );
-else
+} else {
     $this->widget(
-        'zii.widgets.CListView', array(
+        'zii.widgets.CListView',
+        array(
             'id' => 'gallery',
             'dataProvider' => $dataProvider,
-            'itemView'     => '_image',
+            'itemView' => '_image',
             'afterAjaxUpdate' => "function(id, data) { masonry('#gallery .items'); }",
         )
-    ); ?>
-
+    );
+} ?>
 
 <script type="text/javascript">
     function masonry(selector) {
         var $container = $(selector);
-        $container.imagesLoaded(function(){
+        $container.imagesLoaded(function () {
             this.isotope({
-                masonry: { itemSelector : '.image', columnWidth : $container.width() / 19 }
+                masonry: { itemSelector: '.image', columnWidth: $container.width() / 19 }
             });
         });
-    };
-    
+    }
     masonry('#gallery .items');
 </script>
