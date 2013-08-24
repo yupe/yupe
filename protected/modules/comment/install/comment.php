59911fe1ddd14c93dac7d613fb0dac5dc6cb4ad4
Anton Kucherov <idexter.ru@gmail.com>
2013-08-24 22:30:38 +0800

    Добавлен недостающий импорт NestedSetsBehavior

--- protected/modules/comment/install/comment.php
+++ protected/modules/comment/install/comment.php
@@ -5,6 +5,7 @@ return array(
     ),
     'import'    => array(
         'application.modules.comment.models.*',
+        'application.modules.yupe.extensions.nested-set-behavior.NestedSetBehavior',
     ),
     'component' => array(),
     'rules'     => array(),
