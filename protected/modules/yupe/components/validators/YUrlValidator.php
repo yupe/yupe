7436f6fbdf3c3f3d803ad943ac68e14b8ab36c26
Anton Kucherov <idexter.ru@gmail.com>
2013-09-04 19:10:41 +0800

    fix bug #1158

--- protected/modules/yupe/components/validators/YUrlValidator.php
+++ protected/modules/yupe/components/validators/YUrlValidator.php
@@ -7,5 +7,12 @@
 
 class YUrlValidator extends CUrlValidator
 {
-    public $pattern='/^{schemes}:\/\/(([A-ZА-Я0-9][A-ZА-Я0-9_-]*)(\.[A-ZА-Я0-9][A-ZА-Я0-9_-]*)+)/iu';
+    public $pattern = '/^{schemes}:\/\/(([A-ZА-Я0-9][A-ZА-Я0-9_-]*)(\.[A-ZА-Я0-9][A-ZА-Я0-9_-]*)+)/iu';
+    public $clientPattern = '/^{schemes}:\/\/(([A-ZА-Я0-9][A-ZА-Я0-9_-]*)(\.[A-ZА-Я0-9][A-ZА-Я0-9_-]*)+)/i';
+
+    public function clientValidateAttribute($object,$attribute)
+    {
+        $this->pattern = $this->clientPattern;
+        parent::clientValidateAttribute($object,$attribute);
+    }
 }
