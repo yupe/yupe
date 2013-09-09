9a5afcd7968737f92951e19e3bada330fe7ab0b9
Anton Kucherov <idexter.ru@gmail.com>
2013-09-09 09:31:29 +0800

    Реорганизация  userspace.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
  *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
  *   @link     http://yupe.ru
  **/
+
 // подробнее про index.php http://www.yiiframework.ru/doc/guide/ru/basics.entry
 if (!ini_get('date.timezone'))
     date_default_timezone_set('UTC');
@@ -24,18 +25,27 @@ if (strpos($_SERVER['SERVER_ADDR'], '127') === 0) {
     $yii = dirname(__FILE__) . '/framework/yii.php';
 
     // путь к основному конфигурационному файлу Yii
-    $config = dirname(__FILE__) . '/protected/config/main-development.php';
+    $userspace = require_once dirname(__FILE__) . '/protected/config/userspace-development.php';
 
 } else { //production считается во всех случаях, когда не выполнены условия
     // путь к фреймворку Yii, при необходимости заменить на yii.php
     $yii = dirname(__FILE__) . '/framework/yiilite.php';
     // путь к основному конфигурационному файлу Yii
-    $config = dirname(__FILE__) . '/protected/config/main.php';
+    $userspace = require_once dirname(__FILE__) . '/protected/config/userspace.php';
 }
+
+$base = require_once dirname(__FILE__) . '/protected/config/main.php';
 
 require $yii;
 
 Yii::setPathOfAlias('application', dirname(__FILE__) . '/protected/');
 Yii::setPathOfAlias('yii', dirname(__FILE__) . '/framework/');
+
+use \Yii;
+use \CMap;
+use application\modules\yupe\components\ConfigManager;
+
+$confManager = new ConfigManager();
+$config = $confManager->merge($base, $userspace);
 
 Yii::createWebApplication($config)->run();
