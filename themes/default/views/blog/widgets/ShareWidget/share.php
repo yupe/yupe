fea34f4df026c369ad547e3c9440d2c676ad4d65
yupe <aopeykin@yandex.ru>
2013-09-11 19:59:26 +0400

    код для соц. кнопок вынесен в отдельный виджет

---
+++ themes/default/views/blog/widgets/ShareWidget/share.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    (function() {
+        if(window.pluso) if(typeof window.pluso.start == "function") return;
+        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+        var h=d[g]('head')[0] || d[g]('body')[0];
+        h.appendChild(s);
+    })();
+</script>
+<div class="pluso" data-options="small,round,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print" data-background="transparent"></div>
