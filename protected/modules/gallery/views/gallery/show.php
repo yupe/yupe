2036e11b727a85418185ba64d142cf7b1ed3f210
Nickolay Sedov <yamikspark@gmail.com>
2013-09-13 02:13:55 +0500

    gallery/gallery views refactored

--- protected/modules/gallery/views/gallery/show.php
+++ protected/modules/gallery/views/gallery/show.php
@@ -1,24 +1,28 @@
-<h1>
-    <?php echo Yii::t('GalleryModule.gallery', 'Галерея'); ?>
-    "<?php echo CHtml::encode($model->name); ?>"
-</h1>
-
-<p><?php echo CHtml::encode($model->description); ?></p>
-
-<p><?php echo Yii::t('GalleryModule.gallery', 'Количество фото'); ?>
-    : <?php echo $model->imagesCount; ?></p>
-
-<?php $this->widget('zii.widgets.CListView', array(
-    'dataProvider' => $dataProvider,
-    'itemView'     => '_foto_view',
-)); ?>
-
-<?php if (Yii::app()->user->isAuthenticated()): ?>
-    <div id="add-image-form">
-        <h1><?php echo Yii::t('GalleryModule.gallery', 'Добавление фото'); ?></h1>
-        <?php $this->renderPartial('_add_foto_form', array('model' => $image, 'gallery' => $model)); ?>
-    </div>
-<?php else: ?>
-    <?php echo Yii::t('GalleryModule.gallery', 'Для добавления фото Вам необходимо ') ?>
-    <?php echo CHtml::link(Yii::t('GalleryModule.gallery', 'авторизоваться'), array('/user/account/login/')); ?>!
-<?php endif; ?>
+<?php
+/**
+ * Отображение для gallery/show:
+ *
+ * @category YupeView
+ * @package  YupeCMS
+ * @author   Yupe Team <team@yupe.ru>
+ * @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ * @link     http://yupe.ru
+ **/
+?>
+<div class="gallery-show">
+    <?php $this->pageTitle = 'Галерея'; ?>
+    <?php $this->breadcrumbs = array(
+        Yii::t('GalleryModule.gallery', 'Галереи') => array('/gallery/gallery/list'), $model->name
+    ); ?>
+    <h1 class="page-header"><?php echo CHtml::encode($model->name); ?></h1>
+    <?php echo $model->description; ?>
+    <?php $this->widget(
+        'gallery.widgets.GalleryWidget',
+        array('galleryId' => $model->id, 'gallery' => $model, 'limit' => 30)
+    ); ?>
+    <?php if (Yii::app()->user->isAuthenticated()) : ?>
+        <?php if ($model->canAddPhoto) : ?>
+            <?php $this->renderPartial('_form', array('model' => $image, 'gallery' => $model)); ?>
+        <?php endif ?>
+    <?php endif; ?>
+</div>
---
+++ themes/default/views/gallery/gallery/_form.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Отображение для gallery/edit-image:
+ *
+ * @category YupeView
+ * @package  YupeCMS
+ * @author   Yupe Team <team@yupe.ru>
+ * @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ * @link     http://yupe.ru
+ **/
+?>
+<div class="form well">
+    <?php $form = $this->beginWidget(
+        'bootstrap.widgets.TbActiveForm',
+        array(
+            'id' => 'add-image-form',
+            'enableClientValidation' => true,
+            'htmlOptions' => array('enctype' => 'multipart/form-data')
+        )
+    ); ?>
+
+    <legend>
+        <?php echo $model->getIsNewRecord()
+            ? Yii::t('GalleryModule.gallery', 'Добавление изображения')
+            : Yii::t('GalleryModule.gallery', 'Редактирование изображения'); ?>
+    </legend>
+
+    <?php echo $form->errorSummary($model); ?>
+
+    <?php if ($model->file !== null) : ?>
+        <div class="row-fluid">
+            <?php echo CHtml::image($model->getUrl(190), $model->alt); ?>
+        </div>
+    <?php endif; ?>
+
+    <div class='row-fluid control-group <?php echo $model->hasErrors('file') ? 'error' : ''; ?>'>
+        <?php echo $form->fileFieldRow($model, 'file', array('class' => 'span6', 'required' => false)); ?>
+    </div>
+
+    <div class='row-fluid control-group <?php echo $model->hasErrors('name') ? 'error' : ''; ?>'>
+        <?php echo $form->textFieldRow($model, 'name', array('class' => 'span6', 'required' => true)); ?>
+    </div>
+
+    <div class='row-fluid control-group <?php echo $model->hasErrors('description') ? 'error' : ''; ?>'>
+        <?php echo $form->textAreaRow(
+            $model,
+            'description',
+            array('class' => 'span12', 'required' => true, 'rows' => 7, 'cols' => 30)
+        ); ?>
+    </div>
+
+    <div class='row-fluid control-group <?php echo $model->hasErrors('alt') ? 'error' : ''; ?>'>
+        <?php echo $form->textFieldRow($model, 'alt', array('class' => 'span6', 'required' => true)); ?>
+    </div>
+
+    <div class="row-fluid  control-group">
+        <?php
+        $this->widget(
+            'bootstrap.widgets.TbButton',
+            array(
+                'buttonType' => 'submit',
+                'type' => 'primary',
+                'icon' => 'picture',
+                'label' => $model->getIsNewRecord()
+                    ? Yii::t('GalleryModule.gallery', 'Добавить изображение')
+                    : Yii::t('GalleryModule.gallery', 'Обновить изображение')
+            )
+        ); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+</div>
