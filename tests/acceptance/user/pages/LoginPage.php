420450506b5359c5f4273b268f2dc900493e2574
yupe <aopeykin@yandex.ru>
2013-09-13 12:47:55 +0400

    рефакторинг тестов

---
+++ tests/acceptance/user/pages/LoginPage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aopeykin
+ * Date: 13.09.13
+ * Time: 12:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+class LoginPage
+{
+    public static $URL = '/user/account/login';
+
+    public static $passwordField = 'LoginForm[password]';
+
+    public static $emailField = 'LoginForm[email]';
+}
