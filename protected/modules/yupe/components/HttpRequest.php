ff1f5944e1af1ba712b63313f064987c566c0b08
AKulikov <tuxuls@gmail.com>
2013-09-20 22:15:48 +0300

    UPD: Правки касательно перевода WebModule на namespace\ы

---
+++ protected/modules/yupe/components/HttpRequest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * CHttpRequest переопределен для загрузки файлов через ajax, подробнее:
+ * http://www.yiiframework.com/forum/index.php/topic/8689-disable-csrf-verification-per-controller-action/
+ * 
+ */
+
+namespace yupe\components;
+
+use CHttpRequest;
+use Yii;
+
+class HttpRequest extends CHttpRequest
+{
+    public $noCsrfValidationRoutes = array();
+
+    protected function normalizeRequest()
+    {
+        parent::normalizeRequest();
+
+        if ($this->enableCsrfValidation)
+        {
+            foreach ($this->noCsrfValidationRoutes as $route)
+            {
+                if (strpos($this->pathInfo, $route) === 0) {
+                    Yii::app()->detachEventHandler('onBeginRequest', array($this, 'validateCsrfToken'));
+                }
+            }
+        }
+    }
+
+    public function urlReferer($urlIfNull = null)
+    {
+        return isset($_SERVER['HTTP_REFERER'])
+            ? $_SERVER['HTTP_REFERER']
+            : $urlIfNull;
+    }
+}
