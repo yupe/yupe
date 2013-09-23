db57d22ac996ca676f4c31b15ccf105a79f24839
Куликов Евгений <tuxuls@gmail.com>
2013-09-24 00:02:16 +0300

    Update docs.php

--- protected/modules/docs/install/docs.php
+++ protected/modules/docs/install/docs.php
@@ -1,8 +1,5 @@
 <?php
 return array(
-    'module'   => array(
-        'class'           => 'application.modules.docs.DocsModule',
-    ),
     'import'    => array(),
     'rules'     => array(
         '/docs/<moduleID:[a-zA-Z0-9\-_.]+>/<file:[a-zA-Z0-9\-_.]+>.html' => 'docs/show/index',
@@ -13,9 +10,10 @@ return array(
         '/docs'                                                          => 'docs/show/index',
     ),
     'module' => array(
-        'preload' => array('bootstrap'),
-        'components' => array(
-            'bootstrap' => array(
+        'class'           => 'application.modules.docs.DocsModule',
+        'preload'         => array('bootstrap'),
+        'components'      => array(
+            'bootstrap'   => array(
                 'class'          => 'vendor.clevertech.yii-booster.src.components.Bootstrap',
                 'coreCss'        => true,
                 'responsiveCss'  => true,
