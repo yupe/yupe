ca2182d2ffb3524b764025a7017b173e6908573f
yupe <aopeykin@yandex.ru>
2013-09-26 23:26:08 +0400

    acceptance tests for news module

---
+++ tests/acceptance/news/NewsPublishCest.php
@@ -0,0 +1,34 @@
+<?php
+namespace news;
+use \WebGuy;
+
+class NewsPublishCest
+{
+    public function tryToTest(WebGuy $I, $scenario)
+    {
+        $I = new WebGuy\UserSteps($scenario);
+        $I->login('yupe@yupetest.ru','111111');
+        $I->am('admin');
+        $I->amGoingTo('test publishing news...');
+        $I->amOnPage('/yupe/backend/index');
+        $I->see('Панель управления "Юпи!"','h1');
+        $I->amOnPage('/news/default/index');
+        $I->see('Новости');
+        $I->seeLink('Вторая не опубликованная новость');
+        $I->click('Вторая не опубликованная новость');
+        $I->see('Редактирование новости');
+        $I->see('Вторая не опубликованная новость');
+        $I->fillField('News[status]',1);
+        $I->click('Сохранить новость и продолжить');
+        $I->see('Новость обновлена!',\CommonPage::SUCCESS_CSS_CLASS);
+
+        $I->logout();
+        $I->am('anonymous user');
+        $I->amGoingTo('test show just published news...');
+        $I->amOnPage(\NewsPage::route('vtoraja-ne-opublikovannaja-novost'));
+        $I->expectTo('see just published news...');
+        $I->see('Вторая не опубликованная новость','h4');
+        $I->see('Вторая не опубликованная новость текст');
+        $I->seeInTitle('Вторая не опубликованная новость');
+    }
+}
---
+++ tests/acceptance/news/NewsShowCest.php
@@ -0,0 +1,48 @@
+<?php
+namespace news;
+use \WebGuy;
+
+class NewsShowCest
+{
+    public function tryToTestNewsPage(WebGuy $I, $scenario)
+    {
+        $I->am('anonymous user');
+        $I->amGoingTo('test show news page...');
+        $I->amOnPage(\NewsPage::route('pervaja-opublikovannaja-novost'));
+        $I->expectTo('see published news...');
+        $I->see('Первая опубликованная новость','h4');
+        $I->see('Первая опубликованная текст');
+        $I->seeInTitle('Первая опубликованная новость');
+
+        $I->amGoingTo('test show not published news...');
+        $I->amOnPage(\NewsPage::route('vtoraja-ne-opublikovannaja-novost'));
+        $I->expectTo(' see page not found exception...');
+        $I->dontSee('Вторая не опубликованная новость');
+        $I->dontSeeInTitle('Вторая не опубликованная новость');
+
+        $I->amGoingTo('test show protected news...');
+        $I->amOnPage(\NewsPage::route('tretja-novost-tolko-dlja-avtorizovannyh'));
+        $I->expectTo(' see login page...');
+        $I->dontSee('Третья новость только для авторизованных');
+        $I->dontSee('Третья новость только для авторизованных текст');
+        $I->dontSeeInTitle('Третья новость только для авторизованных текст');
+        $I->seeInCurrentUrl('login');
+        $I->see('Для просмотра этой страницы Вам необходимо авторизоваться!', \CommonPage::ERROR_CSS_CLASS);
+
+        $I->am('authorized user');
+        $I = new WebGuy\UserSteps($scenario);
+        $I->login('yupe@yupetest.ru','111111');
+        $I->amGoingTo('test show protected news for authorized user...');
+        $I->amOnPage(\NewsPage::route('tretja-novost-tolko-dlja-avtorizovannyh'));
+        $I->expectTo(' see protected news...');
+        $I->see('Третья новость только для авторизованных','h4');
+        $I->see('Третья новость только для авторизованных текст');
+        $I->seeInTitle('Третья новость только для авторизованных');
+
+        $I->amGoingTo('test show not published news...');
+        $I->amOnPage(\NewsPage::route('vtoraja-ne-opublikovannaja-novost'));
+        $I->expectTo(' see page not found exception...');
+        $I->dontSee('Вторая не опубликованная новость');
+        $I->dontSeeInTitle('Вторая не опубликованная новость');
+    }
+}
