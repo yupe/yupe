8a1c6ea7e40a2e6ebe6b9ff35c4a77f971262790
yupe <aopeykin@yandex.ru>
2013-09-30 19:29:16 +0400

    small theme fix

--- themes/default/views/news/news/_view.php
+++ themes/default/views/news/news/_view.php
@@ -1,18 +1,13 @@
-<div class="post">
-    <div class="title">
-        <?php echo CHtml::link(CHtml::encode($data->title), array('/news/news/show', 'title' => $data->alias)); ?>
-    </div>
-    <div class="author">
-        Опубликовал <b><?php echo $data->user->nick_name?></b>
-        дата: <?php echo $data->creation_date; ?>
-    </div>
-    <br/>
-
-    <div class="content">
-        <p><?php echo $data->short_text; ?></p>
-    </div>
-    <div class="nav">
-        <?php echo CHtml::link('Постоянная ссылка', array('/news/news/show', 'title' => $data->alias));?>
-        | последнее обновление <?php echo $data->change_date;?>
+<div class="row">
+    <div class="span8">
+        <h4><strong><?php echo CHtml::link(CHtml::encode($data->title), array('/news/news/show/', 'slug' => $data->alias)); ?></strong></h4>
     </div>
 </div>
+<div class="row">
+    <div class="span8">
+        <p> <?php echo $data->short_text; ?></p>
+        <p><?php echo CHtml::link(Yii::t('default','читать...'), array('/news/news/show/', 'slug' => $data->alias),array('class' => 'btn'));?></p>
+    </div>
+</div>
+
+<hr>
