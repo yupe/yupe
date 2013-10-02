a22c3e65a26beb16b665a8c2df9b9026892e4939
Anton Kucherov <idexter.ru@gmail.com>
2013-10-03 00:46:53 +0800

    Небольшой скрипт для возврата Yupe к состоянию перед установкой

---
+++ read.me/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -R protected/runtime/cache/*
+rm -R protected/runtime/CSS/*
+rm -R protected/runtime/HTML/*
+rm -R protected/runtime/debug/*
+rm -R protected/runtime/URI/*
+rm -R protected/runtime/application.log*
+
+rm -R public/assets/*
+rm -R protected/config/modules/*
