5f46a1d4cf4ddbf7ff66d9b7ce5649ca620013f9
akulikov <tuxuls@icloud.com>
2013-10-07 00:40:33 +0300

    UPD: fix #1199: Исправлено как предложил @yupe

--- protected/modules/user/widgets/AvatarWidget.php
+++ protected/modules/user/widgets/AvatarWidget.php
@@ -1,28 +1,88 @@
 <?php
-Yii::import('image.widgets.ImageWrapper');
-
 /*
  * Виджет для вывода аватарки
  */
-class AvatarWidget extends ImageWrapper
+class AvatarWidget extends CWidget
 {
+    public $width = '100px';
+    /**
+     * Для достаточно указать только $width
+     * @var string 
+     */
+    public $height;
+    /**
+     * Цвет рамки 
+     * @var string 
+     */
+    public $backgroundColor = '#000';
+    /**
+     * URL изображения, помещаемого в рамку
+     * @var string
+     */
+    public $imageSrc;
+    /**
+     * Альтернативный текст изображения
+     * @var string 
+     */
+    public $imageAlt = '';
+    /**
+     * Если истина - предотвращает кэширование добавлением значения microtime()
+     * к src изображения
+     * @var boolean 
+     */
+    public $noCache = false;
+    
+    public $htmlOptions = array();
+    
     /**
      * Модель пользователя
      * @var User
      */
     public $user;
+
     /**
      * Размер аватарки
      * @var int Пикселей
      */
     public $size = 100;
     
-    public function run() {
+    public function run()
+    {
         $this->imageSrc = $this->user->getAvatar($this->size); 
         $this->imageAlt = $this->user->nick_name;
         $this->width = $this->size . 'px';
         $this->htmlOptions = array('class' => 'avatar avatar-' . $this->user->id);
         
-        parent::run();
+        if(!$this->height) {
+            $this->height = $this->width;
+        }
+        
+        Yii::app()->clientScript->registerCssFile(
+            Yii::app()->assetManager->publish(
+                Yii::getPathOfAlias('user.assets.css').'/image-wrapper.css'
+            )
+        );
+        
+        $htmlOptions = array(
+            'class' => 'img-wrapper-tocenter',
+            'style' => 'width: ' . $this->width . '; height: ' . $this->height . '; background-color: '. $this->backgroundColor .';',
+        );
+        
+        if(isset($this->htmlOptions['class'])) {
+            $class = $this->htmlOptions['class'];
+            unset($this->htmlOptions['class']);
+            $htmlOptions['class'] .= ' ' . $class;
+        }
+        
+        if(isset($this->htmlOptions['style'])) {
+            $style = $this->htmlOptions['style'];
+            unset($this->htmlOptions['style']);
+            $htmlOptions['style'] .= ' ' . $style;
+        }
+        
+        if(is_array($this->htmlOptions) && count($this->htmlOptions) > 0)
+            $htmlOptions = array_merge($htmlOptions, $this->htmlOptions);
+        
+        echo CHtml::tag('div', $htmlOptions, '<span></span>' . CHtml::image($this->imageSrc . ( $this->noCache ? '?'.microtime() : ''),  $this->imageAlt));
     }
 }
