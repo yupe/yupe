49e5481861082e59cc29b4d85f139405326e507b
akulikov <tuxuls@icloud.com>
2013-10-07 03:13:36 +0300

    UPD: fix #1111: Исправлена ошибка при создании заданий (модуль queue) из за неверно сформированных индексов

---
+++ protected/modules/queue/install/migrations/m131007_031000_queue_fix_index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * File Doc Comment
+ * Queue fix index migration
+ * Класс миграций для модуля Queue:
+ *
+ * @category YupeMigration
+ * @package  yupe
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
+class m131007_031000_queue_fix_index extends yupe\components\DbMigration
+{
+
+    public function safeUp()
+    {
+        $this->dropIndex("ux_{{queue_queue}}_worker", '{{queue_queue}}');
+        $this->dropIndex("ux_{{queue_queue}}_priority", '{{queue_queue}}');
+
+        $this->createIndex("ux_{{queue_queue}}_worker", '{{queue_queue}}', "worker");
+        $this->createIndex("ux_{{queue_queue}}_priority", '{{queue_queue}}', "priority");
+    }
+ 
+
+    public function safeDown()
+    {
+    	
+    }
+}
