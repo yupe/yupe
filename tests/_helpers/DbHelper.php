710116db893c375fcc1b622e0c8fb1d5ac8b8a06
Anton Kucherov <idexter.ru@gmail.com>
2013-10-08 23:39:30 +0800

    Решена проблема использования данных для теста установщика из модуля  Db

---
+++ tests/_helpers/DbHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Codeception\Module;
+
+
+class DbHelper extends \Codeception\Module\Db
+{
+    /**
+     * Gets Driver from Db module and adds some sugar.
+     * Yo ho ho !!! :D
+     * @return array dbConfig
+     */
+    public function getDbConfig()
+    {
+        $dbConfig = $this->config;
+        $dbConfig["dbname"] = preg_replace('/.+dbname=(.+)$/is','$1',$dbConfig["dsn"]);
+        return $dbConfig;
+    }
+}
