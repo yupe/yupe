60327e738b9da73b5ad5531fffa44d3df1fdddcc
yupe <aopeykin@yandex.ru>
2013-10-08 23:07:11 +0400

    #1208

--- themes/default/views/blog/blog/index.php
+++ themes/default/views/blog/blog/index.php
@@ -1,14 +1,16 @@
-<?php
-    $this->pageTitle = Yii::t('blog', 'Блоги');
-    $this->breadcrumbs = array(Yii::t('blog', 'Блоги'));
-?>
+<?php $this->pageTitle = Yii::t('default', 'Блоги'); ?>
+<?php $this->breadcrumbs = array(Yii::t('default', 'Блоги')); ?>
 
-<h1><small>Блоги <a href="<?php echo Yii::app()->createUrl('/blog/rss/feed/');?>"><img src="<?php echo Yii::app()->theme->baseUrl?>/web/images/rss.png" alt="Подпишитесь на обновления" title="Подпишитесь на обновления"></a></small></h1>
+<h1>
+    <small><?php echo Yii::t('default','Блоги');?> <a href="<?php echo Yii::app()->createUrl('/blog/rss/feed/');?>">
+    <img src="<?php echo Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/web');?>/images/rss.png" alt="<?php echo Yii::t('default','Подпишитесь на обновления');?>" title="<?php echo Yii::t('default','Подпишитесь на обновления');?>"></a></small>
+</h1>
 
 <?php
 $this->widget(
     'bootstrap.widgets.TbListView', array(
         'dataProvider' => $dataProvider,
         'itemView' => '_view',
+        'template' => '{items}'
     )
 ); ?>
