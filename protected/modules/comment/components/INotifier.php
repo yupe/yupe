7659cdfd0b0ff128ef6c74ef7f2a2d2e303e1e1f
akulikov <tuxuls@icloud.com>
2013-10-09 17:20:39 +0300

    UPD: В CommentModule добавлен интерфейс для компонента Notifier

---
+++ protected/modules/comment/components/INotifier.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Notifier Interface:
+ *
+ * @category YupeComponents
+ * @package  yupe.modules.comments.components
+ * @author   AKulikov <tuxuls@gmail.com>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.1
+ * @link     http://yupe.ru
+ *
+ **/
+
+interface INotifier
+{
+	/**
+     * Comment Notifier Function
+     *
+     * @param CModelEvent $event - инстанс события
+     *
+     * @return bool отправилось ли письмо
+     **/
+	public function newComment(CModelEvent $event = null);
+}
--- protected/modules/comment/components/Notifier.php
+++ protected/modules/comment/components/Notifier.php
@@ -4,7 +4,7 @@
  * Notifier Class:
  *
  * @category YupeComponents
- * @package  yupe
+ * @package  yupe.modules.comments.components
  * @author   AKulikov <tuxuls@gmail.com>
  * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
  * @version  0.1
@@ -12,18 +12,7 @@
  *
  **/
 
-/**
- * Notifier Class:
- *
- * @category YupeComponents
- * @package  yupe
- * @author   AKulikov <tuxuls@gmail.com>
- * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
- * @version  0.1
- * @link     http://yupe.ru
- *
- **/
-class Notifier
+class Notifier implements INotifier
 {
     /**
      * Comment Notifier Function
