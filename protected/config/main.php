bd2ea59652f0aeecca6ba5eacf0128aa13e61606
akulikov <tuxuls@icloud.com>
2013-10-14 11:51:34 +0300

    UPD: doc-comment for ./protected/config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Файл основных настроек приложения
+ *
+ * ВНИМАНИЕ! ДАННЫЙ ФАЙЛ ИСПОЛЬЗУЕТСЯ ЯДРОМ YUPE!
+ * ИЗМЕНЕНИЯ В ДАННОМ ФАЙЛЕ МОГУТ ПРИВЕСТИ К ПОТЕРЕ РАБОТОСПОСОБНОСТИ
+ * Для собственных настроек создайте и используйте "/protected/config/userspace.php"
+ * Подробную информацию по использованию "userspace" можно узнать из официальной
+ * документации.
+ *
+ * @category YupeConfig
+ * @package  Yupe
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.6
+ * @link     http://yupe.ru
+ *
+ **/
+
 // Определяем алиасы:
 Yii::setPathOfAlias('application', dirname(__FILE__) . '/../');
 Yii::setPathOfAlias('yupe', dirname(__FILE__) . '/../modules/yupe/');
