8685a659f21dc4a1176d0bb8893cc90d6624645b
Куликов Евгений <angel-kulikov@users.noreply.github.com>
2013-10-15 22:44:42 +0300

    fix #1239

--- protected/config/db.back.php
+++ protected/config/db.back.php
@@ -6,8 +6,8 @@ return array (
 	'password' => '<db.pass>',
 	'emulatePrepare' => true,
 	'charset' => 'utf8',
-	'enableParamLogging' => 0,
-	'enableProfiling' => 0,
+	'enableParamLogging' => defined('YII_DEBUG') && YII_DEBUG ? true : 0,
+	'enableProfiling' => defined('YII_DEBUG') && YII_DEBUG ? true : 0,
 	'schemaCachingDuration' => 108000,
 	'tablePrefix' => 'yupe_',
 );
