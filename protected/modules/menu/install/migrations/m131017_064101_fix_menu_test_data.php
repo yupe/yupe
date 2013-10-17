c0dcf5175b9685f1eb6ac5e7967f9e460cd6b72c
akulikov <tuxuls@icloud.com>
2013-10-17 11:41:34 +0300

    UPD: small fixes

--- protected/modules/menu/install/migrations/m131017_064101_fix_menu_test_data.php
+++ protected/modules/menu/install/migrations/m131017_064101_fix_menu_test_data.php
@@ -14,50 +14,50 @@ use yupe\components\DAO;
 
 class m131017_064101_fix_menu_test_data extends yupe\components\DbMigration
 {
-	// Пары значений:
-	// старое -> новое
-	protected $replaces = array(
-		'/'             => '/site/index',
-		'/contacts'     => '/feedback/contact/index',
-		'/login'        => '/user/account/login',
-		'/logout'       => '/user/account/logout',
-		'/registration' => '/user/account/registration',
-		'/faq'          => 'feedback/contact/faq',
-	);
+    // Пары значений:
+    // старое -> новое
+    protected $replaces = array(
+        '/'             => '/site/index',
+        '/contacts'     => '/feedback/contact/index',
+        '/login'        => '/user/account/login',
+        '/logout'       => '/user/account/logout',
+        '/registration' => '/user/account/registration',
+        '/faq'          => 'feedback/contact/faq',
+    );
 
-	public function safeUp()
-	{
-		// Проходим по массиву выполняя замены:
-		foreach ($this->replaces as $old => $new) {
-			DAO::wrapper()
-				->in('{{menu_menu_item}}')
-				->where('href = :link', array(':link' => $old))
-				->update(
-					array(
-						'href' => $new,
-					)
-				);
-		}
-	}
+    public function safeUp()
+    {
+        // Проходим по массиву выполняя замены:
+        foreach ($this->replaces as $old => $new) {
+            DAO::wrapper()
+                ->in('{{menu_menu_item}}')
+                ->where('href = :link', array(':link' => $old))
+                ->update(
+                    array(
+                        'href' => $new,
+                    )
+                );
+        }
+    }
 
-	/**
-	 * Всё тоже самое что и в накате миграции,
-	 * только поменяны местами элементы:
-	 * 
-	 * @return void
-	 */
-	public function safeDown()
-	{
-		// Проходим по массиву выполняя замены:
-		foreach ($this->replaces as $new => $old) {
-			DAO::wrapper()
-				->in('{{menu_menu_item}}')
-				->where('href = :link', array(':link' => $old))
-				->update(
-					array(
-						'href' => $new,
-					)
-				);
-		}
-	}
+    /**
+     * Всё тоже самое что и в накате миграции,
+     * только поменяны местами элементы:
+     * 
+     * @return void
+     */
+    public function safeDown()
+    {
+        // Проходим по массиву выполняя замены:
+        foreach ($this->replaces as $new => $old) {
+            DAO::wrapper()
+                ->in('{{menu_menu_item}}')
+                ->where('href = :link', array(':link' => $old))
+                ->update(
+                    array(
+                        'href' => $new,
+                    )
+                );
+        }
+    }
 }
