1e674f114d1462ff23f75edafd20cd7803769e6b
akulikov <tuxuls@icloud.com>
2013-10-17 14:41:24 +0300

    UPD: fix #1154: Модуль ZendSearch требует наличия iconv-расширения, поправили работу модуля, исправили ошибки

--- protected/modules/zendsearch/controllers/ManageBackendController.php
+++ protected/modules/zendsearch/controllers/ManageBackendController.php
@@ -62,39 +62,56 @@ class ManageBackendController extends yupe\components\controllers\BackController
             Zend_Search_Lucene_Analysis_Analyzer::setDefault($analyzer);
             $index = new Zend_Search_Lucene(Yii::getPathOfAlias('application.' . $indexFiles), true);
 
-            // Пробежаться по всем моделям и добавить их в индекс
-            foreach ($searchModels as $modelName => $model) {
+            $messages = array();
 
-                if (is_file(Yii::getPathOfAlias($model['path']) . '.php')) {
-                    $searchNodes = $modelName::model()->findAll();
-                    foreach ($searchNodes as $node) {
-                        $doc = new Zend_Search_Lucene_Document();
-                        $doc->addField(Zend_Search_Lucene_Field::Text('title', CHtml::encode($node->$model['titleColumn']), 'UTF-8'));
-                        $link = str_replace('{' . $model['linkColumn'] . '}', $node->$model['linkColumn'], $model['linkPattern']);
-                        $doc->addField(Zend_Search_Lucene_Field::Text('link', $link, 'UTF-8'));
-                        $contentColumns = explode(',', $model['textColumns']);
-                        $i = 0;
-                        foreach ($contentColumns as $column) {
-                            $content = $this->cleanContent($node->$column);
-                            if ($i == 0) {
-                                $doc->addField(Zend_Search_Lucene_Field::Text('content', $content, 'UTF-8'));
-                                $description = $this->cleanContent($this->previewContent($node->$column, $limit));
-                                $doc->addField(Zend_Search_Lucene_Field::Text('description', $description, 'UTF-8'));
-                            } else {
-                                $doc->addField(Zend_Search_Lucene_Field::Text('content' . $i, $content, 'UTF-8'));
+            if (extension_loaded('iconv') === true) {
+                // Пробежаться по всем моделям и добавить их в индекс
+                foreach ($searchModels as $modelName => $model) {
+                    if (!isset($model['module'])) {
+                        $messages[] = Yii::t('ZendSearchModule.zendsearch', 'Update config file or module, Module index not found for model "{model}"!', array('{model}' => $modelName));
+                    } elseif (is_file(Yii::getPathOfAlias($model['path']) . '.php') && Yii::app()->hasModule($model['module'])) {
+                        $searchNodes = $modelName::model()->findAll();
+                        foreach ($searchNodes as $node) {
+                            $doc = new Zend_Search_Lucene_Document();
+                            $doc->addField(Zend_Search_Lucene_Field::Text('title', CHtml::encode($node->$model['titleColumn']), 'UTF-8'));
+                            $link = str_replace('{' . $model['linkColumn'] . '}', $node->$model['linkColumn'], $model['linkPattern']);
+                            $doc->addField(Zend_Search_Lucene_Field::Text('link', $link, 'UTF-8'));
+                            $contentColumns = explode(',', $model['textColumns']);
+                            $i = 0;
+                            foreach ($contentColumns as $column) {
+                                $content = $this->cleanContent($node->$column);
+                                if ($i == 0) {
+                                    $doc->addField(Zend_Search_Lucene_Field::Text('content', $content, 'UTF-8'));
+                                    $description = $this->cleanContent($this->previewContent($node->$column, $limit));
+                                    $doc->addField(Zend_Search_Lucene_Field::Text('description', $description, 'UTF-8'));
+                                } else {
+                                    $doc->addField(Zend_Search_Lucene_Field::Text('content' . $i, $content, 'UTF-8'));
+                                }
+                                $i++;
                             }
-                            $i++;
+                            $index->addDocument($doc);
                         }
-                        $index->addDocument($doc);
+                        $index->optimize();
+                        $index->commit();
+                    } else {
+                        $messages[] = Yii::t('ZendSearchModule.zendsearch', 'Module "{module}" not installed!', array('{module}' => $model['module']));
                     }
-                    $index->optimize();
-                    $index->commit();
                 }
+            } else {
+                $messages[] = Yii::t('ZendSearchModule.zendsearch', 'This module require "Iconv" extension!');
             }
 
-            Yii::app()->ajax->raw(Yii::t('ZendSearchModule.zendsearch','Index updated successfully!'));
-        } catch (Exception $exc) {
-            Yii::app()->ajax->raw(Yii::t('ZendSearchModule.zendsearch','There is an error!'));
+            Yii::app()->ajax->raw(
+                empty($messages)
+                    ? Yii::t('ZendSearchModule.zendsearch','Index updated successfully!')
+                    : Yii::t('ZendSearchModule.zendsearch','There is an error!')
+                        . ': '
+                        . implode("\n", $messages)
+            );
+        } catch (Exception $e) {
+            Yii::app()->ajax->raw(
+                Yii::t('ZendSearchModule.zendsearch','There is an error!') . ":\n" . $e->getMessage()
+            );
         }
     }
 
