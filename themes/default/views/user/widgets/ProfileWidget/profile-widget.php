ed14303eda5fd64777e7400c0a9af581d425a69c
akulikov <tuxuls@icloud.com>
2013-10-20 00:26:53 +0300

    UPD: small fixes for profile-widget view

--- themes/default/views/user/widgets/ProfileWidget/profile-widget.php
+++ themes/default/views/user/widgets/ProfileWidget/profile-widget.php
@@ -8,23 +8,41 @@
         <div class="bootstrap-widget-content">
             <div class="row-fluid">
                 <div class="span12">
-                    <a href="<?php echo Yii::app()->createUrl('/user/account/profile/');?>" title="<?php echo Yii::t('UserModule.user', 'Edit profile'); ?>">
-                        <?php $this->widget('application.modules.user.widgets.AvatarWidget', array('user' => $user, 'noCache' => true)); ?>
-                    </a>
+                    <?php echo CHtml::link(
+                        $this->widget('application.modules.user.widgets.AvatarWidget', array('user' => $user, 'noCache' => true), true),
+                        array('/user/people/userInfo/', 'username' => $user->nick_name),
+                        array('title' => Yii::t('UserModule.user', 'User profile'))
+                    ); ?>
                 </div>
             </div>
-            <br />
             <div class="row-fluid">
                 <div class="span12">
                   <p>
-                     <i class="icon-envelope"></i> <?php echo $user->email;?><br>
-                     <?php if($user->site):?>
-                         <i class="icon-globe"></i> <?php echo $user->site;?> <br>
-                     <?php endif;?>
-                     <?php if($user->location):?>
-                         <i class="icon-map-marker"></i> <?php echo $user->location;?> <br>
-                     <?php endif;?>
-                   </p>
+                    <ul class="user-info">
+                        <li>
+                            <?php $this->widget(
+                                'bootstrap.widgets.TbButton', array(
+                                    'label' => Yii::t('UserModule.user', 'Edit profile'),
+                                    'icon'  => 'pencil',
+                                    'type'  => 'link',
+                                    'url'   => array('/user/account/profile/'),
+                                )
+                            ); ?>
+                        </li>
+                        <li>
+                            <i class="icon-envelope"></i> <?php echo $user->email;?>
+                        </li>
+                        <?php if($user->site):?>
+                            <li>
+                                <i class="icon-globe"></i> <?php echo $user->site;?>
+                            </li>
+                        <?php endif;?>
+                        <?php if($user->location):?>
+                            <li>
+                                <i class="icon-map-marker"></i> <?php echo $user->location;?>
+                            </li>
+                        <?php endif;?>
+                    </ul>
                 </div>
             </div>
         </div>
