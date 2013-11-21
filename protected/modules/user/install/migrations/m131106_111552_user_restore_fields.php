6f5f61470f96f6f1f42de4581f5e2948e5296c45
yupe <aopeykin@yandex.ru>
2013-11-21 17:08:08 +0400

    closed #1306

--- protected/modules/user/install/migrations/m131106_111552_user_restore_fields.php
+++ protected/modules/user/install/migrations/m131106_111552_user_restore_fields.php
@@ -5,7 +5,7 @@ class m131106_111552_user_restore_fields extends CDbMigration
     public function safeUp()
 	{
         $this->addColumn('{{user_user}}', 'email_confirm', "boolean NOT NULL DEFAULT '0'");
-        $this->addColumn('{{user_user}}', 'registration_date', 'datetime NOT NULL');
+        $this->addColumn('{{user_user}}', 'registration_date', 'datetime NOT NULL DEFAULT NOW()');
 	}
 
 	public function safeDown()
