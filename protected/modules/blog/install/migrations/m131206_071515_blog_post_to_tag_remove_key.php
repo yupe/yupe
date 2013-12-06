6ae68a471634af5b7c858223042d729f88565c8b
Oleg Filimonov <olegsabian@gmail.com>
2013-12-06 11:33:36 +0400

    fix #1315

---
+++ protected/modules/blog/install/migrations/m131206_071515_blog_post_to_tag_remove_key.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * m131206_065613_blog_post_to_tag_remove_key
+ *
+ * Удаление неправильных ключей
+ *
+ * @category YupeMigration
+ * @package  yupe.modules.blog.install.migrations
+ * @author   YupeTeam <team@yupe.ru>
+ * @license  BSD https://raw.github.com/yupe/yupe/master/LICENSE
+ * @link     http://yupe.ru
+ **/
+
+class m131206_065613_blog_post_to_tag_remove_key extends yupe\components\DbMigration
+{
+    public function safeUp()
+    {
+        $this->dropPrimaryKey('post_id', '{{blog_post_to_tag}}');
+        $this->dropPrimaryKey('tag_id', '{{blog_post_to_tag}}');
+    }
+
+    public function safeDown()
+    {
+    }
+}
