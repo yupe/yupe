25f3a1bb066dd19e9e1c718904ce56b38092b509
yupe <aopeykin@yandex.ru>
2013-12-10 22:34:57 +0400

    closed #1322

--- protected/modules/comment/install/comment.php
+++ protected/modules/comment/install/comment.php
@@ -15,6 +15,7 @@ return array(
     ),
     'import'    => array(      
         'application.modules.comment.models.*',
+        'application.modules.blog.models.*',
         'vendor.yiiext.nested-set-behavior.NestedSetBehavior',
     ),
     'component' => array(),
