7a3e9ae9f00250ead671911f0e17d2aeb97591a5
Oleg Filimonov <olegsabian@gmail.com>
2013-12-12 09:43:29 +0400

    Не обрабатывалось несколько папок

--- read.me/makeWorks.sh
+++ read.me/makeWorks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-chmod a+rw ./{assets,uploads,protected/runtime}
+chmod a+rw ./{public/assets,public/uploads,protected/runtime}
 chmod a+rw ./protected/config/module{s,sBack}
 cp ./protected/config/db.back.php ./protected/config/db.php
 chmod a+rw ./protected/config/db.php
