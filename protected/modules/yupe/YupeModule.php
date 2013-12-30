b3a9301fcc7c442d20834688f59cf44b21180e8d
yupe <aopeykin@yandex.ru>
2013-12-30 10:22:54 +0400

    small fix

--- protected/modules/yupe/YupeModule.php
+++ protected/modules/yupe/YupeModule.php
@@ -55,7 +55,7 @@ class YupeModule extends WebModule
      **/
     public function getVersion()
     {
-        return Yii::t('YupeModule.yupe', '0.6(dev)');
+        return Yii::t('YupeModule.yupe', '0.6-beta');
     }
 
     /**
