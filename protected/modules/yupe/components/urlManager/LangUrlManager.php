b70536938702846e4c135ec672f818bf734cf28c
yupe <aopeykin@yandex.ru>
2014-01-02 18:50:08 +0400

    #1274

--- protected/modules/yupe/components/urlManager/LangUrlManager.php
+++ protected/modules/yupe/components/urlManager/LangUrlManager.php
@@ -14,6 +14,14 @@
  * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
  * @link     http://yupe.ru
  */
+namespace yupe\components\urlManager;
+
+use CUrlManager;
+use YFlashMessages;
+use Exception;
+use Yii;
+use CHttpCookie;
+
 class LangUrlManager extends CUrlManager
 {
     public $languages;
--- protected/modules/yupe/components/urlManager/LanguageBehavior.php
+++ protected/modules/yupe/components/urlManager/LanguageBehavior.php
@@ -8,6 +8,15 @@
  * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
  * @link     http://yupe.ru
  */
+namespace yupe\components\urlManager;
+
+use CBehavior;
+use Yii;
+use Exception;
+use CException;
+use YFlashMessages;
+use CHttpCookie;
+
 class LanguageBehavior extends CBehavior
 {
     public $lang          = false;
