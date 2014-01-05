366185949248e18a93d62a6d6947594698261099
yupe <aopeykin@yandex.ru>
2014-01-05 15:00:54 +0400

    theme fix

--- themes/default/views/yupe/widgets/RandomDataWidget/randomdata.php
+++ themes/default/views/yupe/widgets/RandomDataWidget/randomdata.php
@@ -1,1 +1,8 @@
-<?php echo $item;?>
+<div class="bootstrap-widget">
+  <div class="bootstrap-widget-header">
+     <h3>Интересно</h3>
+  </div>
+  <div class="bootstrap-widget-content" id="yw10"><ul class="unstyled">
+      <?php echo $item;?>
+  </div>
+</div>
