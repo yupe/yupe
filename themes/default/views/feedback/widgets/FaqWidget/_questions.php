a356b6b44fcbfa8c09c40980b2c757181be32bba
yupe <aopeykin@yandex.ru>
2014-01-06 18:04:48 +0400

    #1274

--- themes/default/views/feedback/widgets/FaqWidget/_questions.php
+++ themes/default/views/feedback/widgets/FaqWidget/_questions.php
@@ -2,7 +2,7 @@
     <?php $cnt = count($models); $i = 0; ?>
     <?php foreach ($models as $model): ?>
         <li>
-            <?php echo CHtml::link(YText::characterLimiter($model->text,50), array('/feedback/contact/faqView/', 'id' => $model->id)); ?>
+            <?php echo CHtml::link(yupe\helpers\YText::characterLimiter($model->text,50), array('/feedback/contact/faqView/', 'id' => $model->id)); ?>
         </li>
         <?php $i++; if ($i != $cnt) echo '<hr>'; ?>
     <?php endforeach;?>
