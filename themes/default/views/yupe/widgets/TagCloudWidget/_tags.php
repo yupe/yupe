6e543b63be48b67422d10098137d42b7a4e67b7e
yupe <aopeykin@yandex.ru>
2014-01-10 16:50:39 +0400

    theme improved

--- themes/default/views/yupe/widgets/TagCloudWidget/_tags.php
+++
@@ -1,8 +0,0 @@
-<ul class="inline">
-    <?php foreach ($tags as $tag => $count): ?>
-        <?php $count += 2; ?>
-        <?php $link = CHtml::link(CHtml::encode($tag), array('/posts/', 'tag' => CHtml::encode($tag))); ?>
-        <?php echo CHtml::tag('li',array(), CHtml::tag('span', array('class' => 'label label-info','style' => "font-size:{$count}pt"), $link)); ?>
-    <?php endforeach;?>
-</ul>
-
