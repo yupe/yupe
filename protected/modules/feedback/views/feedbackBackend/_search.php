882a53f9cfa014a21be8fc828efa50a76758b123
yupe <aopeykin@yandex.ru>
2014-01-31 11:08:40 +0400

    fix feedback view

--- protected/modules/feedback/views/feedbackBackend/_search.php
+++ protected/modules/feedback/views/feedbackBackend/_search.php
@@ -48,13 +48,11 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
             <?php echo $form->datepickerRow(
                 $model,
                 'creation_date',
-                array(
-                    'class'   => 'span12', 
+                array(                   
                     'options' => array(
                         'language'   => Yii::app()->language,
                         'format'     => 'yyyy-mm-dd',
-                    ),
-                    'prepend' => '<i class="icon-calendar"></i>'
+                    ),                   
                 )
             ); ?>
         </div>
