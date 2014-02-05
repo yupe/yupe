1769dc15ada104ad82fe66a3cf932b60b5925c4e
yupe <aopeykin@yandex.ru>
2014-02-05 17:20:49 +0400

    simplified index.php

--- public/index.php
+++ public/index.php
@@ -13,38 +13,16 @@ if (!ini_get('date.timezone')) {
     date_default_timezone_set('Europe/Moscow');
 }
 
-$development = isset($_SERVER['SERVER_ADDR'])
-        ? strpos($_SERVER['SERVER_ADDR'], '127') === 0
-        : $_SERVER['SERVER_NAME'] == 'localhost';
+// две строки закомментировать на продакшн сервере
+define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-// Выбираем конфигурацию development-main.php, если сайт работает на localhost
-if ($development || defined('APPLICATION_ENV') || getenv('APPLICATION_ENV') !== false) {
-    // Комментируем перед выпуском в продакшен:
-    // в режиме разработки необходимо демонстрировать все ошибки
-    // независимо от среды
-    ini_set('display_errors', 'On');
-    define('YII_DEBUG', true);
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);  
-    // путь к фреймворку Yii
-    $yii = dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yii.php';
-
-} else { //production считается во всех случаях, когда не выполнены условия
-    // путь к фреймворку Yii, при необходимости заменить на yii.php
-    $yii = dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yiilite.php';
-    // путь к основному конфигурационному файлу Yii
-}
-
-require $yii;
+require dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yii.php';
 
 $base = require dirname(__FILE__) . '/../protected/config/main.php';
 
-$userspace = dirname(__FILE__) . '/../protected/config/userspace.php';
-$userspace = file_exists($userspace) ? (require $userspace) : array();
-
 $confManager = new yupe\components\ConfigManager();
-$config = $confManager->merge($base, $userspace);
-
 
 require dirname(__FILE__).'/../vendor/autoload.php';
 
-Yii::createWebApplication($config)->run();
+Yii::createWebApplication($confManager->merge($base))->run();
