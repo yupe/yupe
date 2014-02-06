1938889c53c0b1f022fce2f919c19030a07ae451
yupe <aopeykin@yandex.ru>
2014-02-06 09:27:29 +0400

    closed #1392

--- themes/default/views/blog/widgets/UserBlogsWidget/userblogs.php
+++ themes/default/views/blog/widgets/UserBlogsWidget/userblogs.php
@@ -2,7 +2,7 @@
 	<?php echo Yii::t('BlogModule.blog','Member of :');?>
 	<div>
 		<?php foreach($models as $model):?>
-			<?php echo CHtml::link($model->blog->name, array('/blog/post/show','slug' => $model->blog->slug));?>
+			<?php echo CHtml::link($model->blog->name, array('/blog/blog/show','slug' => $model->blog->slug));?>
 		<?php endforeach;?>	
 	</div>
 <?php endif;?>	
