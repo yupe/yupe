20aa03b47c4e502d616a2d7ecf9413c4af09854a
yupe <aopeykin@yandex.ru>
2014-06-02 18:33:12 +0400

    module "yeeki" moved to yupe-ext

--- protected/modules/yeeki/controllers/YeekiBackendController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class YeekiBackendController extends yupe\components\controllers\BackendController
-{
-    // @TODO требуется добавить проксирующие action-ы.
-    // Контроллер DefaultController модуля wiki наследуется от CController, а должен от YFrontController
-}
