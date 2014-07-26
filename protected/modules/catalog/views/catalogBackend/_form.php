66ac08e84bbfe99f38864209b428ab83c244731e
Dark_Cs <darkcs2@gmail.com>
2014-07-26 14:46:14 +0400

    перевол модуля catalog на yiibooster 4

--- protected/modules/catalog/views/catalogBackend/_form.php
+++ protected/modules/catalog/views/catalogBackend/_form.php
@@ -1,5 +1,5 @@
 <script type='text/javascript'>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('#good-form').liTranslit({
             elName: '#Good_name',
             elAlias: '#Good_alias'
@@ -9,102 +9,225 @@
 
 
 <?php
-$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-    'id'                     => 'good-form',
-    'enableAjaxValidation'   => false,
-    'enableClientValidation' => true,
-    'type'                   => 'vertical',
-    'htmlOptions'            => array('class' => 'well', 'enctype'=>'multipart/form-data'),
-    'inlineErrors'           => true,
-)); ?>
+$form = $this->beginWidget(
+    'bootstrap.widgets.TbActiveForm',
+    array(
+        'id' => 'good-form',
+        'enableAjaxValidation' => false,
+        'enableClientValidation' => true,
+        'type' => 'vertical',
+        'htmlOptions' => array('class' => 'well', 'enctype' => 'multipart/form-data'),
+    )
+); ?>
 
-    <div class="alert alert-info">
-        <?php echo Yii::t('CatalogModule.catalog', 'Fields marked with'); ?>
-        <span class="required">*</span>
-        <?php echo Yii::t('CatalogModule.catalog', 'are required.'); ?>
-    </div>
+<div class="alert alert-info">
+    <?php echo Yii::t('CatalogModule.catalog', 'Fields marked with'); ?>
+    <span class="required">*</span>
+    <?php echo Yii::t('CatalogModule.catalog', 'are required.'); ?>
+</div>
 
-    <?php echo $form->errorSummary($model); ?>
+<?php echo $form->errorSummary($model); ?>
 
-    <div class="wide row-fluid control-group <?php echo ($model->hasErrors('status') || $model->hasErrors('is_special')) ? 'error' : ''; ?>">
-        <div class="span4">
-            <?php echo $form->dropDownListRow($model, 'status', $model->getStatusList(), array('class' => 'popover-help', 'data-original-title' => $model->getAttributeLabel('status'), 'data-content' => $model->getAttributeDescription('status'))); ?>
-        </div>
-        <div class="span3">
-            <br /><br />
-            <?php echo $form->checkBoxRow($model, 'is_special', array('class' => 'popover-help', 'data-original-title' => $model->getAttributeLabel('is_special'), 'data-content' => $model->getAttributeDescription('is_special'))); ?>
-        </div>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('category_id') ? 'error' : ''; ?>">
-        <?php echo $form->dropDownListRow($model, 'category_id', Category::model()->getFormattedList(), array('empty' => Yii::t('CatalogModule.catalog', '--choose--'), 'class' => 'span7 popover-help', 'data-original-title' => $model->getAttributeLabel('category_id'), 'data-content' => $model->getAttributeDescription('category_id'), 'encode' => false)); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('name') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'name', array('class' => 'span7 popover-help', 'size' => 60, 'maxlength' => 250, 'data-original-title' => $model->getAttributeLabel('name'), 'data-content' => $model->getAttributeDescription('name'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('alias') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'alias', array('class' => 'span7 popover-help', 'size' => 60, 'maxlength' => 150, 'data-original-title' => $model->getAttributeLabel('alias'), 'data-content' => $model->getAttributeDescription('alias'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('price') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'price', array('class' => 'span7 popover-help', 'size' => 60, 'maxlength' => 60, 'data-original-title' => $model->getAttributeLabel('price'), 'data-content' => $model->getAttributeDescription('price'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('article') ? 'error' : ''; ?>">
-        <?php echo $form->textFieldRow($model, 'article', array('class' => 'span7 popover-help', 'size' => 60, 'maxlength' => 100, 'data-original-title' => $model->getAttributeLabel('article'), 'data-content' => $model->getAttributeDescription('article'))); ?>
-    </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('image') ? 'error' : ''; ?>">
-        <?php
-        echo CHtml::image(
-            !$model->isNewRecord && $model->image
-                ? $model->getImageUrl()
-                : '#',
-            $model->name, array(
-                'class' => 'preview-image',
-                'style' => !$model->isNewRecord && $model->image
-                        ? ''
-                        : 'display:none'
+<div class="row">
+    <div class="col-sm-4">
+        <?php echo $form->dropDownListGroup(
+            $model,
+            'status',
+            array(
+                'widgetOptions' => array(
+                    'data' => $model->getStatusList(),
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('status'),
+                        'data-content' => $model->getAttributeDescription('status')
+                    ),
+                ),
             )
         ); ?>
-        <?php echo $form->labelEx($model, 'image'); ?>
-        <?php echo  $form->fileField($model, 'image', array('onchange' => 'readURL(this);')); ?>
     </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('description') ? 'error' : ''; ?>">
-        <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('description'); ?>' data-content='<?php echo $model->getAttributeDescription('description'); ?>'>
+    <div class="col-sm-3">
+        <br/>
+        <?php echo $form->checkBoxGroup(
+            $model,
+            'is_special',
+            array(
+                'widgetOptions' => array(
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('is_special'),
+                        'data-content' => $model->getAttributeDescription('is_special')
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup(
+            $model,
+            'category_id',
+            array(
+                'widgetOptions' => array(
+                    'data' => Category::model()->getFormattedList(),
+                    'htmlOptions' => array(
+                        'empty' => Yii::t('CatalogModule.catalog', '--choose--'),
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('category_id'),
+                        'data-content' => $model->getAttributeDescription('category_id'),
+                        'encode' => false,
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup(
+            $model,
+            'name',
+            array(
+                'widgetOptions' => array(
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('name'),
+                        'data-content' => $model->getAttributeDescription('name')
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup(
+            $model,
+            'alias',
+            array(
+                'widgetOptions' => array(
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('alias'),
+                        'data-content' => $model->getAttributeDescription('alias')
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup(
+            $model,
+            'price',
+            array(
+                'widgetOptions' => array(
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('price'),
+                        'data-content' => $model->getAttributeDescription('price')
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup(
+            $model,
+            'article',
+            array(
+                'widgetOptions' => array(
+                    'htmlOptions' => array(
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('article'),
+                        'data-content' => $model->getAttributeDescription('article')
+                    ),
+                ),
+            )
+        ); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php
+        echo CHtml::image(
+            !$model->isNewRecord && $model->image ? $model->getImageUrl() : '#',
+            $model->name,
+            array(
+                'class' => 'preview-image',
+                'style' => !$model->isNewRecord && $model->image ? '' : 'display:none'
+            )
+        ); ?>
+        <?php echo $form->fileFieldGroup($model, 'image', array('widgetOptions' => array('htmlOptions' => array('onchange' => 'readURL(this);', 'style' => 'background-color: inherit;')))); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="popover-help form-group" data-original-title='<?php echo $model->getAttributeLabel('description'); ?>' data-content='<?php echo $model->getAttributeDescription('description'); ?>'>
             <?php echo $form->labelEx($model, 'description'); ?>
-            <?php $this->widget($this->module->editor, array(
-                'model'       => $model,
-                'attribute'   => 'description',
-                'options'     => $this->module->editorOptions,
-            )); ?>
-         </div>
+            <?php $this->widget(
+                $this->module->editor,
+                array(
+                    'model' => $model,
+                    'attribute' => 'description',
+                    'options' => $this->module->editorOptions,
+                )
+            ); ?>
+        </div>
     </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('short_description') ? 'error' : ''; ?>">
-        <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('short_description'); ?>' data-content='<?php echo $model->getAttributeDescription('short_description'); ?>'>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="popover-help form-group" data-original-title='<?php echo $model->getAttributeLabel('short_description'); ?>' data-content='<?php echo $model->getAttributeDescription(
+            'short_description'
+        ); ?>'>
             <?php echo $form->labelEx($model, 'short_description'); ?>
-            <?php $this->widget($this->module->editor, array(
-            'model'       => $model,
-            'attribute'   => 'short_description',
-            'options'     => $this->module->editorOptions,
-        )); ?>
+            <?php $this->widget(
+                $this->module->editor,
+                array(
+                    'model' => $model,
+                    'attribute' => 'short_description',
+                    'options' => $this->module->editorOptions,
+                )
+            ); ?>
         </div>
     </div>
-    <div class="row-fluid control-group <?php echo $model->hasErrors('data') ? 'error' : ''; ?>">
-        <div class="popover-help" data-original-title='<?php echo $model->getAttributeLabel('data'); ?>' data-content='<?php echo $model->getAttributeDescription('data'); ?>'>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="popover-help form-group" data-original-title='<?php echo $model->getAttributeLabel('data'); ?>' data-content='<?php echo $model->getAttributeDescription('data'); ?>'>
             <?php echo $form->labelEx($model, 'data'); ?>
-            <?php $this->widget($this->module->editor, array(
-                'model'       => $model,
-                'attribute'   => 'data',
-                'options'     => $this->module->editorOptions,
-            )); ?>
+            <?php $this->widget(
+                $this->module->editor,
+                array(
+                    'model' => $model,
+                    'attribute' => 'data',
+                    'options' => $this->module->editorOptions,
+                )
+            ); ?>
         </div>
     </div>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
+</div>
+
+<?php $this->widget(
+    'bootstrap.widgets.TbButton',
+    array(
         'buttonType' => 'submit',
-        'type'       => 'primary',
-        'label'      => $model->isNewRecord ? Yii::t('CatalogModule.catalog', 'Add product and continue') : Yii::t('CatalogModule.catalog', 'Save product and continue'),
-    )); ?>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'context' => 'primary',
+        'label' => $model->isNewRecord ? Yii::t('CatalogModule.catalog', 'Add product and continue') : Yii::t('CatalogModule.catalog', 'Save product and continue'),
+    )
+); ?>
+
+<?php $this->widget(
+    'bootstrap.widgets.TbButton',
+    array(
         'buttonType' => 'submit',
-        'htmlOptions'=> array('name' => 'submit-type', 'value' => 'index'),
-        'label'      => $model->isNewRecord ? Yii::t('CatalogModule.catalog', 'Add product and close') : Yii::t('CatalogModule.catalog', 'Save product and close'),
-    )); ?>
+        'htmlOptions' => array('name' => 'submit-type', 'value' => 'index'),
+        'label' => $model->isNewRecord ? Yii::t('CatalogModule.catalog', 'Add product and close') : Yii::t('CatalogModule.catalog', 'Save product and close'),
+    )
+); ?>
 
 <?php $this->endWidget(); ?>
--- protected/modules/catalog/views/catalogBackend/index.php
+++ protected/modules/catalog/views/catalogBackend/index.php
@@ -5,16 +5,16 @@
  * @var $this CatalogBackendController
  */
 $this->breadcrumbs = array(
-        Yii::t('CatalogModule.catalog', 'Products') => array('/catalog/catalogBackend/index'),
-        Yii::t('CatalogModule.catalog', 'Manage'),
-    );
+    Yii::t('CatalogModule.catalog', 'Products') => array('/catalog/catalogBackend/index'),
+    Yii::t('CatalogModule.catalog', 'Manage'),
+);
 
-    $this->pageTitle = Yii::t('CatalogModule.catalog', 'Manage products');
+$this->pageTitle = Yii::t('CatalogModule.catalog', 'Manage products');
 
-    $this->menu = array(
-        array('icon' => 'list-alt', 'label' => Yii::t('CatalogModule.catalog', 'Manage products'), 'url' => array('/catalog/catalogBackend/index')),
-        array('icon' => 'plus-sign', 'label' => Yii::t('CatalogModule.catalog', 'Add a product'), 'url' => array('/catalog/catalogBackend/create')),
-    );
+$this->menu = array(
+    array('icon' => 'glyphicon glyphicon-list-alt', 'label' => Yii::t('CatalogModule.catalog', 'Manage products'), 'url' => array('/catalog/catalogBackend/index')),
+    array('icon' => 'glyphicon glyphicon-plus-sign', 'label' => Yii::t('CatalogModule.catalog', 'Add a product'), 'url' => array('/catalog/catalogBackend/create')),
+);
 ?>
 <div class="page-header">
     <h1>
@@ -23,134 +23,148 @@ $this->breadcrumbs = array(
     </h1>
 </div>
 
-<button class="btn btn-small dropdown-toggle" data-toggle="collapse" data-target="#search-toggle">
-    <i class="icon-search">&nbsp;</i>
-    <?php echo CHtml::link(Yii::t('CatalogModule.catalog', 'Find products'), '#', array('class' => 'search-button')); ?>
-    <span class="caret">&nbsp;</span>
-</button>
+<p>
+    <a class="btn btn-default btn-sm dropdown-toggle" data-toggle="collapse" data-target="#search-toggle">
+        <i class="glyphicon glyphicon-search">&nbsp;</i>
+        <?php echo Yii::t('CatalogModule.catalog', 'Find products'); ?>
+        <span class="caret">&nbsp;</span>
+    </a>
+</p>
 
 <div id="search-toggle" class="collapse out search-form">
-<?php
-Yii::app()->clientScript->registerScript('search', "
+    <?php
+    Yii::app()->clientScript->registerScript(
+        'search',
+        "
     $('.search-form form').submit(function() {
         $.fn.yiiGridView.update('good-grid', {
             data: $(this).serialize()
         });
         return false;
     });
-");
-$this->renderPartial('_search', array('model' => $model));
-?>
+"
+    );
+    $this->renderPartial('_search', array('model' => $model));
+    ?>
 </div>
-
-<br/>
 
 <p><?php echo Yii::t('CatalogModule.catalog', 'This section describes products manager'); ?></p>
 
-<?php $this->widget('yupe\widgets\CustomGridView', array(
-    'id'           => 'good-grid',
-    'dataProvider' => $model->search(),
-    'filter'       => $model,
-    'columns'      => array(
-        array(
-            'name' => 'id',
-            'htmlOptions' => array('style' => 'width:20px'),
-            'type' => 'raw',
-            'value' => 'CHtml::link($data->id, array("/catalog/catalogBackend/update", "id" => $data->id))'
-        ),
-        array(
-            'class' => 'bootstrap.widgets.TbEditableColumn',
-            'name'  => 'name',
-            'editable' => array(
-                'url' => $this->createUrl('/catalog/catalogBackend/inline'),
-                'mode' => 'inline',
-                'params' => array(
-                    Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+<?php $this->widget(
+    'yupe\widgets\CustomGridView',
+    array(
+        'id' => 'good-grid',
+        'dataProvider' => $model->search(),
+        'filter' => $model,
+        'columns' => array(
+            array(
+                'name' => 'id',
+                'htmlOptions' => array('style' => 'width:20px'),
+                'type' => 'raw',
+                'value' => 'CHtml::link($data->id, array("/catalog/catalogBackend/update", "id" => $data->id))'
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'name' => 'name',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'inline',
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'filter' => CHtml::activeTelField($model, 'name', array('class' => 'form-control')),
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'name' => 'alias',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'inline',
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'filter' => CHtml::activeTelField($model, 'alias', array('class' => 'form-control')),
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'popup',
+                    'type' => 'select',
+                    'title' => Yii::t('CatalogModule.catalog', 'Select {field}', array('{field}' => mb_strtolower($model->getAttributeLabel('category_id')))),
+                    'source' => Category::model()->getFormattedList(Yii::app()->getModule('catalog')->mainCategory),
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'name' => 'category_id',
+                'type' => 'raw',
+                'value' => '$data->category->name',
+                'filter' => CHtml::activeDropDownList(
+                    $model,
+                    'category_id',
+                    Category::model()->getFormattedList(Yii::app()->getModule('catalog')->mainCategory),
+                    array('encode' => false, 'empty' => '', 'class' => 'form-control')
                 )
-            )
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'name' => 'price',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'inline',
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'filter' => CHtml::activeTelField($model, 'price', array('class' => 'form-control')),
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'name' => 'article',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'inline',
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'filter' => CHtml::activeTelField($model, 'article', array('class' => 'form-control')),
+            ),
+            array(
+                'name' => 'is_special',
+                'type' => 'raw',
+                'value' => '$this->grid->returnBootstrapStatusHtml($data, "is_special", "Special", array("minus", "star"))',
+                'filter' => Yii::app()->getModule('catalog')->getChoice()
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbEditableColumn',
+                'editable' => array(
+                    'url' => $this->createUrl('/catalog/catalogBackend/inline'),
+                    'mode' => 'popup',
+                    'type' => 'select',
+                    'title' => Yii::t('CatalogModule.catalog', 'Select {field}', array('{field}' => mb_strtolower($model->getAttributeLabel('status')))),
+                    'source' => $model->getStatusList(),
+                    'params' => array(
+                        Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
+                    )
+                ),
+                'name' => 'status',
+                'type' => 'raw',
+                'value' => '$data->getStatus()',
+                'filter' => CHtml::activeDropDownList($model, 'status', $model->getStatusList(), array('class' => 'form-control', 'empty' => '')),
+            ),
+            array(
+                'name' => 'user_id',
+                'type' => 'raw',
+                'value' => 'CHtml::link($data->user->getFullName(), array("/user/userBackend/view", "id" => $data->user->id))',
+                'filter' => CHtml::listData(User::model()->cache(Yii::app()->getModule('yupe')->coreCacheTime)->findAll(), 'id', 'nick_name')
+            ),
+            array(
+                'class' => 'bootstrap.widgets.TbButtonColumn',
+            ),
         ),
-        array(
-            'class' => 'bootstrap.widgets.TbEditableColumn',
-            'name'  => 'alias',
-            'editable' => array(
-                'url' => $this->createUrl('/catalog/catalogBackend/inline'),
-                'mode' => 'inline',
-                'params' => array(
-                    Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
-                )
-            )
-        ),
-        array(
-            'class'  => 'bootstrap.widgets.TbEditableColumn',
-			'editable' => array(
-				'url'    => $this->createUrl('/catalog/catalogBackend/inline'),
-				'mode'   => 'popup',
-				'type'   => 'select',
-				'title'  => Yii::t('CatalogModule.catalog', 'Select {field}', array('{field}' => mb_strtolower($model->getAttributeLabel('category_id')))),
-				'source' => Category::model()->getFormattedList(Yii::app()->getModule('catalog')->mainCategory),
-				'params' => array(
-					Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
-				)
-			),
-            'name'   => 'category_id',
-            'type'   => 'raw',
-            'value'  => '$data->category->name',
-            'filter' => CHtml::activeDropDownList($model, 'category_id', Category::model()->getFormattedList(Yii::app()->getModule('catalog')->mainCategory), array('encode' => false, 'empty' => ''))
-        ),
-        array(
-            'class' => 'bootstrap.widgets.TbEditableColumn',
-            'name'  => 'price',
-            'editable' => array(
-                'url' => $this->createUrl('/catalog/catalogBackend/inline'),
-                'mode' => 'inline',
-                'params' => array(
-                    Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
-                )
-            )
-        ),
-        array(
-            'class' => 'bootstrap.widgets.TbEditableColumn',
-            'name'  => 'article',
-            'editable' => array(
-                'url' => $this->createUrl('/catalog/catalogBackend/inline'),
-                'mode' => 'inline',
-                'params' => array(
-                    Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
-                )
-            )
-        ),
-
-        array(
-            'name'  => 'is_special',
-            'type'  => 'raw',
-            'value'  => '$this->grid->returnBootstrapStatusHtml($data, "is_special", "Special", array("minus", "star"))',
-            'filter' => Yii::app()->getModule('catalog')->getChoice()
-        ),
-        array(
-            'class'  => 'bootstrap.widgets.TbEditableColumn',
-			'editable' => array(
-				'url'    => $this->createUrl('/catalog/catalogBackend/inline'),
-				'mode'   => 'popup',
-				'type'   => 'select',
-				'title'  => Yii::t('CatalogModule.catalog', 'Select {field}', array('{field}' => mb_strtolower($model->getAttributeLabel('status')))),
-				'source' => $model->getStatusList(),
-				'params' => array(
-					Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken
-				)
-			),
-            'name'   => 'status',
-            'type'   => 'raw',
-            'value'  => '$data->getStatus()',
-            'filter' => $model->getStatusList()
-        ),
-        array(
-            'name'   => 'user_id',
-            'type'   => 'raw',
-            'value'  => 'CHtml::link($data->user->getFullName(), array("/user/userBackend/view", "id" => $data->user->id))',
-            'filter' => CHtml::listData(User::model()->cache(Yii::app()->getModule('yupe')->coreCacheTime)->findAll(),'id','nick_name')
-        ),
-        array(
-            'class' => 'bootstrap.widgets.TbButtonColumn',
-        ),
-    ),
-)); ?>
+    )
+); ?>
