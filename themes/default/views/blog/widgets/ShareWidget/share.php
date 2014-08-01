7b2ce6e3a9779f456be0b7267f62af8adb2d161c
yupe <aopeykin@yandex.ru>
2014-08-01 12:32:59 +0400

    pluso replace with yandex share buttons

--- themes/default/views/blog/widgets/ShareWidget/share.php
+++ themes/default/views/blog/widgets/ShareWidget/share.php
@@ -1,11 +1,7 @@
-<script type="text/javascript">
-    (function() {
-        if(window.pluso) if(typeof window.pluso.start == "function") return;
-        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-        var h=d[g]('head')[0] || d[g]('body')[0];
-        h.appendChild(s);
-    })();
-</script>
-<div class="pluso" data-options="small,round,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print" data-background="transparent"></div>
+<script type="text/javascript" src="//yandex.st/share/share.js"
+        charset="utf-8"></script>
+<div class="yashare-auto-init" data-yashareL10n="ru"
+     data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,gplus" data-yashareTheme="counter"
+
+    ></div>
+
