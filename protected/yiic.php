fd713d9b22d42b46b306484e93c209a1c8fe8451
mikspark <yamikspark@gmail.com>
2014-08-28 02:10:03 +0500

    fix yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -3,8 +3,8 @@ define('YII_APP_TYPE', 'console');
 require dirname(__FILE__) . '/../vendor/autoload.php';
 $yiic = require dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yii.php';
 
-$configManager = new yupe\components\ConfigManager();
 $config = require dirname(__FILE__) . '/config/console.php';
+$configManager = new yupe\components\ConfigManager();
 
 $app = Yii::createConsoleApplication($configManager->merge($config));
 $app->commandRunner->addCommands(YII_PATH . '/cli/commands');
