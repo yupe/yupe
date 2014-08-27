bc8591e8fca58d07adaf0e86dcee1391ddb3bae3
mikspark <yamikspark@gmail.com>
2014-08-27 16:51:27 +0500

    refactored yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,19 +1,11 @@
 <?php
-
 define('YII_APP_TYPE', 'console');
-
-require dirname(__FILE__).'/../vendor/autoload.php';
-
-// change the following paths if necessary
+require dirname(__FILE__) . '/../vendor/autoload.php';
 $yiic = require dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yii.php';
-$base = require dirname(__FILE__) . '/config/console.php';
 
-$confManager = new yupe\components\ConfigManager();
-$config = $confManager->merge($base);
+$configManager = new yupe\components\ConfigManager();
+$config = require dirname(__FILE__) . '/config/console.php';
 
-$app=Yii::createConsoleApplication($config);
-$app->commandRunner->addCommands(YII_PATH.'/cli/commands');
+$app = Yii::createConsoleApplication($configManager->merge($config));
+$app->commandRunner->addCommands(YII_PATH . '/cli/commands');
 $app->run();
-
-
-
