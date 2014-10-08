a598361ef1fadfc7ad8262db6a5c5791aa1691c4
Dark_Cs <darkcs2@gmail.com>
2014-10-08 12:09:59 +0400

    правильное количество комментариев у постов

--- themes/default/views/homepage/hp/_view.php
+++ themes/default/views/homepage/hp/_view.php
@@ -1,53 +1,35 @@
 <div class="row">
     <div class="col-sm-12">
-        <h4><strong><?php echo CHtml::link(
-                    CHtml::encode($data->title),
-                    array('/blog/post/show/', 'slug' => CHtml::encode($data->slug))
-                ); ?></strong></h4>
+        <h4>
+            <strong>
+                <?php echo CHtml::link(CHtml::encode($data->title), ['/blog/post/show/', 'slug' => $data->slug]); ?>
+            </strong>
+        </h4>
     </div>
 </div>
 <div class="row">
     <div class="col-sm-12">
         <p>
-            <?php echo $data->getImageUrl() ? CHtml::image($data->getImageUrl(), CHtml::encode($data->title)) : "";?>
+            <?php echo $data->getImageUrl() ? CHtml::image($data->getImageUrl(), CHtml::encode($data->title)) : ""; ?>
         </p>
         <p><?php echo strip_tags($data->getQuote()); ?></p>
-        <!--<p><?php echo CHtml::link(
-            Yii::t('HomepageModule.homepage', 'read...'),
-            array('/blog/post/show/', 'slug' => CHtml::encode($data->slug)),
-            array('class' => 'btn')
-        ); ?></p>-->
     </div>
 </div>
 <div class="row">
     <div class="col-sm-12">
         <p></p>
-
         <p>
-            <i class="glyphicon glyphicon-user"></i> <?php echo CHtml::link(
-                $data->createUser->nick_name,
-                array('/user/people/userInfo', 'username' => CHtml::encode($data->createUser->nick_name))
-            ); ?>
-            | <i class="glyphicon glyphicon-pencil"></i> <?php echo CHtml::link(
-                CHtml::encode($data->blog->name),
-                array('/blog/blog/show/', 'slug' => CHtml::encode($data->blog->slug))
-            ); ?>
-            | <i class="glyphicon glyphicon-calendar"></i> <?php echo Yii::app()->getDateFormatter()->formatDateTime(
-                $data->publish_date,
-                "short",
-                "short"
-            ); ?>
-            | <i class="glyphicon glyphicon-comment"></i>  <?php echo CHtml::link(
-                ($data->commentsCount > 0) ? $data->commentsCount - 1 : 0,
-                array('/blog/post/show/', 'slug' => CHtml::encode($data->slug), '#' => 'comments')
-            ); ?>
+            <i class="glyphicon glyphicon-user"></i> <?php echo CHtml::link($data->createUser->nick_name, ['/user/people/userInfo', 'username' => $data->createUser->nick_name]); ?>
+            | <i class="glyphicon glyphicon-pencil"></i> <?php echo CHtml::link(CHtml::encode($data->blog->name), ['/blog/blog/show/', 'slug' => $data->blog->slug]); ?>
+            | <i class="glyphicon glyphicon-calendar"></i> <?php echo Yii::app()->getDateFormatter()->formatDateTime($data->publish_date, "short", "short"); ?>
+            | <i class="glyphicon glyphicon-comment"></i>  <?php echo CHtml::link($data->commentsCount, ['/blog/post/show/', 'slug' => $data->slug, '#' => 'comments']); ?>
             | <i class="glyphicon glyphicon-tags"></i>
-            <?php if (($tags = $data->getTags()) != array()) : ?>
+            <?php if (($tags = $data->getTags()) != []) : ?>
                 <?php foreach ($tags as $tag): ?>
                     <?php $tag = CHtml::encode($tag); ?>
                     &nbsp;
                     <span class="label label-info">
-                        <?php echo CHtml::link($tag, array('/posts/', 'tag' => $tag)) . ' ' ?>
+                        <?php echo CHtml::link($tag, ['/posts/', 'tag' => $tag]) . ' ' ?>
                     </span>
                 <?php endforeach ?>
             <?php endif; ?>
