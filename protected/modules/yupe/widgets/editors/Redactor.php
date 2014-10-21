806c53931d48de2344853b816d4bb0992468718b
ApexWire <apexwire@gmail.com>
2014-10-21 15:16:59 +0400

    Update Redactor.php
    Добавляем кнопку "HTML" (просмотр исходного кода) в редактор
    Позволяет вставлять текст без ошибки, если активна кнопка "HTML"

--- protected/modules/yupe/widgets/editors/Redactor.php
+++ protected/modules/yupe/widgets/editors/Redactor.php
@@ -50,6 +50,7 @@ class Redactor extends \CInputWidget
     public function getOptions()
     {
         return array(
+            'buttonSource'            => true,
             'imageUpload'             => Yii::app()->createUrl('/yupe/backend/AjaxImageUpload'),
             'fileUpload'              => Yii::app()->createUrl('/yupe/backend/AjaxFileUpload'),
             'imageManagerJson'        => Yii::app()->createUrl('/image/imageBackend/AjaxImageChoose'),
