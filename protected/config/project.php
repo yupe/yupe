a68982ecfce02464cd81e1d7f0cf340c2fa8511a
yupe <aopeykin@yandex.ru>
2015-01-27 09:49:42 +0300

    rollback project.php

--- protected/config/project.php
+++ protected/config/project.php
@@ -1,20 +1,5 @@
 <?php
 
 return [
-    'rules' => [
-        '/backend'                                                     => false,
-        '/backend/login'                                               => false,
-        '/backend/<action:\w+>'                                        => false,
-        '/backend/<module:\w+>/<controller:\w+>'                       => false,
-        '/backend/<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>' => false,
-        '/backend/<module:\w+>/<controller:\w+>/<action:\w+>'          => false,
-
-
-        '/admin'                                                     => '/yupe/backend/index',
-        '/admin/login'                                               => '/user/account/backendlogin',
-        '/admin/<action:\w+>'                                        => '/yupe/backend/<action>',
-        '/admin/<module:\w+>/<controller:\w+>'                       => '/<module>/<controller>Backend/index',
-        '/admin/<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>' => '/<module>/<controller>Backend/<action>',
-        '/admin/<module:\w+>/<controller:\w+>/<action:\w+>'          => '/<module>/<controller>Backend/<action>',
-    ]
+   
 ];
