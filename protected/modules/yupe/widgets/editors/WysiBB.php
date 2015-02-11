5185a5e72024ee69ec1d6aff2ece3197e2a75105
apexwire <apexwire@gmail.com>
2015-02-11 14:44:16 +0400

    WysiBB redactor - добавлен WysiBB редактор. - обновлен composer.json
    Виджет для Yii берется отсюда: https://github.com/brussens/wysibb-yii

---
+++ protected/modules/yupe/widgets/editors/WysiBB.php
@@ -0,0 +1,31 @@
+<?php
+namespace yupe\widgets\editors;
+
+use Yii;
+use CInputWidget;
+
+class WysiBB extends \CInputWidget
+{
+    private $editorWidgetClass = 'vendor.brussens.wysibb-yii.WysiBBWidget';
+
+    public $options = [];
+
+    public function run()
+    {
+        $this->widget(
+            $this->editorWidgetClass,
+            [
+                'model'     => $this->model,
+                'attribute' => $this->attribute,
+                'options'   => \CMap::mergeArray($this->getOptions(), $this->options),
+            ]
+        );
+    }
+
+    public function getOptions()
+    {
+        return [
+            'buttons' => "bold,italic,underline,|,link,|,code,quote",
+        ];
+    }
+}
