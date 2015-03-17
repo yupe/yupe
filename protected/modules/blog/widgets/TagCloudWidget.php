5d85dbcd5d6b84b4546a3e5b9d3d614755c521e6
sabian <olegsabian@gmail.com>
2015-03-17 21:45:44 +0300

    fix #1901

--- protected/modules/blog/widgets/TagCloudWidget.php
+++ protected/modules/blog/widgets/TagCloudWidget.php
@@ -26,10 +26,22 @@ class TagCloudWidget extends yupe\widgets\YWidget
             return false;
         }
         $model = $this->model;
-        $model::model()->resetAllTagsCache();
+        $model::model()->resetAllTagsWithModelsCountCache();
+
         $criteria = new CDbCriteria();
         $criteria->order = 'count DESC';
         $criteria->limit = $this->count;
+
+        if ($model == 'Post') {
+            $criteria->join = 'JOIN ' . $model::model()->tableName() . ' p ON et.post_id = p.id';
+            $criteria->condition = 'p.status = :status';
+            $criteria->addCondition('p.access_type = :access');
+            $criteria->params = [
+                ':status' => $model::STATUS_PUBLISHED,
+                ':access' => $model::ACCESS_PUBLIC,
+            ];
+        }
+
         $this->render('tagcloud', ['tags' => $model::model()->getAllTagsWithModelsCount($criteria)]);
     }
 }
