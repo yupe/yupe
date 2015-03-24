28312f2f85fe0a8937fc313e2c9461d321ad40e1
MikSpark <yamikspark@gmail.com>
2015-03-24 13:00:45 +0000

    fix #1901

--- protected/modules/blog/widgets/TagCloudWidget.php
+++ protected/modules/blog/widgets/TagCloudWidget.php
@@ -2,34 +2,28 @@
 
 class TagCloudWidget extends yupe\widgets\YWidget
 {
-    public $count = 50;
-    public $model;
+    /**
+     * @var int
+     */
+    public $limit = 50;
 
     public function run()
     {
-        if (!@class_exists($this->model)) {
-            echo CHtml::tag(
-                'p',
-                [
-                    'class' => 'alert alert-error'
-                ],
-                Yii::t(
-                    'YupeModule.yupe',
-                    'Widget {widget}: Model "{model}" was not found!',
-                    [
-                        '{model}'  => $this->model,
-                        '{widget}' => get_class($this),
-                    ]
-                )
-            );
+        $model = Post::model();
 
-            return false;
-        }
-        $model = $this->model;
-        $model::model()->resetAllTagsCache();
         $criteria = new CDbCriteria();
         $criteria->order = 'count DESC';
-        $criteria->limit = $this->count;
-        $this->render('tagcloud', ['tags' => $model::model()->getAllTagsWithModelsCount($criteria)]);
+        $criteria->limit = $this->limit;
+
+        $criteria->join = sprintf(
+            'JOIN `%s` p ON et.%s = p.id AND p.status = %s',
+            $model->tableName(),
+            $model->modelTableFk,
+            $model::STATUS_PUBLISHED
+        );
+
+        $this->render('tagsCloud', [
+            'tags' => $model->getAllTagsWithModelsCount($criteria)
+        ]);
     }
 }
