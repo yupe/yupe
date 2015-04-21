9c64a741ce018aabeec679604df8bedd41f14d55
Dark_Cs <darkcs2@gmail.com>
2015-04-21 13:50:48 +0300

    кнопка просмотра на фронте в гриде

--- protected/modules/yupe/widgets/CustomButtonColumn.php
+++ protected/modules/yupe/widgets/CustomButtonColumn.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Michael
- * Date: 31.10.2014
- * Time: 4:58
- */
-
 namespace yupe\widgets;
 
 use Yii;
@@ -14,6 +7,7 @@ Yii::import('bootstrap.widgets.TbButtonColumn');
 
 class CustomButtonColumn extends \TbButtonColumn
 {
+    public $template = '{front_view} {view} {update} {delete}';
     /**
      * @var string the view button icon (defaults to 'eye-open').
      */
@@ -28,4 +22,54 @@ class CustomButtonColumn extends \TbButtonColumn
      * @var string the delete button icon (defaults to 'trash').
      */
     public $deleteButtonIcon = 'fa fa-fw fa-trash-o';
+
+    public $frontViewGetUrlMethodName = 'getUrl';
+
+    public $frontViewButtonLabel;
+
+    public $frontViewButtonIcon = 'fa fa-fw fa-external-link-square';
+
+    public $frontViewButtonUrl;
+
+    public $frontViewButtonOptions = ['class' => 'front-view', 'target' => '_blank'];
+
+    protected function initDefaultButtons()
+    {
+        parent::initDefaultButtons();
+
+
+        /* Кнопка просмотра на фронте */
+
+        if ($this->frontViewButtonLabel === null) {
+            $this->frontViewButtonLabel = Yii::t('zii', 'View');
+        }
+
+        $button = array(
+            'label' => $this->frontViewButtonLabel,
+            'url' => $this->frontViewButtonUrl ?: function ($data) {
+                try {
+                    return $data->{$this->frontViewGetUrlMethodName}();
+                } catch (\Exception $e) {
+                    return null;
+                }
+            },
+            'options' => $this->frontViewButtonOptions,
+            'icon' => $this->frontViewButtonIcon,
+        );
+
+        if (isset($this->buttons['front_view'])) {
+            $this->buttons['front_view'] = array_merge($button, $this->buttons['front_view']);
+        } else {
+            /* показывать кнопку только если задали ей свой url, или модель имеет метод для получения url*/
+            $button['visible'] = function ($row, $data) {
+                // todo: найти нормальный способ узнавания есть ли у модели метод
+                try {
+                    return $this->frontViewButtonUrl || method_exists($data, $this->frontViewGetUrlMethodName) || (bool)$data->{$this->frontViewGetUrlMethodName}();
+                } catch (\Exception $e) {
+                    return false;
+                }
+            };
+            $this->buttons['front_view'] = $button;
+        }
+    }
 }
