bd612a4719aaa189b1f79065fe01f8057e4efd4a
sabian <olegsabian@gmail.com>
2015-06-08 10:41:37 +0300

    #2064 Исправлены правила валидации пользователя

--- protected/modules/user/forms/ProfileForm.php
+++ protected/modules/user/forms/ProfileForm.php
@@ -48,7 +48,7 @@ class ProfileForm extends CFormModel
             [
                 'nick_name',
                 'match',
-                'pattern' => '/^[A-Za-z0-9]{2,50}$/',
+                'pattern' => '/^[A-Za-z0-9_-]{2,50}$/',
                 'message' => Yii::t(
                     'UserModule.user',
                     'Bad field format for "{attribute}". You can use only letters and digits from 2 to 20 symbols'
