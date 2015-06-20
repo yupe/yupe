103a7cce6d36f830e687ee50118092252b5b112b
ApexWire <apexwire@gmail.com>
2015-06-20 22:14:47 +0300

    меню справа отображается только на Medium devices Desktops (≥992px)  на Small devices Tablets (≥768px) и Extra small devices Phones (<768px) оно скрывается

--- protected/modules/yupe/views/layouts/column2.php
+++ protected/modules/yupe/views/layouts/column2.php
@@ -1,6 +1,6 @@
 <?php $this->beginContent($this->yupe->getBackendLayoutAlias("main")); ?>
 <div class="row">
-    <div class="<?php echo $this->hideSidebar ? 'col-sm-12' : 'col-sm-12 col-mg-12 col-lg-10'; ?>">
+    <div class="<?php echo $this->hideSidebar ? 'col-sm-12' : 'col-sm-12 col-md-9 col-lg-10'; ?>">
         <?php
         if (count($this->breadcrumbs)) {
             $this->widget(
@@ -19,7 +19,7 @@
         </div>
         <!-- content -->
     </div>
-    <div class="<?php echo $this->hideSidebar ? 'hidden' : 'col-sm-2 hidden-xs hidden-sm hidden-md'; ?>">
+    <div class="<?php echo $this->hideSidebar ? 'hidden' : 'col-md-3 col-lg-2 hidden-xs hidden-sm'; ?>">
         <?php if (count($this->menu)): ?>
             <div class="panel panel-default" style="padding: 8px 0;">
                 <?php $this->widget(
