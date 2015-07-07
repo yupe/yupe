f8dee511ec04b7d49f4f5c868c265520b689e2fa
Oleg Filimonov <olegsabian@gmail.com>
2015-07-07 23:08:49 +0300

    fix typo

--- protected/modules/robokassa/messages/ru/robokassa.php
+++ protected/modules/robokassa/messages/ru/robokassa.php
@@ -2,7 +2,7 @@
 return [
     'Store' => 'Магазин',
     'Robokassa' => 'Робокасса',
-    'Robokassa payment module' => 'Модуль для приемы оплаты через Робокассу',
+    'Robokassa payment module' => 'Модуль для приема оплаты через Робокассу',
     'Payment order #{id} on "{site}" website' => 'Оплата заказа №{id} на сайте "{site}"',
     'Pay' => 'Заплатить'
 ];
