b9b170b2718218d1cc9aab093dc03e99b61f2582
sabian <olegsabian@gmail.com>
2015-08-24 16:51:15 +0300

    Модуль Яндекс.Касса

---
+++ protected/modules/yandexmoney/messages/ru/ymoney.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'Store' => 'Магазин',
+    'Yandex.Money' => 'Яндекс.Деньги',
+    'Yandex.Money payment module' => 'Модуль для приемы оплаты через Яндекс.Деньги',
+    'Pay' => 'Оплатить',
+    'The order doesn\'t exist.' => 'Запрошенный заказ не существует.',
+    'The order #{n} is already payed.' => 'Заказ №{n} уже оплачен.',
+    'Wrong checksum' => 'Неправильная контрольная сумма',
+    'Wrong payment amount' => 'Некорректная сумма оплаты',
+    'The order #{n} has been payed successfully.' => 'Заказ №{n} успешно оплачен.'
+];
