7d623efd970035931de3e670bc61a2e0d9c4848f
sabian <olegsabian@gmail.com>
2015-09-02 16:18:30 +0300

    #2151 Рефакторинг Робокассы. Исправлена форма

--- protected/modules/robokassa/views/form.php
+++ protected/modules/robokassa/views/form.php
@@ -13,13 +13,14 @@ $description = Yii::t('RobokassaModule.robokassa', 'Payment order #{id} on "{sit
     '{id}' => $id,
     '{site}' => Yii::app()->getModule('yupe')->siteName
 ]);
+?>
 
-echo CHtml::form($action)
-    . CHtml::hiddenField('MrchLogin', $login)
-    . CHtml::hiddenField('OutSum', $price)
-    . CHtml::hiddenField('InvId', $id)
-    . CHtml::hiddenField('Desc', $description)
-    . CHtml::hiddenField('SignatureValue', md5("$login:$price:$id:$password"))
-    . CHtml::hiddenField('Culture', $language)
-    . CHtml::submitButton(Yii::t('RobokassaModule.robokassa','Pay'))
-    . CHtml::endForm();
+<?= CHtml::form($action) ?>
+<?= CHtml::hiddenField('MrchLogin', $login) ?>
+<?= CHtml::hiddenField('OutSum', $price) ?>
+<?= CHtml::hiddenField('InvId', $id) ?>
+<?= CHtml::hiddenField('Desc', $description) ?>
+<?= CHtml::hiddenField('SignatureValue', md5("$login:$price:$id:$password")) ?>
+<?= CHtml::hiddenField('Culture', $language) ?>
+<?= CHtml::submitButton(Yii::t('RobokassaModule.robokassa','Pay')) ?>
+<?= CHtml::endForm() ?>
