825fc793993c2489e8ea033007b9c786005c6a64
sabian <olegsabian@gmail.com>
2015-09-03 21:47:42 +0300

    Небольшой рефакторинг PaymentController

--- protected/modules/payment/controllers/PaymentController.php
+++ protected/modules/payment/controllers/PaymentController.php
@@ -6,13 +6,19 @@ class PaymentController extends \yupe\components\controllers\FrontController
     {
         /* @var $payment Payment */
         $payment = Payment::model()->findByPk($id);
-        if ($payment && $payment->module) {
-            $paymentSystem = Yii::app()->paymentManager->getPaymentSystemObject($payment->module);
-            if ($paymentSystem) {
-                $paymentSystem->processCheckout($payment, Yii::app()->getRequest());
-            }
-        } else {
+
+        if (!$payment && !$payment->module) {
             throw new CHttpException(404);
         }
+
+        /** @var PaymentSystem $paymentSystem */
+        if ($paymentSystem = Yii::app()->paymentManager->getPaymentSystemObject($payment->module)) {
+            $result = $paymentSystem->processCheckout($payment, Yii::app()->getRequest());
+
+            if ($result instanceof Order) {
+                $this->redirect(['/order/order/view', 'url' => $result->url]);
+            }
+        }
+
     }
 }
