8d550d94fa6c8ef44f452ed091da24dac938e131
sabian <olegsabian@gmail.com>
2015-10-07 20:52:36 +0300

    #2111 Новая тема. Сортировка

--- themes/shop/views/store/category/view.php
+++ themes/shop/views/store/category/view.php
@@ -31,41 +31,38 @@ $this->breadcrumbs = array_merge(
             <div class="entry__title">
                 <h1 class="h1"><?= Yii::t('StoreModule.store', 'Products in category "{category}"', ['{category}' => CHtml::encode($category->name)]); ?></h1>
             </div>
-            <div class="catalog-controls">
-                <div class="catalog-controls__col">
-                    <div class="sorter">
-                        <div class="sorter__description">Сортировать:</div><a href="javascript:void(0);" class="asc">по популярности</a><a href="javascript:void(0);" class="desc">по цене</a><a href="javascript:void(0);">по названию</a>
-                    </div>
-                </div>
-                <div class="catalog-controls__col catalog-controls__col_right">
-                    <div class="view-switch">
-                        <div class="view-switch__caption"></div>
-                        <div class="view-switch__toggle">
-                            <div class="switch">
-                                <div class="switch__item">
-                                    <div class="switch-item">
-                                        <input type="radio" id="view-switch-list" name="view-switch" value="list" class="switch-item__input">
-                                        <label for="view-switch-list" class="switch-item__label"><i class="fa fa-th-list fa-fw"></i>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="switch__item">
-                                    <div class="switch-item">
-                                        <input type="radio" id="view-switch-grid" name="view-switch" value="grid" checked class="switch-item__input">
-                                        <label for="view-switch-grid" class="switch-item__label"><i class="fa fa-th fa-fw"></i>
-                                        </label>
+            <?php $this->widget(
+                'zii.widgets.CListView', [
+                    'dataProvider' => $dataProvider,
+                    'itemView' => '//store/product/_item',
+                    'template' => '
+                        <div class="catalog-controls">
+                            <div class="catalog-controls__col">{sorter}</div>
+                            <div class="view-switch">
+                                <div class="view-switch__caption"></div>
+                                <div class="view-switch__toggle">
+                                    <div class="switch">
+                                        <div class="switch__item">
+                                            <div class="switch-item">
+                                                <input type="radio" id="view-switch-list" name="view-switch" value="list" class="switch-item__input">
+                                                <label for="view-switch-list" class="switch-item__label"><i class="fa fa-th-list fa-fw"></i>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="switch__item">
+                                            <div class="switch-item">
+                                                <input type="radio" id="view-switch-grid" name="view-switch" value="grid" checked class="switch-item__input">
+                                                <label for="view-switch-grid" class="switch-item__label"><i class="fa fa-th fa-fw"></i>
+                                                </label>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <?php $this->widget(
-                'zii.widgets.CListView',
-                [
-                    'dataProvider' => $dataProvider,
-                    'itemView' => '//store/product/_item',
+                        {items}
+                        {pager}
+                    ',
                     'summaryText' => '',
                     'enableHistory' => true,
                     'cssFile' => false,
@@ -76,6 +73,7 @@ $this->breadcrumbs = array_merge(
                         'price',
                         'update_time'
                     ],
+                    'sorterHeader' => '<div class="sorter__description">Сортировать:</div>',
                     'htmlOptions' => [
                         'class' => 'catalog'
                     ],
--- themes/shop/views/store/product/index.php
+++ themes/shop/views/store/product/index.php
@@ -25,41 +25,38 @@ $this->breadcrumbs = [Yii::t("StoreModule.store", "Catalog")];
             </div>
         </div>
         <div class="col grid-module-9">
-            <div class="catalog-controls">
-                <div class="catalog-controls__col">
-                    <div class="sorter">
-                        <div class="sorter__description">Сортировать:</div><a href="javascript:void(0);" class="asc">по популярности</a><a href="javascript:void(0);" class="desc">по цене</a><a href="javascript:void(0);">по названию</a>
-                    </div>
-                </div>
-                <div class="catalog-controls__col catalog-controls__col_right">
-                    <div class="view-switch">
-                        <div class="view-switch__caption"></div>
-                        <div class="view-switch__toggle">
-                            <div class="switch">
-                                <div class="switch__item">
-                                    <div class="switch-item">
-                                        <input type="radio" id="view-switch-list" name="view-switch" value="list" class="switch-item__input">
-                                        <label for="view-switch-list" class="switch-item__label"><i class="fa fa-th-list fa-fw"></i>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="switch__item">
-                                    <div class="switch-item">
-                                        <input type="radio" id="view-switch-grid" name="view-switch" value="grid" checked class="switch-item__input">
-                                        <label for="view-switch-grid" class="switch-item__label"><i class="fa fa-th fa-fw"></i>
-                                        </label>
+            <?php $this->widget(
+                'zii.widgets.CListView', [
+                    'dataProvider' => $dataProvider,
+                    'itemView' => '_item',
+                    'template' => '
+                        <div class="catalog-controls">
+                            <div class="catalog-controls__col">{sorter}</div>
+                            <div class="view-switch">
+                                <div class="view-switch__caption"></div>
+                                <div class="view-switch__toggle">
+                                    <div class="switch">
+                                        <div class="switch__item">
+                                            <div class="switch-item">
+                                                <input type="radio" id="view-switch-list" name="view-switch" value="list" class="switch-item__input">
+                                                <label for="view-switch-list" class="switch-item__label"><i class="fa fa-th-list fa-fw"></i>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="switch__item">
+                                            <div class="switch-item">
+                                                <input type="radio" id="view-switch-grid" name="view-switch" value="grid" checked class="switch-item__input">
+                                                <label for="view-switch-grid" class="switch-item__label"><i class="fa fa-th fa-fw"></i>
+                                                </label>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <?php $this->widget(
-                'zii.widgets.CListView',
-                [
-                    'dataProvider' => $dataProvider,
-                    'itemView' => '_item',
+                        {items}
+                        {pager}
+                    ',
                     'summaryText' => '',
                     'enableHistory' => true,
                     'cssFile' => false,
@@ -70,6 +67,7 @@ $this->breadcrumbs = [Yii::t("StoreModule.store", "Catalog")];
                         'price',
                         'update_time'
                     ],
+                    'sorterHeader' => '<div class="sorter__description">Сортировать:</div>',
                     'htmlOptions' => [
                         'class' => 'catalog'
                     ],
