854ae4e007bf1ce3b0adab6453c69dc0ca40456b
sabian <olegsabian@gmail.com>
2015-10-15 11:04:10 +0300

    Виджет для отображения товаров из определенной категории.

---
+++ protected/modules/store/widgets/ProductsFromCategoryWidget.php
@@ -0,0 +1,40 @@
+<?php
+Yii::import('application.modules.store.models.Product');
+
+/**
+ * Class ProductsFromCategoryWidget
+ *
+ * Show products from specified category
+ *
+ * @property integer $categoryId Products category ID
+ * @property bool|integer $limit The number of products. Default: false (unlimited)
+ * @property string $order The order of products. Default: id DESC
+ * @property string $view Widget view file
+ */
+class ProductsFromCategoryWidget extends \yupe\widgets\YWidget
+{
+    public $categoryId;
+    public $limit = false;
+    public $order = 't.id DESC';
+    public $view = 'default';
+
+    public function run()
+    {
+        $criteria = new CDbCriteria();
+        $criteria->select = 't.*';
+        $criteria->with = ['categoryRelation' => ['together' => true]];
+        $criteria->addCondition('categoryRelation.category_id = :category_id OR t.category_id = :category_id');
+        $criteria->addCondition('status = :status');
+        if ($this->limit) {
+            $criteria->limit = $this->limit;
+        }
+        $criteria->order = $this->order;
+        $criteria->group = 't.id';
+        $criteria->params = [
+            ':category_id' => (int)$this->categoryId,
+            ':status' => Product::STATUS_ACTIVE
+        ];
+
+        $this->render($this->view, ['products' => Product::model()->findAll($criteria)]);
+    }
+}
---
+++ themes/default/views/store/widgets/ProductsFromCategoryWidget/_item.php
@@ -0,0 +1,27 @@
+<?php
+$productUrl = Yii::app()->createUrl('/store/product/view', ['name' => CHtml::encode($product->slug)]);
+$basePrice = (float)$product->getBasePrice();
+$price = $product->getResultPrice();
+?>
+<div class="col-sm-1"></div>
+<div class="col-sm-10">
+    <div class="photo">
+        <a href="<?= $productUrl; ?>">
+            <img src="<?= $product->getImageUrl(190, 190, false); ?>"/>
+        </a>
+    </div>
+    <div class="info separator">
+        <div class="row">
+            <div class="price col-sm-12">
+                <h5>
+                    <a href="<?= $productUrl; ?>"><?= CHtml::encode($product->getName()); ?></a>
+                </h5>
+                <h5 class="price-text-color">
+                    <?= $product->getResultPrice(); ?> <i class="fa fa-rub"></i>
+                </h5>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<div class="col-sm-1"></div>
