d9aa9e02fb1c3e0ffc354a372dea183979f11e0f
sabian <olegsabian@gmail.com>
2015-10-16 18:13:11 +0300

    #2111 Новая тема. Проверка статуса заказа.

--- themes/shop/views/order/order/check.php
+++ themes/shop/views/order/order/check.php
@@ -2,66 +2,49 @@
 /**
  * @var $this OrderController
  * @var $model Order
+ * @var $form CActiveForm
  */
 
 $mainAssets = Yii::app()->getTheme()->getAssetsUrl();
-Yii::app()->getClientScript()->registerCssFile($mainAssets . '/css/order-frontend.css');
 Yii::app()->getClientScript()->registerScriptFile($mainAssets . '/js/store.js');
 
 $this->title = [Yii::t('OrderModule.order', 'Check order'), Yii::app()->getModule('yupe')->siteName];
 ?>
 
-
-<?php $this->widget('yupe\widgets\YFlashMessages'); ?>
-
-<?php $form = $this->beginWidget(
-    'bootstrap.widgets.TbActiveForm',
-    [
-        'id'          => 'check-order-form',
-        'type'        => 'vertical',
-        'htmlOptions' => [
-            'class' => 'well',
-        ]
-    ]
-); ?>
-
-<?= $form->errorSummary($model); ?>
-
-<?php if($order):?>
-
-    <div class="alert alert-success">
-        <?= Yii::t('OrderModule.order', 'Status') ?>: <strong><?= $order->status->getTitle();?></strong>
-    </div>
-
-<?php elseif(!$model->hasErrors()): ?>
-
-    <div class="alert alert-danger">
-        <?= Yii::t('OrderModule.order', 'Order not found!') ?>
-    </div>
-
-<?php endif;?>
-
-<div class='row'>
-    <div class="col-xs-6">
-        <?= $form->textFieldGroup($model, 'number'); ?>
-    </div>
+<div class="main__title grid">
+    <h1 class="h2"><?= Yii::t('OrderModule.order', 'Check order') ?></h1>
 </div>
 
-<div class="row">
-    <div class="col-xs-12">
-        <?php
-        $this->widget(
-            'bootstrap.widgets.TbButton',
-            [
-                'buttonType'  => 'submit',
-                'context'     => 'primary',
-                'icon'        => 'glyphicon glyphicon-signin',
-                'label'       => Yii::t('OrderModule.order', 'Check')
-            ]
-        ); ?>
+<div class="main__cart-box grid">
+    <div class="grid-module-6">
+        <?php $form = $this->beginWidget('CActiveForm', [
+            'id' => 'check-order-form',
+        ]); ?>
 
+            <?= $form->errorSummary($model); ?>
+
+            <?php if ($order): ?>
+                <div class="text-success">
+                    <?= Yii::t('OrderModule.order', 'Status') ?>: <strong><?= $order->status->getTitle(); ?></strong>
+                </div>
+            <?php elseif (!$model->hasErrors()): ?>
+                <div class="text-error">
+                    <?= Yii::t('OrderModule.order', 'Order not found!') ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="fast-order__inputs">
+                <div class="column grid-module-4">
+                    <?= $form->textField($model, 'number', ['class' => 'input input_big', 'placeholder' => $model->getAttributeLabel('number')]); ?>
+                    <?= $form->error($model, 'number') ?>
+                </div>
+                <div class="column grid-module-2 pull-right">
+                    <button type="submit" class="btn btn_big btn_primary">
+                        <?= Yii::t('OrderModule.order', 'Check') ?>
+                        <span class="fa fa-fw fa-play"></span>
+                    </button>
+                </div>
+            </div>
+        <?php $this->endWidget(); ?>
     </div>
 </div>
-
-
-<?php $this->endWidget(); ?>
