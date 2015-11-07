6771ab39548e758fd879bc3a64b87f9888668dbe
sabian <olegsabian@gmail.com>
2015-11-07 18:02:24 +0300

    fix #2240 Новая тема. Исправлена страница ошибки

--- themes/shop/views/site/error.php
+++ themes/shop/views/site/error.php
@@ -1,32 +1,32 @@
 <?php $this->title = [Yii::t('default', 'Error') . ' ' . $error['code'], Yii::app()->getModule('yupe')->siteName]; ?>
+<div class="main__title grid">
+    <h1 class="h2"><?= Yii::t('default', 'Error') . ' ' . $error['code']; ?>!</h1>
+</div>
+<div class="main__product-description grid">
+    <?php
+    switch ($error['code']) {
+        case '404':
+            $msg = Yii::t(
+                'default',
+                'Page you try to request, was not found. You can go out from this page and {link}.',
+                [
+                    '{link}' => CHtml::link(
+                        Yii::t('default', 'go to home page'),
+                        $this->createUrl("/" . Yii::app()->defaultController . '/index'),
+                        [
+                            'title' => Yii::t('default', 'go to home page'),
+                            'alt'   => Yii::t('default', 'go to home page'),
+                        ]
+                    ),
 
-<h2><?= Yii::t('default', 'Error') . ' ' . $error['code']; ?>!</h2>
+                ]
+            );
+            break;
+        default:
+            $msg = $error['message'];
+            break;
+    }
+    ?>
 
-<?php
-switch ($error['code']) {
-    case '404':
-        $msg = Yii::t(
-            'default',
-            'Page you try to request, was not found. You can go out from this page and {link}.',
-            [
-                '{link}' => CHtml::link(
-                    Yii::t('default', 'go to home page'),
-                    $this->createUrl("/" . Yii::app()->defaultController . '/index'),
-                    [
-                        'title' => Yii::t('default', 'go to home page'),
-                        'alt'   => Yii::t('default', 'go to home page'),
-                    ]
-                ),
-
-            ]
-        );
-        break;
-    default:
-        $msg = $error['message'];
-        break;
-}
-?>
-
-<p class="alert alert-danger">
-    <?= $msg; ?>
-</p>
+    <p class="errorSummary"><?= $msg; ?></p>
+</div>
