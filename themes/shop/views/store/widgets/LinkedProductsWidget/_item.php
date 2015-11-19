23b9e25854d191087ac10a6ce60e9361d8f34c2e
xoma <aopeykin@yandex.ru>
2015-11-20 00:55:56 +0300

    small fix

--- themes/shop/views/store/widgets/LinkedProductsWidget/_item.php
+++ themes/shop/views/store/widgets/LinkedProductsWidget/_item.php
@@ -1,16 +1,21 @@
 <?php $productUrl = Yii::app()->createUrl('/store/product/view', ['name' => CHtml::encode($data->slug)]); ?>
+
 <div class="h-slider__slide">
-    <div class="product-mini">
-        <div class="product-mini__thumbnail">
-            <a href="<?= $productUrl; ?>">
-                <img src="<?= $data->getImageUrl(90, 90, false); ?>" class="product-mini__img" />
-            </a>
-        </div>
-        <div class="product-mini__info">
-            <div class="product-mini__title"><a href="<?= $productUrl; ?>" class="product-mini__link"><?= CHtml::encode($data->getName()); ?></a>
+    <div class="grid-module-4">
+        <div class="product-mini">
+            <div class="product-mini__thumbnail">
+                <a href="javascript:void(0);">
+                    <img src="<?= $data->getImageUrl(90, 90, false); ?>"
+                         class=" product-mini__img">
+                </a>
             </div>
-            <div class="product-mini__price">
-                <div class="product-price"><?= $data->getResultPrice(); ?><span class="ruble"> <?= Yii::t("StoreModule.store", "RUB"); ?></span>
+            <div class="product-mini__info">
+                <div class="product-mini__title"><a href="<?= $productUrl; ?>"
+                                                    class="product-mini__link"><?= Chtml::encode($data->getName()); ?></a>
+                </div>
+                <div class="product-mini__price">
+                    <div class="product-price"><?= $data->getResultPrice(); ?><span class="ruble"> руб.</span>
+                    </div>
                 </div>
             </div>
         </div>
--- themes/shop/views/store/widgets/LinkedProductsWidget/linked-products.php
+++ themes/shop/views/store/widgets/LinkedProductsWidget/linked-products.php
@@ -1,18 +1,28 @@
-<?php /* @var $dataProvider CActiveDataProvider */ ?>
-<div data-show='4' data-scroll='4' data-infinite="data-infinite" class="h-slider js-slick">
-    <div class="h-slider__buttons">
-        <div class="btn h-slider__control h-slider__next js-slick__next"></div>
-        <div class="btn h-slider__control h-slider__prev js-slick__prev"></div>
+<div class="main__recently-viewed-slider">
+    <div class="grid">
+        <div class="h3">Похожие товары</div>
+        <div data-show='3' data-scroll='3' data-infinite="data-infinite" class="h-slider js-slick">
+            <div class="h-slider__buttons h-slider__buttons_noclip">
+                <div class="btn h-slider__control h-slider__next js-slick__next slick-arrow" style="display: block;"></div>
+                <div class="btn h-slider__control h-slider__prev js-slick__prev slick-arrow" style="display: block;"></div>
+            </div>
+            <div class="h-slider__slides js-slick__container">
+
+                <?php $this->widget(
+                    'zii.widgets.CListView',
+                    [
+                        'dataProvider' => $dataProvider,
+                        'template' => '{items}',
+                        'itemView' => '_item',
+                        'cssFile' => false,
+                        'pager' => false,
+                    ]
+                ); ?>
+
+            </div>
+        </div>
     </div>
-    <?php $this->widget(
-        'zii.widgets.CListView',
-        [
-            'dataProvider' => $dataProvider,
-            'template' => '{items}',
-            'itemView' => '_item',
-            'cssFile' => false,
-            'pager' => false,
-            'itemsCssClass' => 'h-slider__slides js-slick__container'
-        ]
-    ); ?>
 </div>
+
+
+
