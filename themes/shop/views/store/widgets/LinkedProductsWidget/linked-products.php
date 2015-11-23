31b5c70b2e50a56cdf5944c037272fd225ed42ee
sabian <olegsabian@gmail.com>
2015-11-23 19:26:26 +0300

    fix #2256 Исправлена верстка блока похожих товаров

--- themes/shop/views/store/widgets/LinkedProductsWidget/linked-products.php
+++ themes/shop/views/store/widgets/LinkedProductsWidget/linked-products.php
@@ -6,23 +6,14 @@
                 <div class="btn h-slider__control h-slider__next js-slick__next slick-arrow" style="display: block;"></div>
                 <div class="btn h-slider__control h-slider__prev js-slick__prev slick-arrow" style="display: block;"></div>
             </div>
-            <div class="h-slider__slides js-slick__container">
-
-                <?php $this->widget(
-                    'zii.widgets.CListView',
-                    [
-                        'dataProvider' => $dataProvider,
-                        'template' => '{items}',
-                        'itemView' => '_item',
-                        'cssFile' => false,
-                        'pager' => false,
-                    ]
-                ); ?>
-
-            </div>
+                <?php $this->widget('zii.widgets.CListView', [
+                    'dataProvider' => $dataProvider,
+                    'template' => '{items}',
+                    'itemView' => '_item',
+                    'itemsCssClass' => 'h-slider__slides js-slick__container',
+                    'cssFile' => false,
+                    'pager' => false,
+                ]); ?>
         </div>
     </div>
 </div>
-
-
-
