116874c7cab51224e7b63981db93b3755bed1714
sabian <olegsabian@gmail.com>
2016-01-02 21:41:12 +0300

    fix filter product by status

--- protected/modules/store/models/Product.php
+++ protected/modules/store/models/Product.php
@@ -312,7 +312,7 @@ class Product extends yupe\models\YModel implements ICommentable
         $criteria->compare('slug', $this->slug, true);
         $criteria->compare('data', $this->data, true);
         $criteria->compare('is_special', $this->is_special, true);
-        $criteria->compare('status', $this->status);
+        $criteria->compare('t.status', $this->status);
         $criteria->compare('create_time', $this->create_time, true);
         $criteria->compare('update_time', $this->update_time, true);
         $criteria->compare('producer_id', $this->producer_id);
