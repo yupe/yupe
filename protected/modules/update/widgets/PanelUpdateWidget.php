2864cb82c2c9fc4ff920dad4b9bde7b51d5eee26
xoma <aopeykin@yandex.ru>
2016-01-19 12:16:29 +0300

    rm update module

--- protected/modules/update/widgets/PanelUpdateWidget.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class PanelUpdateWidget extends \yupe\widgets\YWidget
-{
-    public function run()
-    {
-        $this->render('panel-update', [ 'count' => Yii::app()->updateManager->getUpdatesCount() ]);
-    }
-} 
