a2e0532e6b67e70adf6fc86faa3f1214b064b7fb
sabian <olegsabian@gmail.com>
2016-02-20 11:08:23 +0300

    Исправлена ошибка ограничения кол-ва товаров в ProductsFromCategoryWidget

--- protected/modules/store/components/repository/ProductRepository.php
+++ protected/modules/store/components/repository/ProductRepository.php
@@ -193,6 +193,11 @@ class ProductRepository extends CApplicationComponent
      */
     public function getListForCategory(StoreCategory $category, $limit = null)
     {
+        $pagination = [
+            'pageSize' => (int)Yii::app()->getModule('store')->itemsPerPage,
+            'pageVar' => 'page',
+        ];
+
         $categories = $category->getChildsArray();
         $categories[] = $category->id;
 
@@ -203,7 +208,9 @@ class ProductRepository extends CApplicationComponent
         $criteria->addInCondition('t.category_id', $categories, 'OR');
         $criteria->group = 't.id';
         $criteria->scopes = ['published'];
+
         if ($limit) {
+            $pagination = false;
             $criteria->limit = $limit;
         }
 
@@ -211,10 +218,7 @@ class ProductRepository extends CApplicationComponent
             Product::model(),
             [
                 'criteria' => $criteria,
-                'pagination' => [
-                    'pageSize' => (int)Yii::app()->getModule('store')->itemsPerPage,
-                    'pageVar' => 'page',
-                ],
+                'pagination' => $pagination,
                 'sort' => [
                     'sortVar' => 'sort',
                     'defaultOrder' => 't.position',
