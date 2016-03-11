297c6a8508b88675e5c97b7a37923243a08cb695
xoma <aopeykin@yandex.ru>
2016-03-11 15:47:56 +0300

    fresh CKEditor + plugins

--- protected/modules/yupe/widgets/editors/CKEditor.php
+++ protected/modules/yupe/widgets/editors/CKEditor.php
@@ -11,21 +11,46 @@ use CInputWidget;
 class CKEditor extends \CInputWidget
 {
     /**
-     * @var string
-     */
-    private $editorWidgetClass = 'bootstrap.widgets.TbCKEditor';
-
-    /**
      * @var array
      */
-    public $options = [];
+    public $editorOptions = [];
 
     /**
      * @throws \Exception
      */
     public function run()
     {
-        Yii::app()->getClientScript()->registerScript(__CLASS__,'
+        list($name, $id) = $this->resolveNameID();
+
+        $this->htmlOptions['id'] = $id;
+
+        echo \CHtml::textArea($name, $this->value, $this->htmlOptions);
+
+        $this->registerClientScript($id);
+    }
+
+    /**
+     * @param $id
+     */
+    public function registerClientScript($id)
+    {
+        $options = [
+            'language' => Yii::app()->getLanguage(),
+            'filebrowserUploadUrl' => Yii::app()->createUrl('/yupe/backend/AjaxImageUploadCKE'),
+            'extraPlugins' => 'table,tableresize,tabletools,stylesheetparser,embed,image',
+        ];
+
+        $assets = Yii::app()->getAssetManager()->publish(
+            Yii::getPathOfAlias('vendor').'/ckeditor/ckeditor/'
+        );
+
+        Yii::app()->getClientScript()->registerScriptFile($assets.'/ckeditor.js', \CClientScript::POS_HEAD);
+        Yii::app()->getClientScript()->registerScriptFile($assets.'/lang/'.Yii::app()->getLanguage().'.js',
+            \CClientScript::POS_HEAD);
+
+        $options = \CJavaScript::encode(\CMap::mergeArray($options, $this->editorOptions));
+
+        Yii::app()->getClientScript()->registerScript(__CLASS__, '
              $(document).off(\'click\', \'.cke_dialog_tabs a:eq(2)\').on(\'click\', \'.cke_dialog_tabs a:eq(2)\', function () {
                 var $form = $(\'.cke_dialog_ui_input_file iframe\').contents().find(\'form\');
                 if (!$form.find(\'input[name=\' + yupeTokenName + \']\').length) {
@@ -38,27 +63,9 @@ class CKEditor extends \CInputWidget
             });
        ');
 
-        $this->widget(
-            $this->editorWidgetClass,
-            [
-                'model' => $this->model,
-                'attribute' => $this->attribute,
-                'name' => $this->name,
-                'editorOptions' => \CMap::mergeArray(
-                    $this->getOptions(),
-                    [
-                        'filebrowserUploadUrl' => Yii::app()->createUrl('/yupe/backend/AjaxImageUploadCKE')
-                    ]
-                ),
-            ]
+        Yii::app()->getClientScript()->registerScript(
+            __CLASS__.'#'.$this->getId(),
+            "CKEDITOR.replace( '$id', $options);"
         );
-    }
-
-    /**
-     * @return array
-     */
-    public function getOptions()
-    {
-        return [];
     }
 }
