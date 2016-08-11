c5490db900eb32c5443ef7bff34278aa8d74e4cc
Дмитрий <divangt@mail.ru>
2016-08-11 12:22:46 +0300

    зависимость для кнопки от атрибутов

--- themes/shop/views/store/widgets/FilterBlockWidget/filter-block.php
+++ themes/shop/views/store/widgets/FilterBlockWidget/filter-block.php
@@ -2,11 +2,13 @@
 /* @var $attributes array */
 ?>
 
-<?php $this->widget(
+<?php $this->beginWidget(
     'application.modules.store.widgets.filters.AttributesFilterWidget', [
         'attributes' => $attributes,
         'category' => $category
     ]
 ) ?>
 
-<div class="catalog-filter__button"><input type="submit" value="Подобрать" class="btn btn_wide"/></div>
+    <div class="catalog-filter__button"><input type="submit" value="Подобрать" class="btn btn_wide"/></div>
+
+<?php $this->endWidget();?>
