ee9ad1888a531d73cae0d11feae4de9b3788ea6d
sabian <olegsabian@gmail.com>
2016-11-04 16:33:38 +0300

    Страница ошибки

--- protected/modules/yupe/views/backend/error.php
+++ protected/modules/yupe/views/backend/error.php
@@ -1,6 +1,32 @@
-<?php $this->title = Yii::t('default', 'Error') . ' ' . $error['code']; ?>
+<?php $this->pageTitle = Yii::t('default', 'Error') . ' ' . $error['code'] . ' - ' . $this->yupe->siteName; ?>
 
-<h2><?= $error['code']; ?>!</h2>
+<h2><?= Yii::t('default', 'Error') . ' ' . $error['code']; ?>!</h2>
 
-<?= $msg; ?>
+<?php
+switch ($error['code']) {
+    case '404':
+        $msg = Yii::t(
+            'default',
+            'Page you try to request, was not found. You can go out from this page and {link}.',
+            [
+                '{link}' => CHtml::link(
+                    Yii::t('default', 'go to home page'),
+                    $this->createUrl('/yupe/backend/index'),
+                    [
+                        'title' => Yii::t('default', 'go to home page'),
+                        'alt'   => Yii::t('default', 'go to home page'),
+                    ]
+                ),
 
+            ]
+        );
+        break;
+    default:
+        $msg = $error['message'];
+        break;
+}
+?>
+
+<p class="alert alert-danger">
+    <?= $msg; ?>
+</p>
