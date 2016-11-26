6a3e896f56be03e3c15416d8bbb29ef04ea045d7
sabian <olegsabian@gmail.com>
2016-11-26 11:08:50 +0300

    #2587 Модуль callback. Небольшие правки

--- protected/modules/callback/controllers/CallbackController.php
+++ protected/modules/callback/controllers/CallbackController.php
@@ -10,11 +10,13 @@ class CallbackController extends \yupe\components\controllers\FrontController
      */
     public function actionSend()
     {
-        if (!Yii::app()->getRequest()->getIsPostRequest() && !Yii::app()->getRequest()->getPost('Callback')) {
+        $request = Yii::app()->getRequest();
+
+        if (!$request->getIsPostRequest() && !$request->getPost('Callback')) {
             throw new CHttpException(404);
         }
 
-        if(Yii::app()->callbackManager->add(Yii::app()->getRequest()->getPost('Callback'), Yii::app()->getRequest()->getUrlReferrer()) === true) {
+        if(Yii::app()->callbackManager->add($request->getPost('Callback'), $request->getUrlReferrer()) === true) {
             Yii::app()->ajax->success(Yii::t('CallbackModule.callback', 'Your message has been successfully sent.'));
         } else {
             Yii::app()->ajax->failure(Yii::t('CallbackModule.callback', 'Sorry, an error has occurred.'));
