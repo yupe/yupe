49935cb9c3498bdb5b2eb7cf3ba93b00eb75fd1d
Anton <koma136@mail.ru>
2017-10-20 11:18:46 +0300

    54-fz pay receipt

--- protected/modules/yandexmoney/views/form.php
+++ protected/modules/yandexmoney/views/form.php
@@ -9,12 +9,13 @@
 <?= CHtml::hiddenField('shopId', $settings['shopid']) ?>
 <?= CHtml::hiddenField('scid', $settings['scid']) ?>
 <?= CHtml::hiddenField('sum', $order->getTotalPriceWithDelivery()) ?>
-<?= CHtml::hiddenField('customerNumber', $order->user_id) ?>
+<?= CHtml::hiddenField('customerNumber', !empty($order->user_id)? $order->user_id : $order->email) ?>
 <?= CHtml::hiddenField('paymentType', $settings['type']) ?>
 <?= CHtml::hiddenField('orderNumber', $order->id) ?>
 <?= CHtml::hiddenField('cps_phone', CHtml::encode($order->phone)) ?>
 <?= CHtml::hiddenField('cps_email', CHtml::encode($order->email)) ?>
 <?= CHtml::hiddenField('shopSuccessURL', Yii::app()->createAbsoluteUrl('/order/order/view', ['url' => $order->url])) ?>
 <?= CHtml::hiddenField('shopFailURL', Yii::app()->createAbsoluteUrl('/order/order/view', ['url' => $order->url])) ?>
+<?= CHtml::hiddenField('ym_merchant_receipt', json_encode($ym_merchant_receipt, JSON_UNESCAPED_UNICODE)) ?>
 <?= CHtml::submitButton(Yii::t('YandexMoneyModule.ymoney', 'Pay')) ?>
 <?= CHtml::endForm() ?>
