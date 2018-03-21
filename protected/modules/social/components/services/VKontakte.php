ecebb7018a8235d0c988380d42fcd956ad5be994
Anton <koma136@mail.ru>
2018-03-21 18:39:27 +0300

    VK Отказ от версии по умолчанию

--- protected/modules/social/components/services/VKontakte.php
+++ protected/modules/social/components/services/VKontakte.php
@@ -25,7 +25,44 @@ class VKontakte extends VKontakteOAuthService
 
         return false;
     }
+    protected function fetchAttributes() {
+        $info = (array)$this->makeSignedRequest('https://api.vk.com/method/users.get.json', array(
+            'query' => array(
+                'user_ids' => $this->uid,
+                'version'    => '5.50',
+                'fields' => '', // uid, first_name and last_name is always available
+                //'fields' => 'nickname, sex, bdate, city, country, timezone, photo, photo_medium, photo_big, photo_rec',
+            ),
+        ));
 
+        $info = $info['response'][0];
+
+        $this->attributes['id'] = $info->uid;
+        $this->attributes['name'] = $info->first_name . ' ' . $info->last_name;
+        $this->attributes['url'] = 'http://vk.com/id' . $info->uid;
+
+    }
+    protected function getCodeUrl($redirect_uri) {
+        $this->setState('redirect_uri', $redirect_uri);
+
+        $url = parent::getCodeUrl($redirect_uri);
+        if (isset($_GET['js'])) {
+            $url .= '&display=popup';
+        }
+        $url_parts = parse_url($url);
+        if (!isset($url_parts['v']) || !isset($url_parts['version'])) {
+            $url .= '&v=5.50';
+        }
+        return $url;
+    }
+    protected function getTokenUrl($code) {
+        $url = parent::getTokenUrl($code);
+        $url_parts = parse_url($url);
+        if (!isset($url_parts['v']) || !isset($url_parts['version'])) {
+            $url .= '&v=5.50';
+        }
+        return $url;
+    }
     public function getAuthData()
     {
         return $this->getState(self::AUTH_DATA_KEY);
