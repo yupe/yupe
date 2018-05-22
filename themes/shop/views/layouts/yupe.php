7ba41f557d866a13e40f271df09aa6c94b630e44
Oleg Filimonov <olegsabian@gmail.com>
2018-05-22 21:37:31 +0300

    Update yupe.php

--- themes/shop/views/layouts/yupe.php
+++ themes/shop/views/layouts/yupe.php
@@ -15,7 +15,7 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title><?= $this->title;?></title>
     <meta name="description" content="<?= $this->description;?>" />
