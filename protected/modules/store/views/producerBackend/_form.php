9df6c009fcae695e8aa0479d12a476116ba81e01
dzhedai <sadreev-kzn@ya.ru>
2018-05-23 18:00:36 +0300

    #2731: Стандартизация формы производителя

--- protected/modules/store/views/producerBackend/_form.php
+++ protected/modules/store/views/producerBackend/_form.php
@@ -4,6 +4,14 @@
  * @var $model Producer
  * @var $form \yupe\widgets\ActiveForm
  */
+?>
+
+<ul class="nav nav-tabs">
+    <li class="active"><a href="#common" data-toggle="tab"><?= Yii::t("StoreModule.store", "General"); ?></a></li>
+    <li><a href="#seo" data-toggle="tab"><?= Yii::t("StoreModule.store", "Data for SEO"); ?></a></li>
+</ul>
+
+<?php
 $form = $this->beginWidget(
     '\yupe\widgets\ActiveForm',
     [
@@ -23,147 +31,137 @@ $form = $this->beginWidget(
 
 <?= $form->errorSummary($model); ?>
 
-<div class="row">
-    <div class="col-sm-2">
-        <?= $form->dropDownListGroup(
-            $model,
-            'status',
-            [
-                'widgetOptions' => [
-                    'data' => $model->getStatusList(),
-                ],
-            ]
-        ); ?>
-    </div>
-</div>
+<div class="tab-content">
+    <div class="tab-pane active" id="common">
 
-<div class="row">
-    <div class="col-sm-6">
-        <?= $form->textFieldGroup($model, 'name_short'); ?>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-6">
-        <?= $form->textFieldGroup($model, 'name'); ?>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <?= $form->slugFieldGroup($model, 'slug', ['sourceAttribute' => 'name_short']); ?>
-    </div>
-</div>
-
-<div class='row'>
-    <div class="col-sm-7">
-        <?php
-        echo CHtml::image(
-            !$model->getIsNewRecord() && $model->image ? $model->getImageUrl() : '#',
-            $model->name,
-            [
-                'class' => 'preview-image img-thumbnail',
-                'style' => !$model->getIsNewRecord() && $model->image ? '' : 'display:none',
-            ]
-        ); ?>
-        <?= $form->fileFieldGroup(
-            $model,
-            'image',
-            [
-                'widgetOptions' => [
-                    'htmlOptions' => [
-                        'onchange' => 'readURL(this);',
-                    ],
-                ],
-            ]
-        ); ?>
-    </div>
-</div>
-<div class='row'>
-    <div class="col-sm-12 <?= $model->hasErrors('description') ? 'has-error' : ''; ?>">
-        <?= $form->labelEx($model, 'description'); ?>
-        <?php $this->widget(
-            $this->module->getVisualEditor(),
-            [
-                'model' => $model,
-                'attribute' => 'description',
-            ]
-        ); ?>
-        <p class="help-block"></p>
-        <?= $form->error($model, 'description'); ?>
-    </div>
-</div>
-
-<div class='row'>
-    <div class="col-sm-12 <?= $model->hasErrors('short_description') ? 'has-error' : ''; ?>">
-        <?= $form->labelEx($model, 'short_description'); ?>
-        <?php $this->widget(
-            $this->module->getVisualEditor(),
-            [
-                'model' => $model,
-                'attribute' => 'short_description',
-            ]
-        ); ?>
-        <p class="help-block"></p>
-        <?= $form->error($model, 'short_description'); ?>
-    </div>
-</div>
-
-<?php $collapse = $this->beginWidget('bootstrap.widgets.TbCollapse'); ?>
-<div class="panel-group" id="extended-options">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="panel-title">
-                <a data-toggle="collapse" data-parent="#extended-options" href="#collapseOne">
-                    <?= Yii::t('StoreModule.store', 'Data for SEO'); ?>
-                </a>
+        <div class="row">
+            <div class="col-sm-2">
+                <?= $form->dropDownListGroup(
+                    $model,
+                    'status',
+                    [
+                        'widgetOptions' => [
+                            'data' => $model->getStatusList(),
+                        ],
+                    ]
+                ); ?>
             </div>
         </div>
-        <div id="collapseOne" class="panel-collapse collapse">
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-sm-7">
-                        <?= $form->textFieldGroup($model, 'meta_title'); ?>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <?= $form->textFieldGroup($model, 'name_short'); ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <?= $form->textFieldGroup($model, 'name'); ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-7">
+                <?= $form->slugFieldGroup($model, 'slug', ['sourceAttribute' => 'name_short']); ?>
+            </div>
+        </div>
+
+        <div class='row'>
+            <div class="col-sm-7">
+                <?php
+                echo CHtml::image(
+                    !$model->getIsNewRecord() && $model->image ? $model->getImageUrl() : '#',
+                    $model->name,
+                    [
+                        'class' => 'preview-image img-thumbnail',
+                        'style' => !$model->getIsNewRecord() && $model->image ? '' : 'display:none',
+                    ]
+                ); ?>
+                <?= $form->fileFieldGroup(
+                    $model,
+                    'image',
+                    [
+                        'widgetOptions' => [
+                            'htmlOptions' => [
+                                'onchange' => 'readURL(this);',
+                            ],
+                        ],
+                    ]
+                ); ?>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-sm-12 <?= $model->hasErrors('description') ? 'has-error' : ''; ?>">
+                <?= $form->labelEx($model, 'description'); ?>
+                <?php $this->widget(
+                    $this->module->getVisualEditor(),
+                    [
+                        'model' => $model,
+                        'attribute' => 'description',
+                    ]
+                ); ?>
+                <p class="help-block"></p>
+                <?= $form->error($model, 'description'); ?>
+            </div>
+        </div>
+
+        <div class='row'>
+            <div class="col-sm-12 <?= $model->hasErrors('short_description') ? 'has-error' : ''; ?>">
+                <?= $form->labelEx($model, 'short_description'); ?>
+                <?php $this->widget(
+                    $this->module->getVisualEditor(),
+                    [
+                        'model' => $model,
+                        'attribute' => 'short_description',
+                    ]
+                ); ?>
+                <p class="help-block"></p>
+                <?= $form->error($model, 'short_description'); ?>
+            </div>
+        </div>
+
+        <?php $collapse = $this->beginWidget('bootstrap.widgets.TbCollapse'); ?>
+        <div class="panel-group" id="template-options">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="panel-title">
+                        <a data-toggle="collapse" data-parent="#template-options" href="#collapse-template">
+                            <?= Yii::t('StoreModule.store', 'Templates settings'); ?>
+                        </a>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-7">
-                        <?= $form->textFieldGroup($model, 'meta_keywords'); ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-7">
-                        <?= $form->textAreaGroup($model, 'meta_description'); ?>
+                <div id="collapse-template" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-7">
+                                <?= $form->textFieldGroup($model, 'view'); ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <?php $this->endWidget(); ?>
     </div>
-</div>
-<?php $this->endWidget(); ?>
 
-<?php $collapse = $this->beginWidget('bootstrap.widgets.TbCollapse'); ?>
-<div class="panel-group" id="template-options">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="panel-title">
-                <a data-toggle="collapse" data-parent="#template-options" href="#collapse-template">
-                    <?= Yii::t('StoreModule.store', 'Templates settings'); ?>
-                </a>
+    <div class="tab-pane" id="seo">
+        <div class="row">
+            <div class="col-sm-7">
+                <?= $form->textFieldGroup($model, 'meta_title'); ?>
             </div>
         </div>
-        <div id="collapse-template" class="panel-collapse collapse">
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-sm-7">
-                        <?= $form->textFieldGroup($model, 'view'); ?>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col-sm-7">
+                <?= $form->textFieldGroup($model, 'meta_keywords'); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-7">
+                <?= $form->textAreaGroup($model, 'meta_description'); ?>
             </div>
         </div>
     </div>
 </div>
-<?php $this->endWidget(); ?>
 
 <?php $this->widget(
     'bootstrap.widgets.TbButton',
